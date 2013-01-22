d0f310e3ebb4fb8bb9763887a4650928cd9e7d9c
Sunny Sun <ssun@salesforce.com>
2013-01-22 12:10:26 -0800

    Title:ui:menu
    Description:ui:menu
    
    @bug W-1480521@
    
    @rev jtroup jmooney@

---
+++ aura-components/src/main/components/ui/menu/menuController.js
@@ -0,0 +1,45 @@
+/* 
+ * Copyright (C) 2012 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+({
+    doInit: function(component, event, helper) {
+        var children = [];
+        // Set "parent" for menu items in body
+        var body = component.getValue("v.body");
+        for (var i = 0; i < body.getLength(); i++) {
+            var c = body.getValue(i);
+            if (c.isInstanceOf("ui:menuItem")) {
+                children.push(c);
+            }
+            if (c.getDef().getAttributeDefs().getDef("parent")) {
+                c.setValue("{!v.parent}", [component]);
+            }
+        }
+        // Set "parent" for menu items in iteration
+        var items = component.find("item");
+        if (items && $A.util.isArray(items)) {
+            for (var j = 0; j < items.length; j++) {
+                var item = items[j];
+                if (item.isInstanceOf("ui:menuItem")) {
+                    children.push(item);
+                }
+                if (item.getDef().getAttributeDefs().getDef("parent")) {
+                    item.setValue("{!v.parent}", [component]);
+                }
+            }
+        }
+        component.setValue("{!v.childMenuItems}", children);
+    }
+})
---
+++ aura-components/src/main/java/org/auraframework/components/ui/MenuItem.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2012 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.auraframework.components.ui;
+
+import java.io.IOException;
+
+import org.auraframework.util.json.Json;
+import org.auraframework.util.json.JsonSerializable;
+
+/**
+ * A menu item object has all the information to create a ui:menuItem component (actionMenuItem, checkboxMenuItem and radioMenuItem).
+ *
+ *
+ * @since 184
+ */
+public class MenuItem implements JsonSerializable {
+    private final String label;    
+    private final boolean selected;
+    private final String type;
+    
+    public MenuItem(String label, boolean selected, String type) {
+        this.label = label;
+        
+        this.selected = selected;
+        this.type = type;
+    }
+
+    /**
+     * Get the label.
+     * @return the label of this menu item
+     */
+    public String getLabel() {
+        return this.label;
+    }
+
+    
+
+    /**
+     * Is the component selected.
+     * @return the true if selected, otherwise false
+     */
+    public boolean isSelected() {
+        return this.selected;
+    }
+
+    /**
+     * Get the value.
+     * @return the value of this option
+     */
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public void serialize(Json json) throws IOException {
+        json.writeMapBegin();
+        json.writeMapEntry("label", this.label);
+        
+        json.writeMapEntry("selected", this.selected);
+        json.writeMapEntry("type", this.type);
+        json.writeMapEnd();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MenuItem(label[%s] selected[%s] type[%s])", this.label, this.selected, this.type);
+    }
+
+}

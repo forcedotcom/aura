516353edadc193a64431a81830801020ada239ff
Diana Widjaja <dwidjaja@mleonard-wsl.internal.salesforce.com>
2013-01-29 13:04:36 -0800

    Doc: Using UI Components

---
+++ aura-components/src/main/components/auradocs/demoForm/demoFormController.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2012 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+  getInput: function(cmp, event) {
+        var inputCmp = cmp.find("myinput");
+        var inputItems = inputCmp.getValue("v.childinputItems");
+        var values = [];
+      //  for (var i = 0; i < inputItems.getLength(); i++) {
+       //     var c = inputItems.getValue(i);
+       //     if (c.get("v.selected") === true) {
+       //         values.push(c.get("v.label"));
+       //     }
+        }

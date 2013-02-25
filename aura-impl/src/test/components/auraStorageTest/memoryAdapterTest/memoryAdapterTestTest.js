1962a3fa31e2426f21cb53435f3f5fcc367be532
Kevin Venkiteswaran <kvenkiteswaran@salesforce.com>
2013-02-25 09:14:03 -0800

    Replace SFDC employee names with Greek lettters
    @bug W-1554124@
    @rev bdubois@

--- aura-impl/src/test/components/auraStorageTest/memoryAdapterTest/memoryAdapterTestTest.js
+++ aura-impl/src/test/components/auraStorageTest/memoryAdapterTest/memoryAdapterTestTest.js
@@ -13,98 +13,98 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 ({
-	setUp:function(cmp){
-		this.adapter = new $A.storageService.createAdapter("memory");
-	},
-	
-	testSizeInitial:{
-		test:function(cmp) {
-			$A.test.assertEquals(0, this.adapter.getSize());
-		}
-	},
-	
-	testSizeOneObject:{
-		test:function(cmp) {
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer"}});
-			// the key and object have 25 characters + 2 associative array entries
-			// the expected number of bytes is: (25 * 2) + (2 * 8) = 66
-			$A.test.assertEquals(66, this.adapter.getSize());
-		}
-	},
+    setUp:function(cmp){
+        this.adapter = new $A.storageService.createAdapter("memory");
+    },
 
-	testSizeSameKeySameObject:{
-		test:function(cmp) {
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer"}});
-			var originalSize = this.adapter.getSize();
-			
-			// do nothing and the size should not have changed
-			$A.test.assertEquals(originalSize, this.adapter.getSize());
-			
-			// add another object to trigger a recalculation of size
-			this.adapter.setItem("key2", {});
-			// size should be the original + the 8 for the new key
-			$A.test.assertEquals(originalSize + (8), this.adapter.getSize());
-		}
-	},
-	
-	testSizeSameKeyEqualObject:{
-		test:function(cmp) {		
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer"}});
-			var originalSize = this.adapter.getSize();
-			
-			// set the key with an equal (===) object.
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer"}});
-			// the size should not have changed
-			$A.test.assertEquals(originalSize, this.adapter.getSize());
-		}
-	},
+    testSizeInitial:{
+        test:function(cmp) {
+            $A.test.assertEquals(0, this.adapter.getSize());
+        }
+    },
 
-	testSizeSameKeyDifferentObject:{
-		test:function(cmp) {
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer"}});
-			// the key and object have 25 characters + 2 associative array entries
-			// the expected number of bytes is: (25 * 2) + (2 * 8) = 66
-			$A.test.assertEquals(66, this.adapter.getSize());
-			
-			this.adapter.setItem("key1", {"value": {"who":"Matthew", "what":"Developer", "now": true}});
-			// new object has one boolean + 30 characters + 3 associative array entries
-			// the expected number of bytes is: 4 + (30 * 2) + (3 * 8) = 88 
-			$A.test.assertEquals(88, this.adapter.getSize());
-		}
-	},
+    testSizeOneObject:{
+        test:function(cmp) {
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            // the key and object have 23 characters + 2 associative array entries
+            // the expected number of bytes is: (23 * 2) + (2 * 8) = 62
+            $A.test.assertEquals(62, this.adapter.getSize());
+        }
+    },
 
-	testSizeMultipleObjects:{
-		test:function(cmp) {
-			// 68 bytes
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer1"}});
-			// 68 bytes
-			this.adapter.setItem("key2", {"value": {"who":"Kevin", "what":"Developer2"}});
-			// 72 bytes
-			this.adapter.setItem("key3", {"value": {"who":"Matthew", "what":"Developer3"}});
-			// 66 bytes
-			this.adapter.setItem("key4", {"value": {"who":"Dave", "what":"Developer4"}});
-			
-			$A.test.assertEquals(274, this.adapter.getSize());
-		}
-	},
-	
-	testSizeAfterRemoveKey:{
-		test:function(cmp) {
-			// the bytes for each line is: (25 chars * 2) + (2 * 8) = 66
-			this.adapter.setItem("key1", {"value": {"who":"Brian", "what":"Developer"}});
-			this.adapter.setItem("key2", {"value": {"who":"Kevin", "what":"Developer"}});
-			$A.test.assertEquals(132, this.adapter.getSize());
-			
-			this.adapter.removeItem("key1");
-			//removing the key, removes it from this.storage
-			$A.test.assertEquals(66, this.adapter.getSize());
-			
-			// adding a new key, new size of object.  70 bytes
-			this.adapter.setItem("key1", {"value": {"who":"Brian2", "what":"Developer3"}});
-			$A.test.assertEquals(136, this.adapter.getSize());
-		}
-	}
-	
+    testSizeSameKeySameObject:{
+        test:function(cmp) {
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            var originalSize = this.adapter.getSize();
+
+            // do nothing and the size should not have changed
+            $A.test.assertEquals(originalSize, this.adapter.getSize());
+
+            // add another object to trigger a recalculation of size
+            this.adapter.setItem("key2", {});
+            // size should be the original + the 8 for the new key
+            $A.test.assertEquals(originalSize + (8), this.adapter.getSize());
+        }
+    },
+
+    testSizeSameKeyEqualObject:{
+        test:function(cmp) {
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            var originalSize = this.adapter.getSize();
+
+            // set the key with an equal (===) object.
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            // the size should not have changed
+            $A.test.assertEquals(originalSize, this.adapter.getSize());
+        }
+    },
+
+    testSizeSameKeyDifferentObject:{
+        test:function(cmp) {
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            // the key and object have 23 characters + 2 associative array entries
+            // the expected number of bytes is: (23 * 2) + (2 * 8) = 62
+            $A.test.assertEquals(62, this.adapter.getSize());
+
+            this.adapter.setItem("key1", {"value": {"alpha":"epsilon", "gamma":"zeta", "now": true}});
+            // new object has one boolean + 28 characters + 3 associative array entries
+            // the expected number of bytes is: 4 + (28 * 2) + (3 * 8) = 84
+            $A.test.assertEquals(84, this.adapter.getSize());
+        }
+    },
+
+    testSizeMultipleObjects:{
+        test:function(cmp) {
+            // 62 bytes
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            // 66 bytes
+            this.adapter.setItem("key2", {"value": {"alpha":"epsilon", "gamma":"zeta"}});
+            // 60 bytes
+            this.adapter.setItem("key3", {"value": {"alpha":"eta", "gamma":"theta"}});
+            // 62 bytes
+            this.adapter.setItem("key4", {"value": {"alpha":"iota", "gamma":"kappa"}});
+
+            $A.test.assertEquals(250, this.adapter.getSize());
+        }
+    },
+
+    testSizeAfterRemoveKey:{
+        test:function(cmp) {
+            // the bytes for each line is: (23 chars * 2) + (2 * 8) = 62
+            this.adapter.setItem("key1", {"value": {"alpha":"beta", "gamma":"delta"}});
+            this.adapter.setItem("key2", {"value": {"alpha":"iota", "gamma":"kappa"}});
+            $A.test.assertEquals(124, this.adapter.getSize());
+
+            this.adapter.removeItem("key1");
+            //removing the key, removes it from this.storage
+            $A.test.assertEquals(62, this.adapter.getSize());
+
+            // adding a new key, new size of object.  64 bytes
+            this.adapter.setItem("key1", {"value": {"alpha":"epsilon", "gamma":"zeta"}});
+            $A.test.assertEquals(128, this.adapter.getSize());
+        }
+    }
+
 })

48c93a1187a40aca943073f737f1ebbbded0a21a
Trevor Bliss <tbliss@salesforce.com>
2013-04-18 15:21:27 -0700

    Automation for getting/setting data attributes on dom element
    @bug W-1611830@
    
    @rev gtorres@

--- aura-impl/src/test/components/clientApiTest/utilTests/utilTestsTest.js
+++ aura-impl/src/test/components/clientApiTest/utilTests/utilTestsTest.js
@@ -39,5 +39,43 @@
             var foo = {};
             $A.test.assertFalse( $A.util.isComponent(foo), "Should not be a component");
         }
+    },
+
+    testGetAndSetDataAttributes: {
+        test : function(cmp){
+            var div = cmp.find("aDiv").getElement();
+            var data = $A.util.getDataAttribute(div, "testData");
+            $A.test.assertEquals("divtestdata", data, "Could not retrieve data attribute hardcoded on div");
+
+            $A.util.setDataAttribute(div, "testData", "newdata");
+            data = $A.util.getDataAttribute(div, "testData");
+            $A.test.assertEquals("newdata", data, "Could not set data attribute to new value");
+
+            $A.util.setDataAttribute(div, "testMonkey", "bananas");
+            var newAttrVal = $A.util.getDataAttribute(div, "testMonkey");
+            $A.test.assertEquals("bananas", newAttrVal, "Setting data attribute with new key should create data attribute");
+        }
+    },
+
+    /**
+     * Verify setting data attribute to undefined removes it from dom element
+     */
+    testRemoveDataAttribute: {
+        test : function(cmp){
+            var div = cmp.find("aDiv").getElement();
+            $A.test.assertTrue(div.hasAttribute("data-test-data"), "Data attribute not present on div");
+            $A.util.setDataAttribute(div, "testData", undefined);
+            $A.test.assertFalse(div.hasAttribute("data-test-data"), "Setting data attribute with undefined value " +
+                "should remove the attribute");
+        }
+    },
+
+    testDataAttributeInvalidNodeType: {
+        test : function(cmp){
+            var textNode = cmp.find("aDiv").getElement().childNodes[0];
+            $A.util.setDataAttribute(textNode, "monkey", "shouldntWork");
+            $A.test.assertNull($A.util.getDataAttribute(textNode, "monkey"), "Should not be able to set data " +
+                "attributes on text nodes");
+        }
     }
 })

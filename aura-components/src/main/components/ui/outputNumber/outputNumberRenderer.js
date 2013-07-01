63824196749f367e3a47155f2b7ba0b2ba22dc27
Jumbo Jesus <jmooney@salesforce.com>
2013-07-01 10:18:15 -0700

    safer renderer for number

--- aura-components/src/main/components/ui/outputNumber/outputNumberRenderer.js
+++ aura-components/src/main/components/ui/outputNumber/outputNumberRenderer.js
@@ -4,31 +4,7 @@
         var f = cmp.get("v.format");
         var num = cmp.get("v.value");
         var formatted;
-        if (!$A.util.isEmpty(f)) {
-            var nf;
-            try {
-                nf = $A.localizationService.getNumberFormat(f);
-            } catch (e) {
-                formatted = "Invalid format attribute";
-                $A.log(e);
-            }
-            if (nf) {
-                formatted = nf.format(num);
-            }
-        } else {
-            formatted = $A.localizationService.formatNumber(num);
-        }
-        span.innerText = formatted;
-        return span;
-    },
-
-    rerender: function outputNumberRerenderer(cmp, helper) {
-        var val = cmp.getValue("v.value");
-        var f = cmp.getValue("v.format");
-        if (val.isDirty() || f.isDirty()) {
-            var formatted;
-            f = f.unwrap();
-            val = val.unwrap();
+        if (!$A.util.isUndefinedOrNull(num)) {
             if (!$A.util.isEmpty(f)) {
                 var nf;
                 try {
@@ -38,12 +14,41 @@
                     $A.log(e);
                 }
                 if (nf) {
-                    formatted = nf.format(val);
+                    formatted = nf.format(num);
                 }
             } else {
-                formatted = $A.localizationService.formatNumber(val);
+                formatted = $A.localizationService.formatNumber(num);
             }
-            cmp.getElement().innerText = formatted;
+            span.innerText = formatted;
+        }
+        return span;
+    },
+
+    rerender: function outputNumberRerenderer(cmp, helper) {
+        var val = cmp.getValue("v.value");
+        var f = cmp.getValue("v.format");
+        if (val.isDirty() || f.isDirty()) {
+            var formatted = '';
+            f = f.unwrap();
+            val = val.unwrap();
+            if (!$A.util.isUndefinedOrNull(val)) {
+                if (!$A.util.isEmpty(f)) {
+                    var nf;
+                    try {
+                        nf = $A.localizationService.getNumberFormat(f);
+                    } catch (e) {
+                        formatted = "Invalid format attribute";
+                        $A.log(e);
+                    }
+                    if (nf) {
+                        formatted = nf.format(val);
+                    }
+                } else {
+                    formatted = $A.localizationService.formatNumber(val);
+                }
+            }
+            var span = cmp.find("span");
+            span.getElement().innerText = formatted;
         }
     }
 })

07285bec13870715677062cda2105c5554a03a5d
gtorressfdc <gtorres@salesforce.com>
2013-07-03 09:30:12 -0700

    retry WebDriver initialization @rev rjayaramappa@

--- aura/src/test/java/org/auraframework/test/RemoteWebDriverFactory.java
+++ aura/src/test/java/org/auraframework/test/RemoteWebDriverFactory.java
@@ -16,6 +16,9 @@
 package org.auraframework.test;
 
 import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -28,6 +31,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
  * @since 0.0.178
  */
 public class RemoteWebDriverFactory implements WebDriverProvider {
+    protected final int MAX_GET_RETRIES = 3;
     protected final URL serverUrl;
 
     public RemoteWebDriverFactory(URL serverUrl) {
@@ -35,8 +39,24 @@ public class RemoteWebDriverFactory implements WebDriverProvider {
     }
 
     @Override
-    public WebDriver get(DesiredCapabilities capabilities) {
-        return new RemoteWebDriver(serverUrl, capabilities);
+    public WebDriver get(final DesiredCapabilities capabilities) {
+        return retry(new Callable<WebDriver>() {
+            @Override
+            public WebDriver call() throws Exception {
+                return new RemoteWebDriver(serverUrl, capabilities);
+            }
+        }, MAX_GET_RETRIES, "Failed to get a new RemoteWebDriver");
+    }
+
+    protected <T> T retry(Callable<T> callable, int retries, String msg) {
+        for (int i = 0; i < retries; i++) {
+            try {
+                return callable.call();
+            } catch (Throwable t) {
+                Logger.getLogger(getClass().getName()).log(Level.WARNING, msg, t);
+            }
+        }
+        throw new Error(msg + String.format(" after %s tries", retries));
     }
 
     @Override

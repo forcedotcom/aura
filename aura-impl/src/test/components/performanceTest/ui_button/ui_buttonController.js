0764b1d1ce9fe8e15e91ef554dcede9be81faa25
tluo <tluo@salesforce.com>
2013-07-16 10:39:52 -0700

    add perf test component for ui:button

---
+++ aura-impl/src/test/components/performanceTest/ui_button/ui_buttonController.js
@@ -0,0 +1,11 @@
+({
+	changeLabel:function(cmp, event) {
+        var button = cmp.find("button1");
+        var mark="ui button label change time";
+        //Set the text value in the ui:outputText component
+         $A.mark(mark);
+          button.getValue("v.label").setValue("clicked");
+         $A.endMark(mark);
+	}
+
+})

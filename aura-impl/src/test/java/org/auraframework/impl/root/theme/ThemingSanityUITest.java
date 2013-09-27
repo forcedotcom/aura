28e9109de8c50b032e44ae0d30c72168a529a3dd
Lokesh Jagasia <ljagasia@salesforce.com>
2013-09-27 15:41:38 -0700

    Fixing ThemingSanity test failures to pass in all browsers

--- aura-impl/src/test/java/org/auraframework/impl/root/theme/ThemingSanityUITest.java
+++ aura-impl/src/test/java/org/auraframework/impl/root/theme/ThemingSanityUITest.java
@@ -16,16 +16,16 @@
 package org.auraframework.impl.root.theme;
 
 import org.auraframework.test.WebDriverTestCase;
+import org.openqa.selenium.By;
 
 /**
- * Tests that verify minimum required functionality of Aura Theming.
- * Basic sanity testing of various flows. As phase 2 is built out more tests will be added here.
+ * Tests that verify minimum required functionality of Aura Theming. Basic sanity testing of various flows. As phase 2
+ * is built out more tests will be added here.
  * 
  * @author ljagasia
- *
  */
 public class ThemingSanityUITest extends WebDriverTestCase {
-    public ThemingSanityUITest (String name) {
+    public ThemingSanityUITest(String name) {
         super(name);
     }
 
@@ -33,41 +33,75 @@ public class ThemingSanityUITest extends WebDriverTestCase {
      * Using theme variables in CSS
      */
     public void testThemeVariablesInCSS() throws Exception {
+        final String expectedBgColor = "rgba(0, 128, 0, 1)"; // green
+        final String expectedFgColor = "rgba(255, 0, 0, 1)"; // red
+        final String expectedHeight = "300px";
+
         open("/themeTest/themeVariable.cmp");
+        String actualBgColor = getDriver().findElement(By.cssSelector(".themeTestHeader")).getCssValue(
+                "background-color");
+        String actualFgColor = getDriver().findElement(By.cssSelector(".themeTestHeader")).getCssValue("color");
+        String actualHeight = getDriver().findElement(By.cssSelector(".themeTestHeader")).getCssValue("height");
 
-        String pageSource = getDriver().getPageSource();
-        String sourceNoWhitespace = pageSource.replaceAll("\\s", "");
-        String expectedCss = ".themeTestHeader{width:100%;height:300px;color:red;background-color:green}";
-        assertTrue("Loaded app does not have template css present.", sourceNoWhitespace.contains(expectedCss));
+        assertTrue("Loaded app should have template css present for background-color.",
+                expectedBgColor.equalsIgnoreCase(actualBgColor));
+        assertTrue("Loaded app should have template css present for color.",
+                expectedFgColor.equalsIgnoreCase(actualFgColor));
+        assertTrue("Loaded app should have template css present for height.",
+                expectedHeight.equalsIgnoreCase(actualHeight));
     }
-    
-    /**
-     * Verify using theme variables across multiple namepaces 
-     * @throws Exception 
-     */
-    public void testThemeVariablesAcrossNamespaces() throws Exception{
-    	open("/themeTest/themeAcrossNamespaces.cmp");
 
-        String pageSource = getDriver().getPageSource();
-        String sourceNoWhitespace = pageSource.replaceAll("\\s", "");
-        String expectedCss = ".themeTestThemeAcrossNamespaces{margin:10px;font-family:Arial,sans-serif;background:#bbbbbb}";
-        assertTrue("Loaded app does not have template css present.", sourceNoWhitespace.contains(expectedCss));
-    }
-    
     /**
-     * Verify theme extension.
+     * Verify using theme variables across multiple namepaces
+     * 
      * @throws Exception
      */
-    public void testThemeExtension() throws Exception{
-    	open("/themeTest/themeExtension.cmp");
+    public void testThemeVariablesAcrossNamespaces() throws Exception {
+        final String expectedFontFamily = "arial,sans-serif";
 
-        String pageSource = getDriver().getPageSource();
-        String sourceNoWhitespace = pageSource.replaceAll("\\s", "");
-        String expectedCss1 = ".themeTestHeader{width:100%;height:300px;color:red;background-color:green}";
-        String expectedCss2 = ".themeTestHeader2{color:yellow;background-color:green}";
-        assertTrue("Loaded app does not have template css present.", sourceNoWhitespace.contains(expectedCss1));
-        assertTrue("Loaded app does not have template css present.", sourceNoWhitespace.contains(expectedCss2));
+        open("/themeTest/themeAcrossNamespaces.cmp");
+        String actualFontFamily = getDriver().findElement(By.cssSelector(".themeTestThemeAcrossNamespaces"))
+                .getCssValue("font-family").replaceAll("\\s", "");
+        assertTrue("Loaded app should have template css present for font family",
+                expectedFontFamily.equalsIgnoreCase(actualFontFamily));
     }
-    
-    
+
+    /**
+     * Verify theme extension.
+     * 
+     * @throws Exception
+     */
+    public void testThemeExtension() throws Exception {
+        // base theme
+        final String expectedHeight1 = "300px";
+        final String expectedColor1 = "rgba(255, 0, 0, 1)"; // red
+        final String expectedBgColor1 = "rgba(0, 128, 0, 1)"; // green
+
+        // overridden child theme
+        final String expectedColor2 = "rgba(255, 255, 0, 1)"; // yellow
+        final String expectedBgColor2 = "rgba(0, 128, 0, 1)"; // green
+
+        open("/themeTest/themeExtension.cmp");
+
+        String actualHeight1 = getDriver().findElement(By.cssSelector(".themeTestHeader")).getCssValue("height");
+        String actualColor1 = getDriver().findElement(By.cssSelector(".themeTestHeader")).getCssValue("color");
+        String actualBgColor1 = getDriver().findElement(By.cssSelector(".themeTestHeader")).getCssValue(
+                "background-color");
+
+        String actualColor2 = getDriver().findElement(By.cssSelector(".themeTestHeader2")).getCssValue("color");
+        String actualBgColor2 = getDriver().findElement(By.cssSelector(".themeTestHeader2")).getCssValue(
+                "background-color");
+
+        assertTrue("Loaded app should have base theme applied for height",
+                expectedHeight1.equalsIgnoreCase(actualHeight1));
+        assertTrue("Loaded app should have base theme applied for color", expectedColor1.equalsIgnoreCase(actualColor1));
+        assertTrue("Loaded app should have base theme applied for background-color",
+                expectedBgColor1.equalsIgnoreCase(actualBgColor1));
+
+        assertTrue("Loaded app should have overridden child theme applied for color",
+                expectedColor2.equalsIgnoreCase(actualColor2));
+        assertTrue("Loaded app should have overridden child theme applied for background-color",
+                expectedBgColor2.equalsIgnoreCase(actualBgColor2));
+    }
+
 }

ff392b69f535cdde3d989eddb7425b60601d9f07
Andrew McCulloch <amcculloch@salesforce.com>
2013-10-16 13:59:37 -0700

    Remove JMX stats from logging @rev dchasman@ @bug 1900162@

--- aura-impl/src/main/java/org/auraframework/impl/context/LoggingContextImpl.java
+++ aura-impl/src/main/java/org/auraframework/impl/context/LoggingContextImpl.java
@@ -15,24 +15,12 @@
  */
 package org.auraframework.impl.context;
 
-import java.lang.management.ManagementFactory;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-
-import javax.management.MBeanServer;
-import javax.management.MXBean;
-import javax.management.ObjectName;
 
 import org.apache.log4j.Logger;
 import org.auraframework.system.LoggingContext;
-import org.auraframework.throwable.AuraRuntimeException;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 
 /**
  * LoggingContext impl.
@@ -41,9 +29,6 @@ import com.google.common.collect.Sets;
 public class LoggingContextImpl implements LoggingContext {
 
     protected static final Logger logger = Logger.getLogger("LoggingContextImpl");
-    private static final LoadingCache<String, Counter> counters = CacheBuilder.newBuilder()
-            .build(new CounterComputer());
-    private static final MBeanServer server = ManagementFactory.getPlatformMBeanServer();
 
     private final Map<String, Object> loggingValues = Maps.newHashMap();
     private final Map<String, Timer> timers = Maps.newHashMap();
@@ -129,45 +114,11 @@ public class LoggingContextImpl implements LoggingContext {
      */
     @Override
     public void log() {
-        Set<String> names = Sets.newHashSet();
-
         for (Map.Entry<String, Timer> entry : timers.entrySet()) {
-            String key = entry.getKey();
-            // As counters cannot handle ":", we have to replace it with
-            // something else .
-            String counterKey = replaceSpecialChars(key);
-            try {
-                counters.get(counterKey).setValue(entry.getValue().getTime());
-            } catch (ExecutionException e) {
-                throw new AuraRuntimeException(e);
-            }
-            names.add(counterKey);
-            loggingValues.put(key, entry.getValue().getTime());
+            loggingValues.put(entry.getKey(), entry.getValue().getTime());
         }
-
-        for (Map.Entry<String, Long> entry : nums.entrySet()) {
-            String key = entry.getKey();
-            // As counters cannot handle ":", we have to replace it with
-            // something else .
-            String counterKey = replaceSpecialChars(key);
-            try {
-                counters.get(counterKey).setValue(entry.getValue());
-            } catch (ExecutionException e) {
-                throw new AuraRuntimeException(e);
-            }
-            names.add(counterKey);
-            loggingValues.put(key, entry.getValue());
-        }
-
-        for (Map.Entry<String, Object> entry : values.entrySet()) {
-            loggingValues.put(entry.getKey(), entry.getValue());
-        }
-
-        for (Map.Entry<String, Counter> entry : counters.asMap().entrySet()) {
-            if (!names.contains(entry.getKey())) {
-                entry.getValue().setValue(0);
-            }
-        }
+        loggingValues.putAll(nums);
+        loggingValues.putAll(values);
         log(loggingValues);
     }
 
@@ -188,94 +139,6 @@ public class LoggingContextImpl implements LoggingContext {
             }
         }
         logger.info(buffer);
-    }
-
-    /**
-     * Replace special characters of JMX.
-     * 
-     */
-    private String replaceSpecialChars(String str) {
-        String regex = "[\\*\\?\\,\\:\n]";
-        return str.replaceAll(regex, "-");
-    }
-
-    @MXBean
-    public static interface CounterMXBean {
-
-        public long getCount();
-
-        public long getMostRecentValue();
-
-        public double getMeanValue();
-
-        public long getMaxValue();
-
-        public long getMinValue();
-
-        public long getTotalValue();
-
-        public void reset();
-    }
-
-    public static class Counter implements CounterMXBean {
-
-        private long mostRecentValue = 0;
-        private long totalValue = 0;
-        private long count = 0;
-        private long maxValue = 0;
-        private long minValue = 0;
-
-        @Override
-        public long getCount() {
-            return count;
-        }
-
-        @Override
-        public long getMostRecentValue() {
-            return mostRecentValue;
-        }
-
-        @Override
-        public double getMeanValue() {
-            return count == 0 ? 0 : (totalValue / count);
-        }
-
-        @Override
-        public long getMaxValue() {
-            return maxValue;
-        }
-
-        @Override
-        public long getMinValue() {
-            return minValue;
-        }
-
-        @Override
-        public void reset() {
-            count = 0;
-            maxValue = 0;
-            minValue = 0;
-            mostRecentValue = 0;
-            totalValue = 0;
-        }
-
-        @Override
-        public long getTotalValue() {
-            return totalValue;
-        }
-
-        public void setValue(long value) {
-            mostRecentValue = value;
-            totalValue += value;
-            if (count == 0 || value < minValue) {
-                minValue = value;
-            }
-            if (count == 0 || value > maxValue) {
-                maxValue = value;
-            }
-            count++;
-        }
-
     }
     
     private static class KVLogger implements KeyValueLogger {
@@ -347,25 +210,6 @@ public class LoggingContextImpl implements LoggingContext {
             startTime = -1;
             totalTime = -1;
         }
-    }
-
-    private static final class CounterComputer extends CacheLoader<String, Counter> {
-
-        @Override
-        public Counter load(String name) {
-
-            Counter counter = new Counter();
-
-            ObjectName objectName;
-            try {
-                objectName = new ObjectName("aura", "name", name);
-                server.registerMBean(counter, objectName);
-            } catch (Exception e) {
-                throw new AuraRuntimeException(e);
-            }
-            return counter;
-        }
-
     }
 
 }

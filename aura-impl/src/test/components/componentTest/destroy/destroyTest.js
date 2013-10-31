4f78d09da6c7697fbc17f7616ac02410da219f80
Trevor Bliss <tbliss@salesforce.com>
2013-10-31 15:42:23 -0700

    Test for Component.destroy(async=true)
    Right now we force destroy() to be synchronous in test modes so this test case will need to remain disabled until we stop making exceptions for test modes.
    
    @bug W-1883677@
    @rev gtorres@

--- aura-impl/src/test/components/componentTest/destroy/destroyTest.js
+++ aura-impl/src/test/components/componentTest/destroy/destroyTest.js
@@ -19,55 +19,93 @@
      * Each level of facet evaluating v.body in its markup.
      */
     testDestroyOnChainedFacet:{
-	test:[
-	      function(cmp){//Before Destroy
-		  $A.test.assertDefined(cmp.find("outerFacet"));
-		  $A.test.assertDefined(cmp.find("innerFacet1"));
-		  $A.test.assertDefined(cmp.find("innerFacet2"));
-		  $A.test.assertEquals(5, cmp.find("team").getElement().childNodes.length);
-	      },function(cmp){//Destroy
-		  var outerFacet = cmp.find("outerFacet");
-		  try{
-		      outerFacet.destroy(false);
-		  }catch(e){
-		      $A.test.fail("Component destroy() failed destroying chained facets with body:"+e)
-		  }
-	      },function(cmp){//After Destroy
-		  $A.test.assertUndefinedOrNull(cmp.find("outerFacet"));
-		  $A.test.assertUndefinedOrNull(cmp.find("textInOuterFacet"));
-		  $A.test.assertUndefinedOrNull(cmp.find("bullPen"));
-		  
-		  $A.test.assertUndefinedOrNull(cmp.find("innerFacet1"));
-		  $A.test.assertUndefinedOrNull(cmp.find("textInInnerFacet"));
-		  $A.test.assertUndefinedOrNull(cmp.find("players"));
-		  
-		  $A.test.assertUndefinedOrNull(cmp.find("innerFacet2"));
-		  $A.test.assertUndefinedOrNull(cmp.find("coach"));
-		  
-		  $A.test.assertEquals(0, cmp.find("team").getElement().childNodes.length);
-	      }
-	      ]
+        test:[ function(cmp){
+            //Before Destroy
+            $A.test.assertDefined(cmp.find("outerFacet"));
+            $A.test.assertDefined(cmp.find("innerFacet1"));
+            $A.test.assertDefined(cmp.find("innerFacet2"));
+            $A.test.assertEquals(5, cmp.find("team").getElement().childNodes.length);
+        },function(cmp){
+            //Destroy
+            var outerFacet = cmp.find("outerFacet");
+            try{
+                outerFacet.destroy(false);
+            }catch(e){
+                $A.test.fail("Component destroy() failed destroying chained facets with body:"+e)
+            }
+        },function(cmp){
+            //After Destroy
+            this.verifyComponentDestroyed(cmp);
+        }]
     },
+
     /**
      * Verify that destroy can handle reference loops in facets.
      * Cause a reference loop by making a facet aware of its parent using an attribute on the facet.
      * W-1584816
      */
     testDestroyOnParentAwareFacet:{
-	test:[
-	      function(cmp){
-		  $A.test.assertDefined(cmp.find("informFacet"));
-		  $A.test.assertDefined(cmp.find("knowParent"));
-	      },function(cmp){
-        	    var facet = cmp.find("informFacet");
-        	    try{
-        		facet.destroy()
-        	    }catch(e){
-        		$A.test.fail("Component destroy() failed to handle reference loops in facet:"+e)
-        	    }
-	      },function(cmp){
-		  $A.test.assertUndefinedOrNull(cmp.find("informFacet"));
-		  $A.test.assertUndefinedOrNull(cmp.find("knowParent"));
-	      }]
+        test:[ function(cmp){
+            $A.test.assertDefined(cmp.find("informFacet"));
+            $A.test.assertDefined(cmp.find("knowParent"));
+        },function(cmp){
+            var facet = cmp.find("informFacet");
+            try{
+                facet.destroy(false);
+            }catch(e){
+                $A.test.fail("Component destroy() failed to handle reference loops in facet:"+e)
+            }
+        },function(cmp){
+            $A.test.assertUndefinedOrNull(cmp.find("informFacet"));
+            $A.test.assertUndefinedOrNull(cmp.find("knowParent"));
+        }]
+    },
+
+    // TODO: Re-enable when we stop forcing destroy to be synchronous in test modes. This is done in the destroy
+    //       functions of Component.js and ArrayValue.js.
+    // W-1928349: When we destroy asynchronously we leave a reference node behind, but not when we destroy
+    //            synchronously. If the reference node only for async is expected then logic in 
+    //            verifyComponentDestroyed will need to change.
+    _testDestroyAsync: {
+        test: function(cmp) {
+            $A.test.assertDefined(cmp.find("outerFacet"));
+            $A.test.assertDefined(cmp.find("players"));
+            // on load the 'players' div should not have it's display property set
+            var playersDisplay = cmp.find("players").getElement().style.display;
+            $A.test.assertEquals("", playersDisplay);
+
+            try{
+                cmp.find("outerFacet").destroy(true);
+            }catch(e){
+                $A.test.fail("Component.destroy(true) failed: " + e);
+            }
+
+            // 'players' div should still exist, but the style.display will be set to 'none' while we destroy
+            $A.test.assertDefined(cmp.find("players"));
+            playersDisplay = cmp.find("players").getElement().style.display;
+            $A.test.assertEquals("none", playersDisplay);
+
+            var component = cmp;
+            $A.test.addWaitFor(
+                true,
+                function() { return $A.util.isUndefinedOrNull(component.find("outerFacet")); },
+                function() { this.verifyComponentDestroyed(cmp); }
+            );
+        }
+    },
+
+    verifyComponentDestroyed : function(cmp) {
+        $A.test.assertUndefinedOrNull(cmp.find("outerFacet"));
+        $A.test.assertUndefinedOrNull(cmp.find("textInOuterFacet"));
+        $A.test.assertUndefinedOrNull(cmp.find("bullPen"));
+
+        $A.test.assertUndefinedOrNull(cmp.find("innerFacet1"));
+        $A.test.assertUndefinedOrNull(cmp.find("textInInnerFacet"));
+        $A.test.assertUndefinedOrNull(cmp.find("players"));
+
+        $A.test.assertUndefinedOrNull(cmp.find("innerFacet2"));
+        $A.test.assertUndefinedOrNull(cmp.find("coach"));
+
+        $A.test.assertEquals(0, cmp.find("team").getElement().childNodes.length);
     }
 })

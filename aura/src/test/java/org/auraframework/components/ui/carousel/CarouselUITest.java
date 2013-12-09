c319ef122b8668351ced1f923561fe3f656ed2d6
ctatlah <ctatlah@salesforce.com>
2013-12-09 15:07:03 -0800

    CarouselUITest flapper fix. @bug W-1851013@ @rev mkohanfars@later
    Fix flapping test
    @bug W-1851013@
    @rev mkohanfars@later

--- aura/src/test/java/org/auraframework/components/ui/carousel/CarouselUITest.java
+++ aura/src/test/java/org/auraframework/components/ui/carousel/CarouselUITest.java
@@ -25,6 +25,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 /**
  * Tests to verify ui:carousel keyboard interactions and UI.
@@ -51,18 +52,19 @@ public class CarouselUITest extends WebDriverTestCase {
     /**
      * Able to tab into a page on the carousel.
      */
-    //TODO: Renable after W-1851013 is fixed. Currently this test flaps.
-    public void _testTabIntoCarouselPage() throws Exception {
+    public void testTabIntoCarouselPage() throws Exception {
         open(URL);
         WebDriver driver = getDriver();
         WebElement carousel = getCarousel(driver, 1);
         WebElement page = getPageOnCarousel(carousel, 2);
 
         // setup
+        waitForCarouselPageSelected(page);
         WebElement navElement = getNavigationItemSelected(carousel);
         navElement.click();
         assertEquals("Navigation bar element should be in focus.",
                 navElement.getAttribute(AURA_RENDERED_BY_ID), auraUITestingUtil.getUniqueIdOfFocusedElement());
+        waitForCarouselPageSelected(page);
 
         // tab into carousel page
         auraUITestingUtil.pressTab(navElement);
@@ -277,4 +279,13 @@ public class CarouselUITest extends WebDriverTestCase {
         return pageItems.get(--entityNum).findElement(By.tagName("a")); // return tab-able element
     }
 
+    public void waitForCarouselPageSelected(final WebElement page) {
+        auraUITestingUtil.waitUntil(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver d) {
+                String cssClass = page.getAttribute("class");
+                return cssClass.contains("carousel-page-selected");
+            }
+        }, timeoutInSecs);
+    }
 }

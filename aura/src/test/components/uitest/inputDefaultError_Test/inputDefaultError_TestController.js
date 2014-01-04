c4c1a59f852e0aeb3419b5c61bbeb5b24e09f355
Michael <mkohanfars@salesforce.com>
2014-01-03 16:51:51 -0800

    Tests for inputDefaultError and modification to Test.js, adding in new assert statement @rev ctatlah@ @bug W-1688562@

---
+++ aura/src/test/components/uitest/inputDefaultError_Test/inputDefaultError_TestController.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ ({
+     
+     doInit : function (cmp, evt, helper){
+	 //Get actual value
+	 var num = ""+cmp.find("randNumInput").get("v.value");
+	 //Get SimpleValue object to modify
+	 var value = cmp.find("randNumInput").getValue("v.value");
+	 
+	 if (num.length < 5 || num.length>7) {
+	     value.setValid(false);
+	     value.addErrors({"message":"Number should have (5,7] digits"})
+	 }
+	 
+	 //Check integer starts with 1;
+	 if(num[0] != "1"){
+	     value.setValid(false);
+	     value.addErrors({"message":"The number given should start with a 1"});
+	 }
+     }
+ })
---
+++ aura/src/test/components/uitest/inputDefaultError_Test/inputDefaultError_TestTest.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+({ 
+
+    //Test checking for to make sure that the aria-describedby attribute is set to the empty string when no errors present
+    testForEmptyAttribute : {
+	attributes : {val : "1000000"},
+        test:function(cmp){
+            var inputValue = $A.test.getElementAttributeValue(cmp.find("randNumInput").getElement(),"aria-describedby");
+            $A.test.assertNotUndefinedOrNull(inputValue, "aria-describedby attribute does not exist on input component");
+            $A.test.assertTrue($A.util.isEmpty(inputValue), "The initial value of aria-describedby attribute should be the empty string");
+            
+        }
+    },
+    //Test checking for to make sure that the aria-describedby attribute is set correctly when errors are present
+    testForNonEmpty : {
+	attributes : {val : "21"},
+        test:function(cmp){
+            var ul = $A.test.getElementByClass("uiInputDefaultError");
+            $A.test.assertEquals(ul.length, 1, "uiInputDefaultError unordered list was not found");
+            $A.test.assertEquals(ul[0].children.length, 2, "The unordered list has to many items in it");
+            
+        }
+    } 
+
+})

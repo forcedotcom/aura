e77fc6355fe4bb24cfe4293f353e637406ebddd3
James Troup <jtroup@salesforce.com>
2014-02-12 15:38:15 -0800

    Fix AuraServletHttpTest.testGetUnhandledError

--- aura/src/test/java/org/auraframework/http/AuraServletHttpTest.java
+++ aura/src/test/java/org/auraframework/http/AuraServletHttpTest.java
@@ -335,7 +335,7 @@ public class AuraServletHttpTest extends AuraHttpTestCase {
 
     public void testGetUnhandledError() throws Exception {
         DefDescriptor<ApplicationDef> desc = addSourceAutoCleanup(ApplicationDef.class,
-                "<aura:application><aura:attribute type='bah'/></aura:application>");
+                "<aura:application><aura:attribute name='bah'/></aura:application>");
         HttpGet get = obtainGetMethod(String.format("/%s/%s.app", desc.getNamespace(), desc.getName()));
         HttpResponse httpResponse = perform(get);
         assertEquals(HttpStatus.SC_OK, getStatusCode(httpResponse));

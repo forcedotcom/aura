a94a1c20c3759fc9fd76c59d3a971769eff63162
Freeland Abbott <fabbott@salesforce.com>
2014-06-02 14:25:23 -0400

    Adding connect-src to CSP headers
    @rev amcculloch@
    @bug W-2234931@

--- aura/src/test/java/org/auraframework/test/IntegrationTestCase.java
+++ aura/src/test/java/org/auraframework/test/IntegrationTestCase.java
@@ -20,9 +20,7 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
+import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -33,11 +31,11 @@ import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.auraframework.Aura;
-import org.auraframework.def.BaseComponentDef;
-import org.auraframework.def.DefDescriptor;
+import org.auraframework.def.*;
 import org.auraframework.def.DefDescriptor.DefType;
+import org.auraframework.http.CSP;
 import org.auraframework.service.ContextService;
-import org.auraframework.system.AuraContext;
+import org.auraframework.system.*;
 import org.auraframework.system.AuraContext.Authentication;
 import org.auraframework.system.AuraContext.Format;
 import org.auraframework.system.AuraContext.Mode;
@@ -144,7 +142,20 @@ public abstract class IntegrationTestCase extends AuraTestCase {
      * @throws Exception
      */
     protected HttpResponse perform(HttpRequestBase method, HttpContext context) throws Exception {
-        return getHttpClient().execute(method, context);
+        HttpResponse response = getHttpClient().execute(method, context);
+        Header cspHeaders[] = response.getHeaders(CSP.Header.REPORT_ONLY);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            // TODO(fabbott): Although a request for e.g. moment.js from testSetRunner.app
+            // does have a header, the same request from AuraFrameworkServletHttpTest does
+            // not.  I suspect this is because the test has no UID, but the "real life" one
+            // does... but for now, let's validate the CSP header only if it's actually there.
+            if (cspHeaders.length != 0) {
+                assertEquals(1, cspHeaders.length);
+                assertTrue("No connect-src in default CSP",
+                        cspHeaders[0].getValue().contains("; connect-src 'self';"));
+            }
+        }
+        return response;
     }
 
     /**

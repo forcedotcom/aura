6fcbce288fcd5d1e32a279c76fd7c24059180d37
John Huang <john.huang@salesforce.com>
2014-06-11 16:42:30 -0700

    ui:tabset examples

---
+++ aura-components/src/main/components/uiExamples/tabsetExample/tabsetExampleModel.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+({
+    tabs: [{
+        "title": "Tab 1 from model",
+        "body":  [{
+            "componentDef": { descriptor:"markup://aura:text" },
+            "attributes": {
+                "values": {
+                    "value": "Tab 1 content"
+                }
+            }
+        }]
+    },
+    {
+        "title": "Tab 2 from model",
+        "active": true,
+        "body":  [{
+            "componentDef": { descriptor:"markup://aura:text" },
+            "attributes": {
+                "values": {
+                    "value": "Tab 2 content"
+                }
+            }
+        }]
+    },
+    {
+        "title": "Tab 3 from model",
+        "body":  [{
+            "componentDef": { descriptor:"markup://aura:text" },
+            "attributes": {
+                "values": {
+                    "value": "Tab 3 content"
+                }
+            }
+        }]
+    }
+    ]
+})

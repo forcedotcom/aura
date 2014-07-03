164596a01d6190ff5a2902c350d7f125efec3add
Tristan <tmoss@salesforce.com>
2014-07-03 15:19:39 -0700

    Allows chaining off of a promise that is already resolved. Previously, a promise chained to an already executed promise would never be executed.
    Allows promise chain insertion when a promise that is a member of a chain returns a new promise. Previously the returned promise would not be added to the chain and would not be executed.
    
    @rev skrenzel@
    @rev dmcmillen@
    @bug W-2217782@
    
    Code Review feedback 1
    
    ===
    
    Allows chaining off of a promise that is already resolved. Previously, a promise chained to an already executed promise would never be executed.
    
    Allows promise chain insertion when a promise that is a member of a chain returns a new promise. Previously the returned promise would not be added to the chain and would not be executed.
    
    @rev skrenzel@
    @rev dmcmillen@
    @bug W-2217782@
    
    Code Review feedback 1
    
    ===
    
    Allows chaining off of a promise that is already resolved. Previously, a promise chained to an already executed promise would never be executed.
    
    Allows promise chain insertion when a promise that is a member of a chain returns a new promise. Previously the returned promise would not be added to the chain and would not be executed.
    
    @rev skrenzel@
    @rev dmcmillen@
    @bug W-2217782@

--- aura-impl/src/test/components/utilTest/promise/promiseTest.js
+++ aura-impl/src/test/components/utilTest/promise/promiseTest.js
@@ -111,18 +111,152 @@
 				p = $A.util.createPromise(function (success, error) {
 					success(42);
 				});
-			
 			p.then(function (r) { return r + 2; })
 				.then(function (r) { return r / 2; })
 				.then(function (r) { result = r; });
 
 			$A.test.addWaitFor(22, function () { return result; });			
 		}]
-	}, 
+	},
 
-	/**
+    /**
+     * Tests that when execution of a thenable returns a thenable with *synchronous* content,
+     * that returned thenable is inserted into the thenable chain.
+     */
+    testChainInsertionSync: {
+
+        test: [function () {
+
+            var result = '';
+            var promise = $A.util.createPromise(function(success, error) {
+                success('A');
+            });
+
+            promise
+                .then(function(r) { return r += 'B'; })
+                .then(function(r) {
+                    return { then: function(success, error) { success(r += 'C'); }}
+                })
+                .then(function(r) { return r  += 'D'; })
+                .then(
+                    function(r) { result = r; },
+                    function(err) { result = err; }
+                );
+
+            $A.test.addWaitFor('ABCD', function() { return result; })
+        }]
+    },
+
+    /**
+     * Tests that when execution of a thenable returns a thenable with *asynchronous* content,
+     * that returned thenable is inserted into the thenable chain.
+     */
+    testChainInsertionAsync: {
+
+        test: [function () {
+
+            var result = '';
+            var promise = $A.util.createPromise(function(success, error) {
+                success('A');
+            });
+
+            promise
+                .then(function(r) { return r += 'B'; })
+                .then(function(r) {
+                    return $A.util.createPromise(function(success, error) {
+                        setTimeout(function() { success(r += 'C'); }, 1);
+                    })
+                })
+                .then(function(r) { return r  += 'D'; })
+                .then(
+                function(r) { result = r; },
+                function(err) { result = err; }
+            );
+
+            $A.test.addWaitFor('ABCD', function() { return result; })
+        }]
+    },
+
+    /**
+     * Tests that when execution of a thenable returns a thenable with *synchronous* content,
+     * and that content errors, the returned thenable is inserted into the thenable chain and
+     * the error cascades down the thenable chain.
+     */
+    testChainInsertionSyncError: {
+
+        test: [function () {
+
+            var result = '';
+            var promise = $A.util.createPromise(function(success, error) {
+                success('A');
+            });
+
+            promise
+                .then(function(r) { return r += 'B'; })
+                .then(function(r) {
+                    return { then: function(success, error) { error("error"); }}
+                })
+                .then(function(r) { return r  += 'D'; })
+                .then(
+                function(r) { result = r; },
+                function(err) { result = err; }
+            );
+
+            $A.test.addWaitFor('error', function() { return result; })
+        }]
+    },
+
+    /**
+     * Tests that when execution of a thenable returns a thenable with *asynchronous* content,
+     * and that content errors, the returned thenable is inserted into the thenable chain and
+     * the error cascades down the thenable chain.
+     */
+    testChainInsertionAsyncError: {
+
+        test: [function () {
+
+            var result = '';
+            var promise = $A.util.createPromise(function(success, error) {
+                success('A');
+            });
+
+            promise
+                .then(function(r) { return r += 'B'; })
+                .then(function(r) {
+                    return $A.util.createPromise(function(success, error) {
+                        setTimeout(function() { error("error"); }, 1);
+                    })
+                })
+                .then(function(r) { return r  += 'D'; })
+                .then(
+                function(r) { result = r; },
+                function(err) { result = err; }
+            );
+
+            $A.test.addWaitFor('error', function() { return result; })
+        }]
+    },
+
+    /**
+     * Tests that a promise can asynchronously execute asynchronous code.
+     */
+    testAsyncContent: {
+
+        test: [function () {
+
+            result = '';
+
+            p = $A.util.createPromise(function(success, error) {
+                setTimeout(function() { result = 'done'; }, 1);
+            });
+
+            $A.test.addWaitFor('done', function () { return result; });
+        }]
+    },
+
+    /**
 	 * Tests that an error within the promise execution is resolved to the
-	 * the chained promsied. 
+	 * the chained promised.
 	 */
 	testChainedResolutionRejection: {
 

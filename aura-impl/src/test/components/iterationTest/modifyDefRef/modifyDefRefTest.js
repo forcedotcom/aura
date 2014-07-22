76d2fa3a3b81ee66235e2604c357025839d42b2d
Derrick <dmcmillen@salesforce.com>
2014-07-22 01:38:43 -0700

    Preventing newComponentAsync from attaching a valueProvider directly to component configs. @bug W-2319787@ @rev byao@

---
+++ aura-impl/src/test/components/iterationTest/modifyDefRef/modifyDefRefTest.js
@@ -0,0 +1,22 @@
+({
+	/**
+	 * aura:iteration will inevitably modify the shape of the ComponentDefRef provided as its body.
+	 * It does this to maintain the "closure" nature provided by the Aura "templating engine".
+	 * 
+	 * This test verifies that the original ComponentDefRef remain untouched (attributes is correctly protected).
+	 */
+	testNoChangesToDefRef: {
+		
+		attributes: {
+			items : ['Foo', 'Bar', 'Baz']
+		},
+		
+		test: function (cmp) {
+			var cdr = cmp.get('v.cdr');
+			
+			// Check for the PassthroughValue appended by aura:iteration. It should NOT be there.
+			$A.test.assertUndefinedOrNull(cdr[0].attributes.valueProvider, "Iteration should not be able to mutate the attributes of the original ComponentDefRef.");
+			$A.test.assertUndefinedOrNull(cdr[0].attributes.values.body.value[0].attributes.valueProvider, "Iteration should not be able to mutate the attributes of the original ComponentDefRef.");
+		}
+	}
+})

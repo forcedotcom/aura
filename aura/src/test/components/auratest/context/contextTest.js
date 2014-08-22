87390058d364072420fe7503ce56d36425cefa9c
Trevor <tbliss@salesforce.com>
2014-08-21 17:47:55 -0700

    Update context's loaded object test
    @bug W-2347012@
    @rev gtorres@
    @rev goliver@

--- aura/src/test/components/auratest/context/contextTest.js
+++ aura/src/test/components/auratest/context/contextTest.js
@@ -1,8 +1,8 @@
 ({
     labels : [ "auraSanity" ],
-        //
-        // Get a component, clearing the configs if necessary.
-        //
+    //
+    // Get a component, clearing the configs if necessary.
+    //
     doGet : function(doGetComponent, descriptor, haveComponents) {
         var action = $A.get("c.aura://ComponentController.get" + (doGetComponent ? "Component" : "Application"));
         // sanity check.
@@ -15,6 +15,9 @@
             gotResponse = true;
             if (haveComponents) {
                 $A.test.clearAndAssertComponentConfigs(a);
+            } else {
+                $A.test.assertEquals(0, $A.getContext().clearComponentConfigs(a.getId()),
+                        "Action returned from server with unexpected component configs");
             }
         });
         $A.test.callServerAction(action);
@@ -70,16 +73,15 @@
         } ]
     },
 
-    
     /**
-     * Component that is preloaded, because it is a namespace dependency, will not appear in loaded set.
+     * Component that is preloaded, because it is explicitly listed as a dependency, will not appear in loaded set.
      */
-    testLoaded_PreloadedNamespaceDependencyIsNotLoaded : {
+    testLoaded_PreloadedExplicitDependencyIsNotLoaded : {
         test : [ function(c) {
-            this.assertNoUid("COMPONENT@markup://aura:text");
+            this.assertNoUid("COMPONENT@markup://auratest:test_button");
             this.doGet(true, "aura:text", false);
         }, function(c) {
-            this.assertNoUid("COMPONENT@markup://aura:text");
+            this.assertNoUid("COMPONENT@markup://auratest:test_button");
         } ]
     },
     
@@ -135,7 +137,8 @@
 
     /**
      * Loaded component with dependency of an already loaded component will remove the dependency from the loaded set.
-     * W-1989778
+     * 
+     * TODO(W-2356289): dependency is not removed from loaded set
      */
     _testLoaded_LoadedDependencyIsUnloadedIfParentIsLoaded : {
         test : [ function(c) {
@@ -145,11 +148,10 @@
         }, function(c) {
             this.assertNoUid("COMPONENT@markup://auratest:testComponent1");
             this.assertUid("COMPONENT@markup://auratest:testComponent2");
-            this.doGet(true, "auratest:testComponent1", false);
+            this.doGet(true, "auratest:testComponent1", true);
         }, function(c) {
             this.assertUid("COMPONENT@markup://auratest:testComponent1");
             this.assertNoUid("COMPONENT@markup://auratest:testComponent2"); // TODO: dependency is not removed from loaded set
         } ]
     }
-    
 })

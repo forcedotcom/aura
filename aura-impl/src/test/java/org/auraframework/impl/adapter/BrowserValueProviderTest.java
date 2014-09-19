e6e4136e245072cb1c1bd40751e3248bf1073e8e
Trevor <tbliss@salesforce.com>
2014-09-19 13:52:13 -0700

    Add isIE11 to $Browser and update User Agent tests
    - Adding cases for new IE that impersonates Android/Apple
    - Adding checks on user agents for browser type
    - Small clean up on assert messages etc
    
    @bug W-2377025@
    @bug W-2357285@
    @rev byao@

--- aura-impl/src/test/java/org/auraframework/impl/adapter/BrowserValueProviderTest.java
+++ aura-impl/src/test/java/org/auraframework/impl/adapter/BrowserValueProviderTest.java
@@ -51,6 +51,25 @@ public class BrowserValueProviderTest extends AuraImplTestCase {
                 BrowserValueProvider.IS_WINDOWS_PHONE, null);
     }
 
+    private interface BrowserType {
+        final PropertyReference isWebKit = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_WEBKIT, null);
+        final PropertyReference isFirefox = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_FIREFOX, null);
+        final PropertyReference isIE6 = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_IE6, null);
+        final PropertyReference isIE7 = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_IE7, null);
+        final PropertyReference isIE8 = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_IE8, null);
+        final PropertyReference isIE9 = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_IE9, null);
+        final PropertyReference isIE10 = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_IE10, null);
+        final PropertyReference isIE11 = new PropertyReferenceImpl(
+                BrowserValueProvider.IS_IE11, null);
+    }
+
     public void testValidate() throws Exception {
         BrowserValueProvider bvp = new BrowserValueProvider();
         bvp.validate(BrowserProperty.isTablet);
@@ -94,9 +113,9 @@ public class BrowserValueProviderTest extends AuraImplTestCase {
         }
     }
 
-    private void assertBrowserProperty(BrowserValueProvider bvp,
-            PropertyReference property, Object expected) throws Exception {
-        assertEquals("Unexpected value for " + property.toString(), expected,
+    private void assertBrowserProperty(BrowserValueProvider bvp, PropertyReference property, Object expected,
+            String userAgent) throws Exception {
+        assertEquals("Unexpected value for " + property.toString() + " for UserAgent <" + userAgent + ">", expected,
                 bvp.getValue(property));
     }
 
@@ -104,21 +123,23 @@ public class BrowserValueProviderTest extends AuraImplTestCase {
             boolean isPhone, boolean isAndroid, String formFactor,
             boolean isIPad, boolean isIPhone, boolean isIOS, boolean isWindowsPhone) throws Exception {
         AuraContext context = Aura.getContextService().getCurrentContext();
-        context.setClient(new Client(userAgent == null ? null : userAgent
-                .getUserAgentString()));
+        String userAgentString = userAgent == null ? null : userAgent.getUserAgentString();
+        context.setClient(new Client(userAgentString));
         BrowserValueProvider bvp = new BrowserValueProvider();
-        assertBrowserProperty(bvp, BrowserProperty.isTablet, isTablet);
-        assertBrowserProperty(bvp, BrowserProperty.isPhone, isPhone);
-        assertBrowserProperty(bvp, BrowserProperty.isAndroid, isAndroid);
-        assertBrowserProperty(bvp, BrowserProperty.formFactor, formFactor);
-        assertBrowserProperty(bvp, BrowserProperty.isIPad, isIPad);
-        assertBrowserProperty(bvp, BrowserProperty.isIPhone, isIPhone);
-        assertBrowserProperty(bvp, BrowserProperty.isIOS, isIOS);
-        assertBrowserProperty(bvp, BrowserProperty.isWindowsPhone, isWindowsPhone);
+        assertBrowserProperty(bvp, BrowserProperty.isTablet, isTablet, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.isPhone, isPhone, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.isAndroid, isAndroid, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.formFactor, formFactor, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.isIPad, isIPad, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.isIPhone, isIPhone, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.isIOS, isIOS, userAgentString);
+        assertBrowserProperty(bvp, BrowserProperty.isWindowsPhone, isWindowsPhone, userAgentString);
     }
 
-    // sample some user agents
-    public void testGetValue() throws Exception {
+    /**
+     * Verify user agent strings return the correct browser properties (form factor, OS, etc.).
+     */
+    public void testGetValueBrowserProperties() throws Exception {
         assertBrowserProperties(null, false, false, false, "DESKTOP", false, false, false, false);
         assertBrowserProperties(UserAgent.EMPTY, false, false, false, "DESKTOP", false, false, false, false);
         assertBrowserProperties(UserAgent.GOOGLE_CHROME, false, false, false, "DESKTOP", false, false, false, false);
@@ -140,14 +161,19 @@ public class BrowserValueProviderTest extends AuraImplTestCase {
         assertBrowserProperties(UserAgent.ANDROID1_6, false, true, true, "PHONE", false, false, false, false);
         assertBrowserProperties(UserAgent.ANDROID2_3, false, true, true, "PHONE", false, false, false, false);
         assertBrowserProperties(UserAgent.ANDROID4_2, false, true, true, "PHONE", false, false, false, false);
+        assertBrowserProperties(UserAgent.NEXUS_10, true, false, true, "TABLET", false, false, false, false);
         assertBrowserProperties(UserAgent.KINDLE_FIRE, true, false, false, "TABLET", false, false, false, false);
         assertBrowserProperties(UserAgent.PLAYBOOK, true, false, false, "TABLET", false, false, false, false);
         assertBrowserProperties(UserAgent.NOKIA_N95, false, true, false, "PHONE", false, false, false, false);
         assertBrowserProperties(UserAgent.NOKIA_920, false, true, false, "PHONE", false, false, false, true);
+        assertBrowserProperties(UserAgent.LUMIA_928, false, true, false, "PHONE", false, false, false, true);
         assertBrowserProperties(UserAgent.IE10_WINDOWS_PHONE_8, false, true, false, "PHONE", false, false, false, true);
         assertBrowserProperties(UserAgent.IE10_WINDOWS_RT_8, true, false, false, "TABLET", false, false, false, false);
-        assertBrowserProperties(UserAgent.IE11_WINDOWS_PHONE_8_1, false, true, false, "PHONE", false, false, false, true);
+        assertBrowserProperties(UserAgent.IE11_WINDOWS_PHONE_8_1, false, true, false, "PHONE", false, false, false,
+                true);
         assertBrowserProperties(UserAgent.IE11_WINDOWS_RT_8_1, true, false, false, "TABLET", false, false, false, false);
+        assertBrowserProperties(UserAgent.IE11_WINDOWS_PHONE_8_1, false, true, false, "PHONE", false, false, false,
+                true);
         assertBrowserProperties(UserAgent.BLACKBERRY_10, false, true, false, "PHONE", false, false, false, false);
         assertBrowserProperties(UserAgent.BLACKBERRY_7, false, true, false, "PHONE", false, false, false, false);
         assertBrowserProperties(UserAgent.GOOD_IPHONE, false, true, false, "PHONE", false, true, true, false);
@@ -159,4 +185,65 @@ public class BrowserValueProviderTest extends AuraImplTestCase {
         assertEquals(null,
                 bvp.getValue(new PropertyReferenceImpl("isBlackberry", null))); // undefined property
     }
+
+    public void testGetValueBrowserTypes() throws Exception {
+        assertBrowserTypes(UserAgent.EMPTY, false, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.GOOGLE_CHROME, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.IE6, false, false, true, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.IE7, false, false, false, true, false, false, false, false);
+        assertBrowserTypes(UserAgent.IE8, false, false, false, false, true, false, false, false);
+        assertBrowserTypes(UserAgent.IE9, false, false, false, false, false, true, false, false);
+        // TODO(W-2383688): IE10 user agent is being reported as IE9
+        // assertBrowserTypes(UserAgent.IE10, false, false, false, false, false, false, true, false);
+        assertBrowserTypes(UserAgent.IE11, false, false, false, false, false, false, false, true);
+        assertBrowserTypes(UserAgent.IE11_WINDOWS_PHONE_8_1_SDK, false, false, false, false, false, false, false,
+                true);
+        assertBrowserTypes(UserAgent.IE11_NET_FRAMEWORK, false, false, false, false, false, false, false, true);
+        assertBrowserTypes(UserAgent.IE11_WINDOWS_PHONE_8_1, false, false, false, false, false, false, false, true);
+        assertBrowserTypes(UserAgent.IE11_WINDOWS_RT_8_1, false, false, false, false, false, false, false, true);
+        assertBrowserTypes(UserAgent.LUMIA_928, false, false, false, false, false, false, false, true);
+        assertBrowserTypes(UserAgent.NOKIA_920, false, false, false, false, false, false, true, false);
+        assertBrowserTypes(UserAgent.FIREFOX, false, true, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.NETSCAPE, false, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.SAFARI5_MAC, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.SAFARI5_WINDOWS, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.SAFARI6, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.IPAD, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.IPAD_7, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.IPAD_WEBVIEW, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.NEXUS_10, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.OPERA12, false, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.OPERA12_MOBILE, false, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.OPERA_MINI, false, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.KINDLE_FIRE, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.ANDROID4_2, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.BLACKBERRY_7, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.BLACKBERRY_10, true, false, false, false, false, false, false, false);
+        assertBrowserTypes(UserAgent.GOOD_IPHONE, true, false, false, false, false, false, false, false);
+        // Good Technology for Android is not reported as WebKit, is that correct?
+        assertBrowserTypes(UserAgent.GOOD_ANDROID, false, false, false, false, false, false, false, false);
+    }
+
+    private void assertBrowserType(BrowserValueProvider bvp, PropertyReference property, Object expected,
+            String userAgent) throws Exception {
+        assertEquals("Unexpected value for " + property.toString() + " for UserAgent <" + userAgent + ">", expected,
+                bvp.getValue(property));
+    }
+
+    private void assertBrowserTypes(UserAgent userAgent, boolean isWebKit, boolean isFirefox, boolean isIE6,
+            boolean isIE7, boolean isIE8, boolean isIE9, boolean isIE10, boolean isIE11) throws Exception {
+        AuraContext context = Aura.getContextService().getCurrentContext();
+        String userAgentString = userAgent == null ? null : userAgent.getUserAgentString();
+        context.setClient(new Client(userAgentString));
+        BrowserValueProvider bvp = new BrowserValueProvider();
+        assertBrowserType(bvp, BrowserType.isWebKit, isWebKit, userAgentString);
+        assertBrowserType(bvp, BrowserType.isFirefox, isFirefox, userAgentString);
+        assertBrowserType(bvp, BrowserType.isIE6, isIE6, userAgentString);
+        assertBrowserType(bvp, BrowserType.isIE7, isIE7, userAgentString);
+        assertBrowserType(bvp, BrowserType.isIE8, isIE8, userAgentString);
+        assertBrowserType(bvp, BrowserType.isIE9, isIE9, userAgentString);
+        assertBrowserType(bvp, BrowserType.isIE10, isIE10, userAgentString);
+        assertBrowserType(bvp, BrowserType.isIE11, isIE11, userAgentString);
+    }
+
 }

161ac9ab784cfafc550276b6f0b66ac17671d538
Gabriel Such <gsuch@salesforce.com>
2014-10-10 09:13:36 -0700

    less intrusive way to extend WebDriverTestCase

--- aura/src/test/java/org/auraframework/test/page/PageObjectTestCase.java
+++ aura/src/test/java/org/auraframework/test/page/PageObjectTestCase.java
@@ -1,14 +1,21 @@
 package org.auraframework.test.page;
 
 import org.auraframework.test.WebDriverTestCase;
+import org.openqa.selenium.WebDriver;
 
 public abstract class PageObjectTestCase<T extends PageObject> extends WebDriverTestCase {
 
-	private T page;
+	private final T page;
 	
 	public PageObjectTestCase(T page) {
-		super(page);
+		super(page.getName());
 		this.page = page;
+	}
+	
+	@Override
+	public void setCurrentDriver(WebDriver currentDriver) {
+		super.setCurrentDriver(currentDriver);
+		page.setDriver(currentDriver);
 	}
 
 	public T page() {

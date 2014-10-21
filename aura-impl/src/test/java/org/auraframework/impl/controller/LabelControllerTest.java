0da4921a95b5af0b4b2f30e4ea9aeb0f7194bf97
Trevor <tbliss@salesforce.com>
2014-10-21 14:50:50 -0700

    Update expression parser test
    @bug W-1530127@
    @rev goliver@

--- aura-impl/src/test/java/org/auraframework/impl/controller/LabelControllerTest.java
+++ aura-impl/src/test/java/org/auraframework/impl/controller/LabelControllerTest.java
@@ -32,50 +32,51 @@ import com.google.common.collect.Maps;
 /**
  * 
  * Unit Tests for LabelController.
- *
+ * 
  */
 @UnAdaptableTest
-//Missing labels behave differently in SFDC stack
+// Missing labels behave differently in SFDC stack
 public class LabelControllerTest extends AuraImplTestCase {
     DefDescriptor<ControllerDef> labelCntrDesc = DefDescriptorImpl.getInstance(
             "java://org.auraframework.impl.controller.LabelController", ControllerDef.class);
     String getLabelDesc = "java://org.auraframework.impl.controller.LabelController/ACTION$getLabel";
     String placeholder = "FIXME - LocalizationAdapter.getLabel() needs implementation!";
-    
-    public LabelControllerTest(String name){
+
+    public LabelControllerTest(String name) {
         super(name);
     }
-    
-    public void testLabelController() throws Exception{
+
+    public void testLabelController() throws Exception {
         ControllerDef def = labelCntrDesc.getDef();
         assertNotNull("Failed to fetch the definition of the Label Controller.", def);
         runLabelAction("Related_Lists", "task_mode_today", State.SUCCESS, "Today");
     }
-    
-    public void testInvalidSection() throws Exception{
-        runLabelAction("FOO", "task_mode_today", State.SUCCESS, "Today"); //In Aura, section doesn't matter
+
+    public void testInvalidSection() throws Exception {
+        runLabelAction("FOO", "task_mode_today", State.SUCCESS, "Today"); // In Aura, section doesn't matter
         runLabelAction("FOO", null, State.SUCCESS, placeholder);
-        runLabelAction("FOO", "" , State.SUCCESS, placeholder);
+        runLabelAction("FOO", "", State.SUCCESS, placeholder);
     }
-    
-    public void testInvalidLabel() throws Exception{
+
+    public void testInvalidLabel() throws Exception {
         runLabelAction("Related_Lists", "FooBared", State.SUCCESS, placeholder);
         runLabelAction("FooBared", "FooBared", State.SUCCESS, placeholder);
-        runLabelAction(null, "FooBared" , State.SUCCESS, placeholder);
-        runLabelAction("", "FooBared" , State.SUCCESS, placeholder);
+        runLabelAction(null, "FooBared", State.SUCCESS, placeholder);
+        runLabelAction("", "FooBared", State.SUCCESS, placeholder);
     }
-    
-    public Action runLabelAction(String section, String name, State expectedStatus, String expectedLabel) throws Exception{
+
+    public Action runLabelAction(String section, String name, State expectedStatus, String expectedLabel)
+            throws Exception {
         Map<String, Object> params = Maps.newHashMap();
         params.put("section", section);
         params.put("name", name);
-        
+
         Action instance = (Action) Aura.getInstanceService().getInstance(getLabelDesc,
                 ActionDef.class, params);
         instance.run();
-        assertEquals(State.SUCCESS, expectedStatus);
+        assertEquals(expectedStatus, instance.getState());
         assertEquals(expectedLabel, instance.getReturnValue());
-        
+
         return instance;
     }
 }

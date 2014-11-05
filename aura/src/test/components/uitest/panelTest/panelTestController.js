bd43997ade687501f39dcf81786330081951ef09
Ryan <rdewsbury@salesforce.com>
2014-11-05 11:27:44 -0500

    adding panelDialog with nonModal support @bug W-2376792@ @rev ssun@

--- aura/src/test/components/uitest/panelTest/panelTestController.js
+++ aura/src/test/components/uitest/panelTest/panelTestController.js
@@ -39,7 +39,8 @@
 		overlay.addHandler('press', cmp, 'c.showModal')
 		var openPanelEvent = $A.get('e.ui:openPanel').setParams({
         	show: true,
-        	isModal: true,
+            isModal: true,
+            isDialog: true,
         	title: 'my panel',
         	'class': 'myClass',
         	body: cmps
@@ -50,6 +51,28 @@
         });
         openPanelEvent.fire();
 	},
+
+    showNonModal: function(cmp, event, helper) {
+        var cmps = $A.componentService.newComponentAsync(this, function(){}, helper.getConfig(cmp));
+        var overlay = cmps[3];
+        var referenceElement = cmp.find("nonModalButton").getElement();
+        overlay.addHandler('press', cmp, 'c.showModal');
+
+        var openPanelEvent = $A.get('e.ui:openPanel').setParams({
+            show: true,
+            isModal: false,
+            isDialog: true,
+            referenceElement: referenceElement,
+            title: 'my panel',
+            'class': 'myClass',
+            body: cmps
+//            callbacks: { onCreate: function(panel){
+//            	var cmps = $A.componentService.newComponentAsync(this, function(){}, helper.getConfig(cmp));
+//            	panel.set("v.body", cmps);
+//            }}
+        });
+        openPanelEvent.fire();
+    },
 	
 	showPanelSlider: function(cmp,event, helper) {
 		if (cmp._panelSlider) {

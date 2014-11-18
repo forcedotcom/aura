8b351d4710cb86d3addcccfadb32deb62bf16544
Ryan <rdewsbury@salesforce.com>
2014-11-18 17:07:48 -0500

    add more accepted tags for ckeditor @rev john.huang@ @bug W-2377124@

--- aura-resources/src/main/resources/aura/resources/ckeditor/ckeditor-4.x/rel/config.js
+++ aura-resources/src/main/resources/aura/resources/ckeditor/ckeditor-4.x/rel/config.js
@@ -7,5 +7,21 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	// CK4 filters some html tags and attributes by default, which disappear on save or during toggle from html to source and back
+	// see http://docs.ckeditor.com#!/guide/dev_allowed_content_rules
+	// these are safe, supported tags we always want to allow in client validation regardless of which plugins or toolbars are loaded
+	// some of these would only be needed if pasting in styled content from an outside app (which is supported)
+	var tags = [];
+	tags.push('div{*}(*); span{*}(*); p{*}(*); br{*}(*); hr{*}(*);');
+	tags.push('h1{*}(*); h2{*}(*); h3{*}(*); h4{*}(*); h5{*}(*); h6{*}(*);');
+	tags.push('a[!href]{*}(*);')
+	tags.push('img[!src,alt,width,height,border]{*}(*);');
+	tags.push('font[face,size,color];')
+	tags.push('strike; s; b; em; strong; i; big; small; sub; sup; blockquote; ins; kbd; pre; tt;');
+	tags.push('abbr; acronym; address; bdo; caption; cite; code; col; colgroup;');
+	tags.push('dd; del; dfn; dl; dt; q; samp; var;');
+	tags.push('table{*}(*)[align,border,cellpadding,cellspacing,summary];');
+	tags.push('caption{*}(*); tbody{*}(*); thead{*}(*); tfoot{*}(*); th{*}(*)[scope]; tr{*}(*); td{*}(*)[scope];');
+	config.extraAllowedContent = tags.join(' ');
 };
 

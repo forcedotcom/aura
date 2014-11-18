078fde4fc3df2b0cdedd3e6671b1373716c9263f
Kris Gray <kgray@salesforce.com>
2014-11-18 14:33:38 -0800

    Initial fix for properly bubbling passthrough value change events
    @bug W-2427498@
    @rev jbuchanan@

---
+++ aura/src/test/components/auratest/valueChangeEvents/valueChangeEventsController.js
@@ -0,0 +1,26 @@
+({
+	doInit: function(component, event, helper) {
+
+		// Add some data to the "data" attribute to show in the iteration
+		var mapdata = {
+			items: [
+				{ "label": "item0"},
+				{ "label": "item1"},
+				{ "label": "item2"},
+				{ "label": "item3"},
+				{ "label": "item4"},
+			]
+		};
+
+		var listdata = [
+			"listitem0",
+			"listitem1",
+			"listitem2",
+			"listitem3",
+			"listitem4"
+		];
+
+		component.set("v.mapdata", mapdata);
+		component.set("v.listdata", listdata);
+	}
+})

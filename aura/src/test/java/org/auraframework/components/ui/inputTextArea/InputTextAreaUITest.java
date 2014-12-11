6b7caa830ecc63cacc88d919b15afc97e7e08318
ronak.shah <ronak.shah@salesforce.com>
2014-12-11 12:35:48 -0800

    Automation around Cursor jumping in textArea
    @bug W-2356548@
    @rev ctatlah@

--- aura/src/test/java/org/auraframework/components/ui/inputTextArea/InputTextAreaUITest.java
+++ aura/src/test/java/org/auraframework/components/ui/inputTextArea/InputTextAreaUITest.java
@@ -19,6 +19,7 @@ import org.auraframework.test.WebDriverTestCase;
 import org.auraframework.test.WebDriverUtil.BrowserType;
 import org.auraframework.test.annotation.UnAdaptableTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -29,6 +30,7 @@ import org.openqa.selenium.WebElement;
 public class InputTextAreaUITest extends WebDriverTestCase {
 
     public static final String TEST_CMP = "/uitest/inputTextArea_UpdateOnTest.cmp";
+    public static final String TEST_CMP2 = "/uitest/inputTextArea_UpdateOnValueChange.cmp";
 
     public InputTextAreaUITest(String name) {
         super(name);
@@ -75,6 +77,36 @@ public class InputTextAreaUITest extends WebDriverTestCase {
         input.clear();
         input.click();
         input.sendKeys(inputText);
+        outputDiv.click(); // to simulate tab behavior for touch browsers
+        String actualText = (String) auraUITestingUtil.getEval(valueExpression);
+        assertEquals("Total number of bytes with \r\n does not match", inputText.getBytes().length + 3,
+                actualText.getBytes().length);
+        assertEquals("Value of Input text Area shoud be updated after removing carriage return", inputText,
+                actualText.replaceAll("(\\r)", ""));
+    }
+    
+    /*
+     * Test case for Stopping cursor from jumping to end of input field
+     * Bug: W-2356548
+     * Issue is on firefox and IE
+     */
+    public void testTextAreaWithMultipleLinesOfText() throws Exception {
+        open(TEST_CMP2);
+        WebElement div = findDomElement(By.id("textAreaWithLabel"));
+        WebElement input = div.findElement(By.tagName("textarea"));
+        WebElement outputDiv = findDomElement(By.id("output"));
+
+        String inputAuraId = "textAreaWithLabel";
+        String valueExpression = auraUITestingUtil.getValueFromCmpRootExpression(inputAuraId, "v.value");
+
+        String inputText = String.format("%s%n%s%n%s%n%s", "LINE1", "LINE2", "LINE3", "L4");
+        input.clear();
+        input.click();
+        input.sendKeys(inputText);
+        input.sendKeys(Keys.ARROW_LEFT, Keys.ARROW_LEFT);
+        String inputNewString = "L5";
+        input.sendKeys(inputNewString);
+        inputText = String.format("%s%n%s%n%s%n%s", "LINE1", "LINE2", "LINE3", "L5L4");
         outputDiv.click(); // to simulate tab behavior for touch browsers
         String actualText = (String) auraUITestingUtil.getEval(valueExpression);
         assertEquals("Total number of bytes with \r\n does not match", inputText.getBytes().length + 3,

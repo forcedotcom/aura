858904a2a6ae3dae81e4c72ddff6d7233235bf38
Gordon <goliver@salesforce.com>
2014-12-26 20:30:24 -0500

    Move things for dependencies

---
+++ aura-integration-test/src/test/java/org/auraframework/test/css/CSSValidationOverrideTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.auraframework.test.css;
+
+import org.auraframework.Aura;
+import org.auraframework.def.ComponentDef;
+import org.auraframework.def.DefDescriptor;
+import org.auraframework.def.StyleDef;
+import org.auraframework.impl.AuraImplTestCase;
+import org.auraframework.impl.css.parser.StyleParser;
+import org.auraframework.test.annotation.ThreadHostileTest;
+import org.auraframework.throwable.quickfix.QuickFixException;
+import org.auraframework.throwable.quickfix.StyleParserException;
+
+/**
+ * Automation to verify that CSS validation override. Aura enforces validation of CSS in component bundles. This css
+ * validation can be configured in a aura.conf file. The aura.conf file should be at one of the Java classpath roots of
+ * the aura projects. For example aura-integration-test/src/test/java/aura.conf >aura.css.validate=true
+ */
+
+public class CSSValidationOverrideTest extends AuraImplTestCase {
+    DefDescriptor<StyleDef> styleDefDesc;
+
+    public CSSValidationOverrideTest(String name) {
+        super(name);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        DefDescriptor<ComponentDef> cmp = addSourceAutoCleanup(ComponentDef.class,
+                String.format(baseComponentTag, "", ""));
+        assertNotNull(cmp.getDef());
+        styleDefDesc = Aura.getDefinitionService().getDefDescriptor(
+                String.format("%s://%s.%s", DefDescriptor.CSS_PREFIX, cmp.getNamespace(), cmp.getName()),
+                StyleDef.class);
+        addSourceAutoCleanup(styleDefDesc,
+                ".xyErrorText {" +
+                        "color: #808080;" +
+                        "padding-bottom: 5px;" +
+                        "}" +
+                        ".xyLabel {" +
+                        "padding-right: 5px;" +
+                        "}");
+    }
+
+    /**
+     * By default all component CSS is validated by StyleParser.
+     */
+    public void testDefaultProps() {
+        assertTrue("By default all component CSS should be validated", Aura.getConfigAdapter().validateCss());
+        getInvalidStyleDef(true);
+    }
+
+    /**
+     * Override the validateCss flag on configAdapter and make sure validations are skipped
+     */
+    @ThreadHostileTest("disables CSS validation")
+    public void testOverrideCSSValidation() {
+        getMockConfigAdapter().setValidateCss(false);
+        assertFalse("Expected CSS validation to be overriden.", Aura.getConfigAdapter().validateCss());
+        getInvalidStyleDef(false);
+    }
+
+    private void getInvalidStyleDef(boolean expectException) {
+        try {
+            StyleParser.getInstance().parse(styleDefDesc, getSource(styleDefDesc));
+            if (expectException) fail("Expected CSS validation to be turned on and catch the invalid CSS");
+        } catch (StyleParserException expected) {
+            if (!expectException) {
+                fail("Did not expect to encounter CSS validation exception.");
+            } else {
+                assertTrue(
+                        "Unexpected error message in StyleParserException",
+                        expected.getMessage().contains(
+                                "CSS selector must begin with"));
+            }
+        } catch (QuickFixException e) {
+            fail("Test setup failed. Looking for component test.testInValidCSS with invalid CSS");
+        }
+    }
+}
---
+++ aura-integration-test/src/test/java/org/auraframework/test/namespace/NamespaceDefTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.auraframework.test.namespace;
+
+import org.auraframework.Aura;
+import org.auraframework.def.DefDescriptor;
+import org.auraframework.def.NamespaceDef;
+import org.auraframework.def.StyleDef;
+import org.auraframework.impl.AuraImplTestCase;
+import org.auraframework.impl.system.DefDescriptorImpl;
+import org.auraframework.service.DefinitionService;
+import org.auraframework.system.MasterDefRegistry;
+import org.auraframework.system.Source;
+import org.auraframework.throwable.quickfix.DefinitionNotFoundException;
+
+public class NamespaceDefTest extends AuraImplTestCase {
+
+    public NamespaceDefTest(String name) {
+        super(name);
+    }
+
+    public void testGetNamespaceSource() {
+        MasterDefRegistry reg = Aura.getContextService().getCurrentContext().getDefRegistry();
+        DefinitionService defService = Aura.getDefinitionService();
+        DefDescriptor<NamespaceDef> descriptor = defService.getDefDescriptor("test", NamespaceDef.class);
+        Source<NamespaceDef> src = reg.getSource(descriptor);
+        assertNotNull(src);
+
+        descriptor = DefDescriptorImpl.getInstance("nonExistantNamespace", NamespaceDef.class);
+        src = reg.getSource(descriptor);
+        assertNull(src);
+    }
+
+    public void testGetNamespaceDef() throws Exception {
+        DefinitionService defService = Aura.getDefinitionService();
+        DefDescriptor<NamespaceDef> descriptor = defService.getDefDescriptor("aura", NamespaceDef.class);
+        NamespaceDef def = descriptor.getDef();
+        assertNotNull(def);
+
+        descriptor = defService.getDefDescriptor("nonExistantNamespace", NamespaceDef.class);
+        try {
+            descriptor.getDef();
+            fail("Expected Exception when trying to compile non-existent NamespaceDef");
+        } catch (Exception e) {
+            checkExceptionFull(e, DefinitionNotFoundException.class,
+                    "No NAMESPACE named markup://nonExistantNamespace found");
+        }
+
+        descriptor = defService.getDefDescriptor("namespaceDefTest", NamespaceDef.class);
+        def = descriptor.getDef();
+        assertNotNull(def);
+        assertEquals("red", def.getStyleTokens().get("FOO"));
+    }
+
+    /* tokens functionality deprecated by themes */
+    public void _testStyleTokens() throws Exception {
+        DefinitionService defService = Aura.getDefinitionService();
+        StyleDef styleDef = defService.getDefinition("namespaceDefTest.testStyleTokens", StyleDef.class);
+        assertEquals(
+                ".namespaceDefTestTestStyleTokens {background-color:red; color:FOOL; border-color:black; " +
+                        "background:-webkit-linear-gradient(top, hsl(0, 80%, 70%), #bada55); " +
+                        "background:linear-gradient(to bottom, hsl(0, 80%, 70%), #bada55)}",
+                styleDef.getCode());
+    }
+}

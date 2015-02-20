4f9b6590b822af20f5a1b801e28e8045627d642f
Iurie <icangea@salesforce.com>
2015-02-20 18:55:15 -0500

    Revert: Attaching click event handling not necessary for scroller @bug W-2474954@ @rev dval@

--- aura-components/src/main/components/ui/scroller/scrollerHelper.js
+++ aura-components/src/main/components/ui/scroller/scrollerHelper.js
@@ -334,6 +334,8 @@
         for (var i = 0; i < events.length; i++) {
             this._bridgeScrollerAction(component, scrollerInstance, events[i]);
         }
+
+        this._captureClickEvents(component, scrollerInstance);
     },
     /*
     * If we use native scrolling pullToShowMore and pullToRefresh will render as part of the scroller
@@ -355,7 +357,24 @@
         var wrapper = this._getScrollerWrapper(component);
         wrapper.ondragstart = function () { return false; }; //testing
     },
+    /*
+    * @_captureClickEvents:
+    *
+    * Due to the way aura handles click events (attaching the handler directly to the DOM element (no delegation mechanism))
+    * We need to handle the click event in the capture face, and if the scroller didn't move, let  the event flow and let aura handle the event.
+    * Otherwise a click will be fired even when you have moved the scroller which presents a huge usability issue.
+    * This function will make the click pointers and touch events work properly across devices/platforms
+    */
+    _captureClickEvents: function (component, scroller) {
+        var wrapper = this._getScrollerWrapper(component);
 
+        wrapper.addEventListener('click', function (e) {
+            if (scroller.moved) {
+                e.cancelBubble = true;
+                e.stopPropagation();
+            }
+        }, true);
+    },
     deactivate: function(component) {
         var namespace = this.getScrollerNamespace(),
             scroller  = this.getScrollerInstance(component),

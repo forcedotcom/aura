5d84984411538670ad76270bbb2ad13906ba36b4
Cheng <cheng.li@salesforce.com>
2015-03-02 13:38:24 -0800

    Fixing Aura library example. @rev gtorres@

--- aura-components/src/main/components/uiExamples/libraryExample/libraryExampleHelper.js
+++ aura-components/src/main/components/uiExamples/libraryExample/libraryExampleHelper.js
@@ -17,6 +17,6 @@
     getLoadedScriptMessage: function() {
         return "aura:library loaded and attached to the helper via the 'libraryDoc' property."
             + " The result of invoking libraryDoc.MyLib.getValue() is: \"" + 
-            this.libraryDoc.MyLib.getValue() + "\"";
+            this.libraryDoc.jsfile.getValue() + "\"";
     }
 })

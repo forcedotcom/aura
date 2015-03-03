1e06b2f4b56d03ca897ca465d4c14cb38abf2f8a
Gordon <goliver@salesforce.com>
2015-03-03 17:00:56 -0500

    Add dynamicType to $Global.
    @bug W-2443315@
    
    @rev kgray@

---
+++ aura-components/src/test/components/gvpTest/dynamicTypeTest/dynamicTypeTestTest.js
@@ -0,0 +1,86 @@
+({
+    testDynamicTypeSizeDefault:{
+        test:function(cmp){
+            $A.test.assertEquals(""+$A.get("$Global.dynamicTypeSize"), $A.test.getTextByComponent(cmp.find("data")));
+        }
+    },
+
+    _testDynamicTypeSizeChange:{
+        test:[function(cmp) {
+            $A.set("$Global.dynamicTypeSize", "small")
+        }, function(cmp) {
+            $A.test.assertEquals("small", $A.test.getTextByComponent(cmp.find("data")));
+            $A.set("$Global.dynamicTypeSize", "medium")
+        }, function(cmp) {
+            $A.test.assertEquals("medium", $A.test.getTextByComponent(cmp.find("data")));
+        }]
+    },
+
+    insertClientSide: function(cmp) {
+        var finished = false;
+
+        $A.componentService.newComponentAsync(this, 
+            function(newCmp) {
+                cmp.find("insertion").set("v.body", [ newCmp ]);
+                finished = true;
+            },
+            {
+                "componentDef": "gvpTest:dynamicTypeTest"
+            });
+        // wait for our component to be inserted.
+        $A.test.addWaitFor(true, function() { return finished});
+    },
+
+    insertServerSide: function(cmp) {
+        var finished = false;
+        var action = $A.get("c.aura://ComponentController.getComponent");
+
+        action.setParams({
+                "name" : "markup://gvpTest:dynamicTypeTest"
+        });
+        action.setCallback(this, function(a) {
+            var newCmp = $A.newCmpDeprecated(a.getReturnValue());
+            cmp.find("insertion").set("v.body", [ newCmp ])
+            finished = true;
+        }, "SUCCESS");
+        $A.enqueueAction(action);
+        // wait for our component to be inserted.
+        $A.test.addWaitFor(true, function() { return finished});
+    },
+
+    testDynamicTypeSizeSmallOnCreate:{
+        test:[function(cmp) {
+            $A.set("$Global.dynamicTypeSize", "small");
+            this.insertClientSide(cmp);
+        }, function(cmp) {
+            $A.test.assertEquals("small", $A.test.getTextByComponent(cmp.find("insertion")));
+        }]
+    },
+
+    testDynamicTypeSizeMediumOnCreate:{
+        test:[function(cmp) {
+            $A.set("$Global.dynamicTypeSize", "medium");
+            this.insertClientSide(cmp);
+        }, function(cmp) {
+            $A.test.assertEquals("medium", $A.test.getTextByComponent(cmp.find("insertion")));
+        }]
+    },
+
+    testDynamicTypeSizeSmallOnServerCreate:{
+        test:[function(cmp) {
+            $A.set("$Global.dynamicTypeSize", "small");
+            this.insertServerSide(cmp);
+        }, function(cmp) {
+            $A.test.assertEquals("small", $A.test.getTextByComponent(cmp.find("insertion")));
+        }]
+    },
+
+    testDynamicTypeSizeMediumOnServerCreate:{
+        test:[function(cmp) {
+            $A.set("$Global.dynamicTypeSize", "medium");
+            this.insertServerSide(cmp);
+        }, function(cmp) {
+            $A.test.assertEquals("medium", $A.test.getTextByComponent(cmp.find("insertion")));
+        }]
+    }
+})

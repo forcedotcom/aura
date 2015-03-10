9c7f60cd148c3a67bc69983b3aa8723c76ea5bba
Diego Ferreiro Val <elfogris@gmail.com>
2015-03-09 17:14:51 -0700

    _comment flapper test

--- aura-components/src/test/components/test/basicCspTest/basicCspTestTest.js
+++ aura-components/src/test/components/test/basicCspTest/basicCspTestTest.js
@@ -48,7 +48,7 @@
     },
     
     //test for [script-src 'self' chrome-extension: 'unsafe-eval' 'unsafe-inline';]
-    testScriptSource : {
+    _testScriptSource : {
         browsers : [ "-IE8", "-IE7" ],
     	attributes: { testScriptSource: true },
         test:[ 

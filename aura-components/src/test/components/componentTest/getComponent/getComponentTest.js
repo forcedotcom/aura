df0b9e9c7b37b1e6023b4f00cbf8ed6ec29548cc
Cheng <cheng.li@salesforce.com>
2015-03-13 16:00:53 -0700

    Adding automation tests for new API method getComponenet in AuraComponentService
    @bug W-2523835@
    @rev yungcheng.chen@
    @rev tbliss@

---
+++ aura-components/src/test/components/componentTest/getComponent/getComponentTest.js
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+({
+    /*
+     * Verify getting component by global Id
+     */
+    testGetComponentWithGlobalId : {
+        test : function(cmp) {
+            var expected = cmp.find('auraText');
+            var textCmpGlobalId = cmp.find('auraText').getGlobalId();
+
+            var actual = $A.getComponent(textCmpGlobalId);
+
+            $A.test.assertEquals(expected, actual);
+        }
+    },
+
+    /*
+     * Verify getting component by html element
+     */
+    testGetComponentWithAuraRenderedElement : {
+        test : function(cmp) {
+            var expected = cmp.find('auraDiv');
+            var element = document.getElementById('auraDiv');
+
+            var actual = $A.getComponent(element);
+
+            $A.test.assertEquals(expected, actual);
+        }
+    },
+
+    /*
+     * Verify getting component by html element
+     */
+    testGetComponentWithAuraRenderedInnerElement : {
+        test : function(cmp) {
+            var expected = cmp.find('innerDiv');
+            var element = document.getElementById('innerDiv');
+
+            var actual = $A.getComponent(element);
+
+            $A.test.assertEquals(expected, actual);
+        }
+    },
+
+    /*
+     * Verify calling getComponent with undefined
+     */
+    testGetComponentWithUndefined : {
+        test : function(cmp) {
+            var cmp = $A.getComponent();
+
+            $A.test.assertNull(cmp);
+        }
+    },
+
+    /*
+     * Verify calling getComponent with invalid global Id
+     */
+    testGetComponentWithInvalidGlobalId : {
+        test : function(cmp) {
+            var cmpGlobalId = 'invalid';
+
+            var actual = $A.getComponent(cmpGlobalId);
+
+            $A.test.assertUndefined(actual);
+        }
+    },
+
+    /*
+     * Verify using getComponent to get a destroyed component
+     */
+    testGetComponentWithDestroyedCmp : {
+        test : function(cmp) {
+            var component = cmp.find("destroyedComponent");
+            var globalId = component.getGlobalId();
+            // the componenet exists and can be retrieved
+            var tempCmp = $A.getComponent(globalId);
+            $A.test.assertEquals(component, tempCmp);
+            try {
+                component.destroy(false);
+            } catch(e) {
+                $A.test.fail("Component destroy() failed:" + e);
+            }
+
+            var actual = $A.getComponent(globalId);
+
+            $A.test.assertUndefined(actual);
+        }
+    },
+
+    /*
+     * Verify using getComponent to get component by given a element without tag
+     */
+    testGetComponentWithNoTagElement : {
+        test :function(cmp) {
+            var expected = cmp.find('auraDiv');
+            var textCmp = cmp.find('innerText');
+
+            var actual = $A.getComponent(textCmp.getElement());
+
+            $A.test.assertEquals(expected, actual);
+        }
+    },
+
+    /*
+     * Verify using getComponent to get a dynamically created component by using its
+     * global ID
+     *
+     * Temporarily diable it, since the component created by $A.createComponent() is NOT
+     * reachable by cmp.find().
+     * TODO: W-2529066
+     */
+    _testGetComponentWithCreatedCmp : {
+        test : [
+            function(cmp) {
+                var createdComponent;
+                $A.createComponent("ui:button", {
+                        "aura:id" : "auraButton",
+                        "label" : "button"
+                    }, function(component) {
+                            createdComponent = component;
+                            var globalId = component.getGlobalId();
+                            cmp.set("v.cmpGlobalId", globalId);
+                        });
+
+                $A.test.addWaitFor(true, function() {
+                        return typeof createdComponent !== "undefined";
+                    }, function() {
+                        cmp.find("createdComponents").set("v.body", createdComponent);
+                    });
+            }, function(cmp) {
+                var expected = cmp.find("auraButton");
+                var globalId = cmp.get("v.cmpGlobalId");
+
+                var actual = $A.getComponent(globalId);
+
+                $A.test.assertEquals(expected, actual);
+            }
+        ]
+    }
+
+})

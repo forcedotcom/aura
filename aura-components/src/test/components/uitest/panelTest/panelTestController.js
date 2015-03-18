c3238c5e65776f518cc0db1a6fc462d4113c5b65
John Huang <john.huang@salesforce.com>
2015-03-18 16:29:13 -0700

    Adding test for @bug W-2512261@ @rev ronak.shah@

--- aura-components/src/test/components/uitest/panelTest/panelTestController.js
+++ aura-components/src/test/components/uitest/panelTest/panelTestController.js
@@ -1,26 +1,37 @@
 ({
 	
-	showModal: function(cmp, event, helper) {
-		$A.componentService.newComponentAsync(this, function(cmps){
+    showSimpleModal: function(cmp, event, helper) {
+        $A.componentService.newComponentAsync(this, function(cmps){
             var overlay = cmps[3];
             overlay.addHandler('press', cmp, 'c.showModal')
             var openPanelEvent = $A.get('e.ui:openPanel').setParams({
                 show: true,
                 isModal: true,
                 isDialog: true,
-                title: 'my panel',
+                title: 'Dialog Title',
                 'class': 'myClass',
                 body: cmps
-    //            callbacks: { onCreate: function(panel){
-    //            	$A.componentService.newComponentAsync(this, function(cmps){
-    //              	panel.set("v.body", cmps);
-    //              }, helper.getConfig(cmp));
-    //            }}
             });
             openPanelEvent.fire();
         }, 
         // TODO(W-2528400): Cannot do helper.getConfig(cmp)) here because helper references wrong file
         cmp.getDef().getHelper().getConfig(cmp));
+    },
+    
+	showModal: function(cmp, event, helper) {
+		$A.componentService.newComponentAsync(this, function(cmps){
+            var openPanelEvent = $A.get('e.ui:openPanel').setParams({
+                show: true,
+                isModal: true,
+                isDialog: true,
+                title: 'Dialog Title',
+                'class': 'myClass',
+                body: cmps
+            });
+            openPanelEvent.fire();
+        }, 
+        // TODO(W-2528400): Cannot do helper.getConfig(cmp)) here because helper references wrong file
+        cmp.getDef().getHelper().getConfig(cmp, 'mixConfig'));
 	},
 
     showNonModal: function(cmp, event, helper) {
@@ -34,13 +45,9 @@
                 isModal: false,
                 isDialog: true,
                 referenceElement: referenceElement,
-                title: 'my panel',
+                title: 'Dialog Title',
                 'class': 'myClass',
                 body: cmps
-    //            callbacks: { onCreate: function(panel){
-    //            	var cmps = $A.componentService.newComponentAsync(this, function(){}, helper.getConfig(cmp));
-    //            	panel.set("v.body", cmps);
-    //            }}
             });
             openPanelEvent.fire();
         },
@@ -50,41 +57,41 @@
     
     showNonModalNonTransient: function(cmp, event, helper) {
         if(!cmp._panel){
-      $A.newCmpAsync(this, function(newCmp) {
-    	  var referenceElement = cmp.find("nonModalButton").getElement();
-    	  var overlay = newCmp[3];
-    	  overlay.addHandler('press', cmp, 'c.showNonModalNonTransient');
-          var openPanelEvent = $A.get('e.ui:openPanel').setParams({
-              show: true,
-              isModal: false,
-              isDialog: true,
-              referenceElement: referenceElement,
-              title: 'my panel',
-              body: newCmp,
-              closeOnClickOut: true,
-              isTransient : false,
-              callbacks: {onCreate: function(panel){
-                  cmp._panel = panel;
-              }}
-          });
-          openPanelEvent.fire();
-      }, 
-      // TODO(W-2528400): Cannot do helper.getConfig(cmp)) here because helper references wrong file
-      cmp.getDef().getHelper().getConfig(cmp));
-        } else if (cmp._panel && cmp._panel.isValid()){
-          var openPanelEvent = $A.get('e.ui:openPanel').setParams({
-            show: true,
-            isModal: false,
-            isDialog: true,
-            closeOnClickOut: true,
-            instance: cmp._panel,
-            isTransient : false,
-        });
-        openPanelEvent.fire();
+            $A.newCmpAsync(this, function(newCmp) {
+                var referenceElement = cmp.find("nonModalButton").getElement();
+                var overlay = newCmp[3];
+                overlay.addHandler('press', cmp, 'c.showNonModalNonTransient');
+                var openPanelEvent = $A.get('e.ui:openPanel').setParams({
+                  show: true,
+                  isModal: false,
+                  isDialog: true,
+                  referenceElement: referenceElement,
+                  title: 'my panel',
+                  body: newCmp,
+                  closeOnClickOut: true,
+                  isTransient : false,
+                  callbacks: {onCreate: function(panel){
+                      cmp._panel = panel;
+                  }}
+              });
+              openPanelEvent.fire();
+          }, 
+          // TODO(W-2528400): Cannot do helper.getConfig(cmp)) here because helper references wrong file
+          cmp.getDef().getHelper().getConfig(cmp));
+        } else if (cmp._panel && cmp._panel.isValid()) {
+            var openPanelEvent = $A.get('e.ui:openPanel').setParams({
+                show: true,
+                isModal: false,
+                isDialog: true,
+                closeOnClickOut: true,
+                instance: cmp._panel,
+                isTransient : false,
+            });
+            openPanelEvent.fire();
         }
     },
-	
-	newOverlay: function(cmp,event, helper) {
-		this.showModal(cmp,event, helper);
-	}
+    
+    newOverlay: function(cmp,event, helper) {
+        this.showModal(cmp,event, helper);
+    }
 })
--- aura-components/src/test/components/uitest/panelTest/panelTestHelper.js
+++ aura-components/src/test/components/uitest/panelTest/panelTestHelper.js
@@ -1,45 +1,73 @@
 ({
-	getConfig: function(cmp) {
-		if (!this._componentConfig) {
-			this._componentConfig= [{
-				componentDef: 'markup://ui:outputText',
-				attributes : {
-					values :
-					{
-						value : 'testing'
-					}
-				}
-			},
-			{
-				componentDef: 'markup://ui:button',
-				attributes : {
-					values :
-					{
-						label : 'button 1'
-					}
-				}
-			},
-			{
-				componentDef: 'markup://ui:button',
-				attributes : {
-					values :
-					{
-						label : 'button 2'
-					}
-				}
-			},
-			{
-				componentDef: 'markup://ui:button',
-				attributes : {
-					values :
-					{
-						"class": 'pressOverlay',
-						label : 'open another overlay'
-					}
-				}
-			}
-		];
-		}
-		return this._componentConfig;
-	}	
+	simpleConfig: [{
+        componentDef: 'markup://ui:outputText',
+        attributes : {
+            values :
+            {
+                value : 'Dialog with Buttons'
+            }
+        }},
+        {
+        componentDef: 'markup://ui:button',
+        attributes : {
+            values :
+            {
+                label : 'button 1'
+            }
+        }},
+        {
+            componentDef: 'markup://ui:button',
+            attributes : {
+            values :
+            {
+                label : 'button 2'
+            }
+        }},
+        {
+            componentDef: 'markup://ui:button',
+            attributes : {
+                values :
+                {
+                    "class": 'pressOverlay',
+                    label : 'open another overlay'
+                }}
+        }],
+    mixConfig: [{
+        componentDef: 'markup://ui:outputText',
+        attributes : {
+            values :
+            {
+                value : 'Dialog with Inputs'
+            }
+        }},
+        {
+            componentDef: 'markup://ui:inputText',
+            attributes : {
+                values :
+                {
+                    label : 'First Name',
+                    'class' : 'firstInput'
+                }
+            }
+        }, {
+            componentDef: 'markup://ui:inputText',
+            attributes : {
+                values :
+                {
+                    label : 'Last Name'
+                }
+            }
+        },
+        {
+            componentDef: 'markup://ui:button',
+            attributes : {
+                values :
+                {
+                    label : 'button 1'
+                }
+        }}
+    ],
+    getConfig: function(cmp, mode) {
+		return this[mode || 'simpleConfig'];
+	}
 })

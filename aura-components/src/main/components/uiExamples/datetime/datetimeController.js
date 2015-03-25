cfa25031ce8f31dad0f4ac3f369fbc49abaacaf3
Diana Widjaja <dwidjaja@salesforce.com>
2015-03-25 12:58:01 -0700

    Doc: datetime demo updates

--- aura-components/src/main/components/uiExamples/datetime/datetimeController.js
+++ aura-components/src/main/components/uiExamples/datetime/datetimeController.js
@@ -17,16 +17,15 @@
     doInit : function(component, event, helper) {
         var today = new Date();
         component.set('v.today', today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate());
-        component.set('v.deadline', today);
     },
     
     setOutput : function(component, event, helper) {
     	var el = component.find("msg");
     	$A.util.removeClass(el.getElement(), 'hide');
     	
-        var deadline = component.find("deadline").get("v.value");
+        var todayVal = component.find("today").get("v.value");
         var oDateTime = component.find("oDateTime");
-        oDateTime.set("v.value", deadline);
+        oDateTime.set("v.value", todayVal);
       
     }
 })

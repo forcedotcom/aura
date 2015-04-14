bbf6a42169b346cdeeaac3dad1dd1b01bc4f4ab2
Nihar <ndandekar@salesforce.com>
2015-04-14 15:29:37 -0700

    ESlint tools @bug W-2551264@

---
+++ aura-components/src/test/eslint/GruntFile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-eslint')
+    grunt.initConfig({
+        eslint: {
+            options: {
+                configFile: '${basedir}/src/test/eslint/eslint-overrides.json',
+                outputFile: '${project.build.directory}/eslint-output'
+            },
+            target: [
+                '${basedir}/src/main/components/'
+            ]
+        }
+    });
+    grunt.registerTask('default', ['eslint']);
+};

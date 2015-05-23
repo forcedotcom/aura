129af3f692c8d8400d7ff158516829be896af20c
Diego Ferreiro Val <elfogris@gmail.com>
2015-05-23 00:36:04 -0700

    moving tests

---
+++ aura-components/src/test/components/performanceComponentTest/runnerExample/runnerExampleController.js
@@ -0,0 +1,35 @@
+({
+    // --- Performance framework lifecycle
+    setup: function (cmp, event, helper) {
+        // Trivial (sync) setup phase
+        cmp.set('v.loaded', false);
+
+        // --- If the setup was async: ---
+        // var done = event.getParam('arguments').done;
+        // var finish = done.async();
+        // ...
+        // finish();
+    },
+    // --- Performance framework lifecycle
+    run: function (cmp, event, helper) {
+        // The "done" attribute gets provided by the perfRunner
+        var done = event.getParam('arguments').done;
+
+        // Action to load a component
+        var action = $A.get("c.aura://ComponentController.getComponent");
+
+        // Tells the perfRunner this operation is async 
+        // (we will need to execute the returned finishRun callback when we are done)
+        var finishRun = done.async(); 
+
+        action.setParams({ name: 'ui:image' });
+
+        action.setCallback(this, function () {
+            console.log('Downloaded def: ', action.getReturnValue());
+            cmp.set('v.loaded', true);
+            finishRun();
+        });
+
+        $A.enqueueAction(action);
+    }
+})

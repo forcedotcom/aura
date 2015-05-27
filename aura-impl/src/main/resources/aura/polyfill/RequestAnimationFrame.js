a1318db27b3c87ebd451179cc2575155de26858b
Bob Yao <byao@salesforce.com>
2015-05-27 10:17:23 -0700

    Fix syntax in requestAnimationFrame polyfill

--- aura-impl/src/main/resources/aura/polyfill/RequestAnimationFrame.js
+++ aura-impl/src/main/resources/aura/polyfill/RequestAnimationFrame.js
@@ -21,27 +21,27 @@ Date.now = (Date.now || function () {  // thanks IE8
 });
 
 var lastTime = 0,
-    caf      = 'CancelAnimationFrame',
-    vendors  = ['ms', 'moz', 'webkit', 'o'];
-
-for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = w[vendors[x] + 'RequestAnimationFrame'];
-    window.cancelAnimationFrame  = w[vendors[x] + caf] || w[vendors[x] + caf];
+    vendors = ['ms', 'moz', 'webkit', 'o'];
+for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
 }
 
 if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function(callback, element) {
-        var currTime   = new Date().getTime(),
-            timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-            id         = window.setTimeout(function() {callback(currTime + timeToCall);}, timeToCall);
-
+    window.requestAnimationFrame = function (callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            },
+            timeToCall);
         lastTime = currTime + timeToCall;
         return id;
     };
 }
 
 if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function(id) {
-        window.clearTimeout(id);
+    window.cancelAnimationFrame = function (id) {
+        clearTimeout(id);
     };
 }

f02fd51723258f1bc15ea737bf661922aee44438
Cheng <cheng.li@salesforce.com>
2015-06-15 16:26:38 -0700

    Adding versioning server side tests

---
+++ aura-components/src/test/components/componentTest/versionInServer/versionInServerController.js
@@ -0,0 +1,20 @@
+({
+    updateVersionFromCreatedComponent: function(cmp, evt, helper) {
+        $A.createComponent("auratest:requireWithModel", null, function(newCmp) {
+                newCmp.updateVersionFromClientController();
+                helper.updateVersion(cmp, newCmp.get("v.version"));
+                cmp.set("v.actionDone", true);
+            });
+    },
+
+    updateVersionFromSameNamespaceServerController: function(cmp, evt, helper) {
+        var action = cmp.get("c.getContextAccessVersion");
+        action.setCallback(this, function(action){
+            if(action.getState() === "SUCCESS") {
+                helper.updateVersion(cmp, action.returnValue);
+                cmp.set("v.actionDone", true);
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})

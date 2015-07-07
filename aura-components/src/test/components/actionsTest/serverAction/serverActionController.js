88821b7b1b82095b64ba378edcdd41e3b3cd24c3
Cheng <cheng.li@salesforce.com>
2015-07-07 10:09:30 -0700

    Adding a test to verify calling component descriptor existing in server action
    @bug W-2612157@
    @rev lhong@

--- aura-components/src/test/components/actionsTest/serverAction/serverActionController.js
+++ aura-components/src/test/components/actionsTest/serverAction/serverActionController.js
@@ -1,14 +1,14 @@
 ({
-	cExecuteInForeground : function(component) {
-		var serverAction = component.get("c.executeInForeground");
-		
-		serverAction.setCallback(this, function(action) {
-			//no-op
-		});
-		
-		$A.enqueueAction(serverAction);
-	},
-        cErrorInForeground : function(component) {
+    cExecuteInForeground : function(component) {
+        var serverAction = component.get("c.executeInForeground");
+
+        serverAction.setCallback(this, function(action) {
+            //no-op
+        });
+
+        $A.enqueueAction(serverAction);
+    },
+    cErrorInForeground : function(component) {
              var serverAction = component.get("c.errorInForeground");
              serverAction.setCallback(this, function(action) {
                  component.set("v.errorMessage", action.error[0].message);
@@ -16,32 +16,45 @@
 
              $A.enqueueAction(serverAction);
         },
-	cExecuteInForegroundWithReturn : function(component) {
-		var serverAction = component.get("c.executeInForegroundWithReturn");
-		serverAction.setParams({ i : 0 });
-		serverAction.setCallback(this, function(action) {
-			//no-op
-		});
-		
-		$A.enqueueAction(serverAction);
-	},
-	cExecuteInBackground : function(component) {
-		var serverAction = component.get("c.executeInBackground");
-		
-		serverAction.setCallback(this, function(action) {
-			//no-op
-		});
-		
-		$A.enqueueAction(serverAction);
-	},
-	cExecuteInBackgroundWithReturn : function(component) {
-		var serverAction = component.get("c.executeInBackgroundWithReturn");
-		serverAction.setParams({ i : 0 });
-		
-		serverAction.setCallback(this, function(action) {
-			//no-op
-		});
-		
-		$A.enqueueAction(serverAction);
-	}
+    cExecuteInForegroundWithReturn : function(component) {
+        var serverAction = component.get("c.executeInForegroundWithReturn");
+        serverAction.setParams({ i : 0 });
+        serverAction.setCallback(this, function(action) {
+            //no-op
+        });
+
+        $A.enqueueAction(serverAction);
+    },
+    cExecuteInBackground : function(component) {
+        var serverAction = component.get("c.executeInBackground");
+
+        serverAction.setCallback(this, function(action) {
+            //no-op
+        });
+
+        $A.enqueueAction(serverAction);
+    },
+    cExecuteInBackgroundWithReturn : function(component) {
+        var serverAction = component.get("c.executeInBackgroundWithReturn");
+        serverAction.setParams({ i : 0 });
+
+        serverAction.setCallback(this, function(action) {
+            //no-op
+        });
+
+        $A.enqueueAction(serverAction);
+    },
+
+    updateTextWithCallingDescrptor : function(component) {
+        var action = component.get("c.currentCallingDescriptor");
+        action.setCallback(component, function(result) {
+            var state = result.getState();
+            if(state === "SUCCESS") {
+                component.set("v.text", result.getReturnValue());
+            } else if(state === "INCOMPLETE" || state === "ERROR") {
+                throw new $A.auraError("Failed to get calling descriptor from server: "+result.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    }
 })

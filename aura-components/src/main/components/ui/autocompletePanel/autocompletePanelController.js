36d2e143b0578b1e2f93c0ae551ad64868ec0747
Stephen <stephen.woods@salesforce.com>
2015-07-29 23:06:39 -0700

    autocomplete Panel Not actually a panel, mostly to simplfiy things for when the autocomplete communicates with autocomplete list. current limitation: scrolling the container will cause the panel to lose contact with input @rev jparadis@ @bug W-2680195@

---
+++ aura-components/src/main/components/ui/autocompletePanel/autocompletePanelController.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2013 salesforce.com, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+({
+
+	handleZIndex: function(cmp, evt, helper) {
+		cmp.getElement().style.zIndex = cmp.get('v.zIndex');
+	},
+
+	handleReferenceElement: function(cmp, evt, helper) {
+		
+		var elem = cmp.getElement();
+		var referenceElem = cmp.get('v.referenceElement');
+		elem.offsetHeight;
+		if(!cmp.positionConstraint) {
+			cmp.positionConstraint = helper.lib.panelPositioning.createRelationship({
+	            element:elem,
+	            target:referenceElem,
+	            appendToBody: true,
+	            align: 'left top',
+	            targetAlign: 'left bottom'
+        	});
+		}
+
+		//some kind of race condtion here, look at tomorrow, brain failing
+		helper.lib.panelPositioning.reposition();
+		setTimeout(function() {
+			helper.lib.panelPositioning.reposition();
+		}, 100);
+		
+	}
+})

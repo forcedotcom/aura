8cae526eee815632bf66019d9a85541daf197a3e
Nihar <ndandekar@salesforce.com>
2015-10-15 14:42:52 -0700

    xunit code coverage aura-components

--- aura-util/src/test/tools/xUnit/dependencies/ScriptLoadStrategy.JSCover.js
+++ aura-util/src/test/tools/xUnit/dependencies/ScriptLoadStrategy.JSCover.js
@@ -16,29 +16,15 @@ Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover=function () {
         var fullPath=System.IO.Path.GetFullPath(path);
         var script=System.IO.File.GetFile(fullPath);
         if (script==null)throw new Error(String.Format("System.Script.ScriptLoadStrategy.JsCover.ImportJson: There was an error loading '{0}'.\nError: File not found.", fullPath));
-        var tempPath=System.IO.Path.GetFullPath("ScriptLoadStrategy.ImportJson.RESULT");
+        var tempPath=System.IO.Path.GetFullPath(path.replace(new RegExp(System.IO.Path.DirectorySeparator, "gm"), "_"));
         System.IO.File.SaveFile(System.IO.Path.GetFullPath(tempPath),String.Format("Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover.RESULT=({0});",script));
         return mockGetFile(this,function(){
-            try{
-                System.Environment.Write("\n**********: 1111111111 \n");
-                script=System.IO.File.GetFile(tempPath).replace(new RegExp(tempPath,"gm"),path);
-                System.Environment.Write("\n**********: 2222222222 \n");
-                System.Environment.Write("\n ----------"+String.Format("{0}\n//@ sourceURL={1}",script,path.replace(/\s/g, '_'))+"----------\n");
-                _loadStrategy.Load(String.Format("{0}\n//@ sourceURL={1}",script,path.replace(/\s/g, '_')));
-                System.Environment.Write("\n**********: 3333333333 \n");
-                System.IO.File.DeleteFile(tempPath);
-                System.Environment.Write("\n**********: 4444444444 \n");
-                var result=Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover.RESULT;
-                System.Environment.Write("\n**********: 5555555555 \n");
-                delete Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover.RESULT;
-                System.Environment.Write("\n**********: 6666666666 \n");
-                if(callback)callback(path, result);
-                System.Environment.Write("\n**********: 7777777777 \n");
-            }
-            catch(e){
-                System.Environment.Write("\n**********:"+path+"\n");
-                System.Environment.Write(e+"\n");
-            }
+            script=System.IO.File.GetFile(tempPath).replace(new RegExp(tempPath,"gm"),path);
+            _loadStrategy.Load(String.Format("{0}\n//@ sourceURL={1}",script,path.replace(/\s/g, '_')));
+            System.IO.File.DeleteFile(tempPath);
+            var result=Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover.RESULT;
+            delete Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover.RESULT;
+            if(callback)callback(path, result);
         });
     };
 
@@ -55,16 +41,13 @@ Test.Tools.Aura.Script.ScriptLoadStrategy.JsCover=function () {
     function mockGetFile(strategy,during){
         return Mocks.GetMock(System.IO.File,"GetFile",function getMock(path){
             var script="";
-            if(Object.Global().system&&Object.Global().snarf){System.Environment.Write("\n**********: 8888888888 \n");
-                //System.Environment.Write("\n\n SM:"+String.Format("\"java -jar '{0}' '{1}' -io\"", System.IO.Path.GetFullPath(strategy.JarPath),path)+"\n");
+            if(Object.Global().system&&Object.Global().snarf){
                 // SM
                 script=System.Environment.Execute("sh", ["-c",String.Format("\"java -jar '{0}' '{1}' -io\"", System.IO.Path.GetFullPath(strategy.JarPath),path)]);
-            }else{System.Environment.Write("\n**********: 9999999999 \n");
-                //System.Environment.Write("\n\n V8:"+String.Format("java -jar \"{0}\" \"{1}\" -io", System.IO.Path.GetFullPath(strategy.JarPath), path)+"\n");
-                // V8
+            }else{
+                // V8 and Rhino
                 script = System.Environment.Execute("sh", ["-c", String.Format("java -jar \"{0}\" \"{1}\" -io", System.IO.Path.GetFullPath(strategy.JarPath), path)]);
-            }System.Environment.Write("\n**********: 0000000000 \n");
-            //System.Environment.Write("\n\n Instrumented code <<<<<<<<<<<<<<<<<<< path:" + path + "\n" +script+" >>>>>>>>>> End\n\n");
+            }
             return script;
         })(during);
     }

75c91218f6251817a7c5f7b660edc138fc90a32c
Cheng <cheng.li@salesforce.com>
2015-10-21 12:57:32 -0700

    Move errors UI tests to same package.
    @W-2520024@

--- aura/src/test/java/org/auraframework/test/error/ComponentClassErrorUITest.java
+++ aura/src/test/java/org/auraframework/test/error/ComponentClassErrorUITest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.auraframework.integration.test.components;
+package org.auraframework.test.error;
 
 import java.util.List;
 

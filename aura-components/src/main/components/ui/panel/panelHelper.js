a6089fa3bf405a8e2f0421f2ece152c9a1e13847
Sunny Sun <ssun@salesforce.com>
2015-11-02 15:54:44 -0800

    changes for mobile modal

--- aura-components/src/main/components/ui/panel/panelHelper.js
+++ aura-components/src/main/components/ui/panel/panelHelper.js
@@ -1,5 +1,4 @@
-/*
- * Copyright (C) 2013 salesforce.com, inc.
+opyright (C) 2013 salesforce.com, inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

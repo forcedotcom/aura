851110abce419cebfa1243ecbada9229124d1440
JF Paradis <jparadis@salesforce.com>
2015-12-11 01:03:39 +0100

    Fix error message when no controller method found @bug W-2865798@

--- aura-components/src/test/components/actionsTest/clientActionNoServerController/clientActionNoServerControllerTest.js
+++ aura-components/src/test/components/actionsTest/clientActionNoServerController/clientActionNoServerControllerTest.js
@@ -6,7 +6,7 @@
      */
     testGetNonExistentAction : {
         test : function(cmp) {
-            var errorMsg = "Cannot read property '$getActionDef$' of undefined";
+            var errorMsg = "Assertion Failed!: Unknown controller action 'notHereCaptain' : undefined";
             try {
                 var action = cmp.get("c.notHereCaptain");
                 $A.test.fail("Attemping to get a non-existent client side controller action should have thrown error.");

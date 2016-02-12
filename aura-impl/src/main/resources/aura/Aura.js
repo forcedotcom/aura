d7d705dec78cd5e980f4aa38c8261ce69c1bbc81
Caridy <cpatino@salesforce.com>
2016-02-12 14:15:43 -0500

    locker iframe is an internal detail of the locker implementation, and do not need to be created in user-land via templates, instead, we create it manually during initAsync() process, this will open the door for, in the future, change the internals of the safe-evaluation without requiring code changes at the app level. Of course, this introduce a very small perf penalty that can be mitigated by the cache manifest and services workers.

--- aura-impl/src/main/resources/aura/Aura.js
+++ aura-impl/src/main/resources/aura/Aura.js
@@ -542,7 +542,9 @@ AuraInstance.prototype.initAsync = function(config) {
     function createAuraContext() {
         // Context is created async because of the GVPs go though async storage checks
         $A.context = new Aura.Context.AuraContext(config["context"], function(context) {
-
+            if (!window["$$safe-eval$$"]) {
+                throw new $A.auraError("Aura(): Failed to initialize locker worker.");
+            }
             $A.context = context;
             $A.clientService.initHost(config["host"]);
             $A.setLanguage();
@@ -553,12 +555,31 @@ AuraInstance.prototype.initAsync = function(config) {
         });
     }
 
-    // safe eval worker is an iframe that enables the page to run arbitrary evaluation,
-    // if this iframe is still loading, we should wait for it before continue with
-    // initialization.
     if (!window['$$safe-eval$$']) {
-        $A.util.on(document.getElementById('safeEvalWorker'), 'load', createAuraContext);
+        // safe eval worker is an iframe that enables the page to run arbitrary evaluation,
+        // if this iframe is still loading, we should wait for it before continue with
+        // initialization, in the other hand, if the iframe is not available, we create it,
+        // and wait for it to be ready.
+        var el = document.getElementById('safeEvalWorker');
+        if (!el) {
+            el = document.createElement('iframe');
+            // TODO: we should use `config["context"]["fwuid"]` as a token for cache control
+            el.setAttribute('src', '/auraFW/resources/lockerservice/safeEval.html');
+            el.setAttribute('width', "0");
+            el.setAttribute('height', "0");
+            el.setAttribute('tabIndex', "-1");
+            el.setAttribute('aria-hidden', "true");
+            el.setAttribute('title', "scripts");
+            el.style.display = 'none';
+            document.body.appendChild(el);
+        }
+        $A.util.on(el, 'load', createAuraContext);
+        $A.util.on(el, 'error', function () {
+            throw new $A.auraError("Aura(): Failed to load locker worker.");
+        });
     } else {
+        // provision for an alternative safe evaluation. This will open the door to do some
+        // performance optimization.
         createAuraContext();
     }
 };

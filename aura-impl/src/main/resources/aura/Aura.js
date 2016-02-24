576edff47ca91b67f271b10d702f2a732e66d46a
Trevor <tbliss@salesforce.com>
2016-02-23 17:33:55 -0800

    Only load Locker worker iframe if host is not full path
    @rev cpatino@
    @bug W-2943804@

--- aura-impl/src/main/resources/aura/Aura.js
+++ aura-impl/src/main/resources/aura/Aura.js
@@ -550,11 +550,10 @@ AuraInstance.prototype.getCurrentTransactionId = function() { return undefined; 
  * @public
  */
 AuraInstance.prototype.initAsync = function(config) {
-
     function createAuraContext() {
         // Context is created async because of the GVPs go though async storage checks
         $A.context = new Aura.Context.AuraContext(config["context"], function(context) {
-            if (!window["$$safe-eval$$"]) {
+            if (!window["$$safe-eval$$"] && !regexpDetectURLProcotolSegment.test(config["host"])) {
                 throw new $A.auraError("Aura(): Failed to initialize locker worker.");
             }
             $A.context = context;

8b080cdb42d52d8fd1e0572fb5e52dff78cbcc4b
Shaocong Mo <smo@salesforce.com>
2016-02-26 09:51:34 -0800

    Fix inputSmartNumberTest @bug W-2940075@ @rev reinier.guerra@

--- aura-components/src/main/components/ui/inputSmartNumber/inputSmartNumberTest.js
+++ aura-components/src/main/components/ui/inputSmartNumber/inputSmartNumberTest.js
@@ -14,65 +14,63 @@
  * limitations under the License.
  */
 ({
-    /**
-     * Test currency formatted correctly.
+    /*
+     * Pass nothing to component
      */
-    testCurrency: {
-        attributes: {value: 1234 },
+    testUnassigned: {
         test: function (component) {
-            var value = component.getElement().value;
-            $A.test.assertEquals(1234, component.get("v.value"), "Cmp value does not equal expected");
-            $A.test.assertEquals("$1,234.00", value, "Element value does not equal expected");
+            this.assertValue(component, undefined, "");
         }
     },
 
-    /**
-     * Test currency formatted correctly with custom format.
+    /*
+     * Test 0 to catch if(number) bug
      */
-    testCurrencyWithFormat: {
-        attributes: {value: 1234, format: "$#,###.0000"},
+    testZero: {
+        attributes: {value: 0},
         test: function (component) {
-            var value = component.getElement().value;
-            $A.test.assertEquals(1234, component.get("v.value"), "Cmp value does not equal expected");
-            $A.test.assertEquals("$1,234.0000", value, "Element value does not equal expected");
+            this.assertValue(component, "0", "");
         }
     },
 
-    /**
-     * Verify that when the value changes it is re-rendered with the non-formatted new value
+    /*
+     * Test general number
      */
-    testUpdateValue: {
-        attributes: {value: 1234, format: "$#,###.0000"},
-        test: [
-            function (cmp) {
-                var value = cmp.getElement().value;
-                $A.test.assertEquals(1234, cmp.get("v.value"), "Cmp value does not equal expected");
-                $A.test.assertEquals("$1,234.0000", value, "Element value does not equal expected");
-            },
-            function (cmp) {
-                cmp.set("v.value", 5678);
-                $A.test.assertEquals(5678, cmp.get("v.value"), "Cmp value does not equal expected");
+    testNumber: {
+        attributes: {value: 12345.67},
+        test: function (component) {
+            this.assertValue(component, "12345.67", "12,345.67");
+        }
+    },
 
-                $A.test.addWaitFor("$5,678.0000", function () {
-                    return cmp.helper.getInputElement(cmp).value;
-                });
-            }]
+    /*
+     * Test negative number
+     */
+    testNegativeNumber: {
+        attributes: {value: -12345.67},
+        test: function (component) {
+            this.assertValue(component, "-12345.67", "-12,345.67");
+        }
     },
 
     /**
      * Verify that when the format changes it is not re-rendered with the new format
      */
     testUpdateFormat: {
-        attributes: {value: 1234, format: "$#,###.0000"},
-        test: function (component) {
-            var value = component.getElement().value;
-            $A.test.assertEquals(1234, component.get("v.value"), "Cmp value does not equal expected");
-            $A.test.assertEquals("$1,234.0000", value, "Element value does not equal expected");
-            component.set("v.format", '£#,###.00');
-            $A.rerender(component);
-            value = component.getElement().value;
-            $A.test.assertEquals(1234, component.get("v.value"), "Cmp value does not equal expected");
-            $A.test.assertEquals("$1,234.0000", value, "Element value does not equal expected");
-        }
+        attributes: {value: 1234, format: "#,###.0000"},
+        test: [function (component) {
+            this.assertValue(component, "1234", "1,234.0000");
+            component.set("v.format", '#,##.00');
+        }, function (component) {
+            this.assertValue(component, "1234", "1,234.0000");
+        }]
+    },
+
+    // check component's internval v.value and displayed value on the input box
+    assertValue: function (component, expectedComponentValue, expectedElementValue) {
+        $A.test.assertEquals(expectedComponentValue, component.get("v.value"),
+                "Cmp value does not equal expected");
+        $A.test.assertEquals(expectedElementValue, component.getElement().value,
+                "Element value does not equal expected");
     }
 })// eslint-disable-line semi

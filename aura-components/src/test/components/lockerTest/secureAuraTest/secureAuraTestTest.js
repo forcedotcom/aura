b15a883435eaeeecfc6520e913ea411818119de8
Trevor <tbliss@salesforce.com>
2016-03-15 19:37:19 -0700

    Adding SecureAura tests that mysteriously were not included in previous commit

--- aura-components/src/test/components/lockerTest/secureAuraTest/secureAuraTestTest.js
+++ aura-components/src/test/components/lockerTest/secureAuraTest/secureAuraTestTest.js
@@ -4,15 +4,6 @@
      * to get objects and then return them to the test for verification.
      */
 
-    // TODO(tbliss): WIP - not much exposed on SecureAura yet
-    testGetComponentReturnsSecureComponent: {
-        test: function(cmp) {
-            cmp.getSecureAura();
-            var wrapped = cmp.get("v.log");
-            
-        }
-    },
-
     // Many things are not exposed on SecureAura, but this one was specifically called out so writing automation for it
     testStorageServiceNotDefined: {
         test: function(cmp) {
@@ -46,7 +37,8 @@
      * 
      * This also exercises $A.createComponents (plural) to create a set of components rather than a single one.
      */
-    testDynamicallyCreatedComponentDifferentNamespaceIsSecureComponentRef: {
+    // TODO(W-2973793): $A.createComponent should return created component in the callback as SecureComponent(Ref) instead of the raw Component
+    _testDynamicallyCreatedComponentDifferentNamespaceIsSecureComponentRef: {
         test: [function(cmp) {
             cmp.dynamicallyCreateCmpsDifferentNamespace();
             $A.test.addWaitFor(true, function() {
@@ -54,16 +46,17 @@
             });
         }, function(cmp) {
             cmp.getSecureComponent();
-            var secureAura = cmp.get("v.log");
-            var dynamicCmps = secureAura.get("v.dynamicCmps");
+            var secureComponenet = cmp.get("v.log");
+            var dynamicCmps = secureComponenet.get("v.dynamicCmps");
             dynamicCmps.forEach(function(component) {
                 $A.test.assertStartsWith("SecureComponentRef", component.toString(), "Expected dynamic component to be"
                         + " a SecureComponentRef");
             });
         }]
     },
-    
-    testDynamicallyCreatedComponentSameNamespaceIsSecureComponent: {
+
+    // TODO(W-2973793): $A.createComponent should return created component in the callback as SecureComponent(Ref) instead of the raw Component
+    _testDynamicallyCreatedComponentSameNamespaceIsSecureComponent: {
         test: [function(cmp) {
             cmp.dynamicallyCreateCmpSameNamespace();
             $A.test.addWaitFor(true, function() {
@@ -71,13 +64,72 @@
             });
         }, function(cmp) {
             cmp.getSecureComponent();
-            var secureAura = cmp.get("v.log");
-            var dynamicCmps = secureAura.get("v.dynamicCmps");
+            var secureComponenet = cmp.get("v.log");
+            var dynamicCmps = secureComponenet.get("v.dynamicCmps");
             debugger;
             dynamicCmps.forEach(function(component) {
                 $A.test.assertStartsWith("SecureComponent", component.toString(), "Expected dynamic component to be"
                         + " a SecureComponent");
             });
         }]
+    },
+
+    testGetRootReturnsSecureComponent: {
+        test: function(cmp) {
+            cmp.getSecureAura();
+            var secureAura = cmp.get("v.log");
+            $A.test.assertStartsWith("SecureComponent", secureAura.getRoot().toString(), "Expected $A.getRoot() to return"
+                    + " a SecureComponent");
+        }
+    },
+    
+    // TODO(W-2974238): issues with $A.enqueueAction
+    _testEnqueueAction: {
+        test: function(cmp) {
+            cmp.callEnqueueAction();
+        }
+    },
+
+    testGetCallback: {
+        test: function(cmp) {
+            cmp.callGetCallback();
+            $A.test.addWaitFor(true, function() {
+                return !!cmp.get("v.log");
+            }, function() {
+                // log returns [this, window, document] from $A.getCallback call
+                var log = cmp.get("v.log");
+                $A.test.assertStartsWith("SecureWindow", log[0].toString(), "Expected SecureWindow as context to $A.getCallback");
+                $A.test.assertStartsWith("SecureWindow", log[1].toString(), "Expected SecureWindow for window in $A.getCallback");
+                $A.test.assertStartsWith("SecureDocument", log[2].toString(), "Expected SecureDocument as document in $A.getCallback");
+            });
+        }
+    },
+
+    testGetGVP: {
+        test: function(cmp) {
+            cmp.getGVP();
+            var browserGvp = cmp.get("v.log");
+            // check that a couple things on $Browser are there to verify it's the correct object
+            $A.test.assertDefined(browserGvp["formFactor"]);
+            $A.test.assertDefined(browserGvp["isPhone"]);
+        }
+    },
+    
+    testGetDifferentNamespaceComponentReturnsSecureComponentRef: {
+        test: function(cmp) {
+            cmp.callGetComponentDifferentNamespace();
+            var component = cmp.get("v.log");
+            $A.test.assertStartsWith("SecureComponentRef", component.toString(), "Expected $A.getComponent on a component"
+                    + " from another namespace to be a SecureComponentRef");
+        }
+    },
+    
+    testGetSameNamespaceComponentReturnsSecureComponent: {
+        test: function(cmp) {
+            cmp.callGetComponentSameNamespace();
+            var component = cmp.get("v.log");
+            $A.test.assertStartsWith("SecureComponent", component.toString(), "Expected $A.getComponent on a component"
+                    + " from the same namespace to be a SecureComponent");
+        }
     }
 })

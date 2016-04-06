5a9cf20523867370984245fb12a204a9317fbeff
Cheng <cheng.li@salesforce.com>
2016-04-05 17:36:19 -0700

    Dynamic label tests
    @rev tbliss@
    @bug W-2966246@

--- aura-components/src/test/components/gvpTest/dynamicLabels/dynamicLabelsTest.js
+++ aura-components/src/test/components/gvpTest/dynamicLabels/dynamicLabelsTest.js
@@ -56,7 +56,7 @@
             );
         }
     },
-    
+
     /**
      * Dynamically create component on server and verify labels declared dependency cmp of created component are present.
      */
@@ -90,51 +90,56 @@
             );
         }
     },
-    
-    /**
-     * Prefetch label using GVP and load a new component with the label.
-     */
-    testFetchLabelAndLoadComponentWithLabel:{
-        test:[
-          //Fetch the label using gvp, which causes a server action
-          function(cmp){
-            var tempLabel = $A.get("$Label" + ".Related_Lists" + ".task_mode_today");
-            $A.test.assertEquals("[Related_Lists.task_mode_today]", tempLabel);
-            $A.test.assertTrue($A.test.isActionPending(),
-                    "Test Setup Failure: test expects the label to be fetched from server");
-            $A.test.addWaitFor(
-                        false,
-                        $A.test.isActionPending,
-                        function(){
-                            //Verify that label is available
-                            $A.test.assertEquals("Today", $A.get("$Label.Related_Lists.task_mode_today"),
-                                "Failed to fetch label from server dynamically");
-                        }
-                    );
-        },
-        //Load a new component which refers to same label
-        function(cmp){
-            $A.componentService.newComponentAsync(
-                    this,
-                    function(newCmp){
-                        cmp.find("container").set("v.body", newCmp);
-                    },
-                    {
-                        componentDef: "gvpTest:newLabels"
-                    }
-                );
-            $A.test.addWaitFor(
-                        false,
-                        $A.test.isActionPending,
-                        function(){
-                            //Nothing should have changed
-                            $A.test.assertEquals("Today", $A.get("$Label.Related_Lists.task_mode_today"),
-                                "Failed to fetch label");
-                            //New label should have been merged into LabelValueProvider
-                            $A.test.addWaitForWithFailureMessage("Today + Overdue", function(){return $A.get("$Label.Related_Lists.task_mode_today_overdue")},
-                                   "Failed to merge labels from dynamically created components");
-                        }
-                    );
-        }]
+
+    testLoadDynamicLabelFromServer: {
+        test: function(cmp) {
+            var actualLabel;
+            var labelSection = ".Related_Lists";
+            var labelName = ".task_mode_today";
+            var placeholder = $A.get("$Label" + labelSection + labelName, function(label) {
+                actualLabel = label;
+            });
+            // if the label doesn't exist in client, return a placeholder with section and name
+            // if this fails, perhaps the label is preloaded to client.
+            $A.test.assertEquals("[Related_Lists.task_mode_today]", placeholder);
+
+            $A.test.addWaitFor(false, $A.test.isActionPending,
+                function(){
+                    $A.test.assertEquals("Today", actualLabel, "Failed to fetch label from server.");
+                });
+        }
+    },
+
+    testLabelExistsOnClientAfterLoadedFromServer: {
+        test: function(cmp){
+            var labelName = "task_mode_today";
+            var placeholder = $A.get("$Label.Related_Lists." + labelName);
+            $A.test.assertEquals("[Related_Lists.task_mode_today]", placeholder);
+
+            $A.test.addWaitFor(false, $A.test.isActionPending,
+                function(){
+                    // the server action is done. it means the label is loaded to client.
+                    var actualLabel = $A.get("$Label.Related_Lists." + labelName);
+                    $A.test.assertEquals("Today", actualLabel,
+                            "The label doesn't exist in client LabelValueProvider.");
+                });
+        }
+    },
+
+    testLabelIsLoadedToClientWhenCreatingComponent: {
+        test: function(cmp){
+                $A.createComponent("gvpTest:newLabels",null,function(newCmp, state) {
+                        $A.test.assertEquals("SUCCESS", state);
+                    });
+
+                $A.test.addWaitFor(false, $A.test.isActionPending,
+                    function() {
+                        // since the label is loaded to client when creating component,
+                        // the label should be directly returned.
+                        var actualLabel = $A.get("$Label.Related_Lists.task_mode_today_overdue");
+                        $A.test.assertEquals("Today + Overdue", actualLabel,
+                                "The label doesn't exist in client LabelValueProvider.");
+                    });
+        }
     }
 })

ea2e244efb929ffa6ec06a0060c4b9c6ac09c6b4
Reinier <reinier.guerra@salesforce.com>
2016-05-17 10:52:26 -0700

    inputFile clean up

--- aura-components/src/test/components/uitest/inputFile_Test/inputFile_TestController.js
+++
@@ -1,9 +0,0 @@
-({
-    handleChange : function (cmp, event, helper) {
-        cmp.set('v.count',cmp.get('v.count') + 1);
-        cmp.set('v.lastChangeEvent', event);
-    },
-    clear : function (cmp, event, helper) {
-        cmp.find('inputFile').reset();
-    }
-})

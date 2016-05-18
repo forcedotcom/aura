18ecc6d2a8d937fdfe7d1da52e615cdc1773a631
Gordon <goliver@salesforce.com>
2016-05-18 09:51:10 -0400

    Fix up case for SecureElement
    @bug W-3156406@
    
    @rev dchasman@

--- aura-components/src/test/components/lockerTest/secureElementTest/secureElementTestController.js
+++ aura-components/src/test/components/lockerTest/secureElementTest/secureElementTestController.js
@@ -4,6 +4,7 @@
         var auraId = event.getParam("arguments").auraId;
         var elementPropertiesWhitelist = event.getParam("arguments").elementPropertiesWhitelist;
         var elementProperitesBlacklist = event.getParam("arguments").elementPropertiesBlacklist;
+                  debugger;
         var element = cmp.find(auraId).getElement();
 
         elementPropertiesWhitelist.forEach(function(name) {

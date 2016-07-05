d536086f24b0ef6282ab7d777cbf7838920799a4
tbliss <tbliss@salesforce.com>
2016-07-05 14:27:51 -0700

    LockerService - only add geolocation to navigator if exists
    @bug W-3221153@
    @rev goliver@

--- aura-components/src/test/components/lockerTest/secureNavigatorTest/secureNavigatorTestController.js
+++ aura-components/src/test/components/lockerTest/secureNavigatorTest/secureNavigatorTestController.js
@@ -1,15 +1,15 @@
 ({  
     testPropertiesExposed: function(cmp) {
         var testUtils = cmp.get("v.testUtils");
-        
+
         ["appCodeName", "appName", "appVersion", "cookieEnabled", "geolocation", 
          "language", "onLine", "platform", "product", "userAgent"].forEach(function(name) {
-        	testUtils.assertTrue(name in window.navigator, "Expected window.navigator." + name + " to be exposed as a property");
-    	});
+            testUtils.assertTrue(name in window.navigator, "Expected window.navigator." + name + " to be exposed as a property");
+         });
     },
-    
+
     testLanguage: function(cmp) {
         var testUtils = cmp.get("v.testUtils");
-    	testUtils.assertEquals("en-us", window.navigator.language.toLowerCase(), "Unexpected window.navigator.language value");
+        testUtils.assertEquals("en-us", window.navigator.language.toLowerCase(), "Unexpected window.navigator.language value");
     }
 })

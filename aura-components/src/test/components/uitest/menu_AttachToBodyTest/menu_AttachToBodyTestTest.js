f4ead6ed1ba1ec474918cba6c5e42e95fcd24e3d
Shaocong Mo <smo@salesforce.com>
2016-07-12 09:47:59 -0700

    menu_AttachToBodyTest flapper fix
    @rev tbliss@
    @bug W-3225570@

--- aura-components/src/test/components/uitest/menu_AttachToBodyTest/menu_AttachToBodyTestTest.js
+++ aura-components/src/test/components/uitest/menu_AttachToBodyTest/menu_AttachToBodyTestTest.js
@@ -14,70 +14,86 @@
  * limitations under the License.
  */
 ({
-    clickAnchor: function (trigger) {
-        var anchor = trigger.getElement().getElementsByTagName("a")[0];
-        anchor.click();
-    },
     /**
-     * Test to verify action menu works when interacting with the menu items 
+     * Test to verify action menu works when interacting with the menu items
      * which has attachToBody attribute set
-     * using AURA API  
+     * using AURA API
      * TODO: Disabling test in IE7&8, bug W-2320774
      * Flapping in Safari
      */
-    testActionMenuWithAttachToBodySet:{
-        labels : ["flapper"],
-        browsers: ["-IE7","-IE8", "-SAFARI"],
+    testActionMenuWithAttachToBodySet: {
+        browsers: ["-IE7", "-IE8", "-SAFARI"],
         test: [function(cmp) {
                 actionMenu = cmp.find("actionMenuAttachToBody");
                 menuLabel = cmp.find("triggerAttachToBody");
-                item1 = "actionItemAttachToBody1";
-                item2 = "actionItemAttachToBody2";
-                item3 = "actionItemAttachToBody3";
-                
+                item1 = cmp.find("actionItemAttachToBody1");
+                item2 = cmp.find("actionItemAttachToBody2");
+                item3 = cmp.find("actionItemAttachToBody3");
+
                 //check menu is default to hidden by using AURA API
                 $A.test.assertFalse(actionMenu.get('v.visible'),"Action Menu should not be visible");
-                
+
                 //check menu is default to hidden by using DOM API
                 $A.test.assertTrue($A.util.hasClass(actionMenu.getElement(),"uiMenuList"), "Class name should be just uiMenuList");
                 $A.test.assertFalse($A.util.hasClass(actionMenu.getElement(),"visible"), "Class name should not contain visible");
+
+                //open menu
                 this.clickAnchor(menuLabel);
+                this.waitForMenuVisible(actionMenu, true);
+            }, function() {
                 //Check if secondItem in the menu is disabled
-                $A.test.addWaitForWithFailureMessage(true, function(){return cmp.find(item2).get("v.disabled");}, "Check if Item2 in the menu is disabled");
-            }, function(cmp) {
+                $A.test.assertTrue(this.isMenuItemDisabled(item2), "Menu item 2 should be disabled");
+                $A.test.assertFalse(this.isMenuItemDisabled(item1), "Menu item 1 should be clickable");
+
+                //disable ActionItem1
+                item1.set("v.disabled", true);
+                $A.test.addWaitForWithFailureMessage(true, function() {
+                    return item1.get("v.disabled");
+                }, "Menu item 1 should not be clickable");
+            }, function() {
                 //make sure menuItem is attached to body directly and its not attached to uiMenu instead
                 //Test case for W-2181713
                 var actionMenuParentElement = actionMenu.getElement().parentNode;
                 var bodyElement = document.getElementsByTagName("body")[0];
-                $A.test.assertEquals(actionMenuParentElement, bodyElement,"Menu Item List should be attached to body");
-             }, function(cmp) {   
+                $A.test.assertEquals(actionMenuParentElement, bodyElement,
+                        "Menu Item List should be attached to body");
+
                 //Make sure horizontal alignment of menuItem is correct with reference to triggerElement
                 $A.test.addWaitForWithFailureMessage(true, function() {
-                var actionMenuLeftPostionValue = parseInt($A.test.getStyle(actionMenu.getElement(),"left"));
-                var triggerLeftPositonValue = Math.floor(menuLabel.getElement().getBoundingClientRect().left);
-                    return actionMenuLeftPostionValue === triggerLeftPositonValue;
+                    var actionMenuLeftPositionValue = parseInt($A.test.getStyle(actionMenu.getElement(),"left"));
+                    var triggerLeftPositionValue = Math.floor(menuLabel.getElement().getBoundingClientRect().left);
+                    return actionMenuLeftPositionValue === triggerLeftPositionValue;
                 }, "Menu Item is not alligned properly wrt to trigger it should be at left");
-                    
-                var disableAttrValue = cmp.find(item1).get("v.disabled");
-                $A.test.assertFalse(disableAttrValue,"Menu item 1 should be clickable");
-                
-                //check menu is visible by using AURA API
-                $A.test.assertTrue(actionMenu.get('v.visible'),"Menu should be visible");
-                
-                $A.test.assertTrue($A.util.hasClass(actionMenu.getElement(),"visible"), "Class name should be uiMenuList visible");
-                
-                //disable ActionItem1
-                cmp.find(item1).set("v.disabled", true);
-                var disableAttrValue = cmp.find(item1).get("v.disabled");
-                $A.test.assertTrue(disableAttrValue,"Menu item 1 should not be clickable");
-                
-                //click actionItem3 and check if label is updated
-                this.clickAnchor(cmp.find(item3));
-                $A.test.addWaitForWithFailureMessage(cmp.find(item3).get('v.label'), function(){return menuLabel.get('v.label')}, "Label should be updated to "+ cmp.find(item3).get('v.label'));
-            }, function(cmp) {
-                $A.test.assertFalse(actionMenu.get('v.visible'),"Menu should not be visible");
+            }, function() {
+                //click actionItem3, menu should close
+                this.clickAnchor(item3);
+                this.waitForMenuVisible(actionMenu, false);
+
+                //check if label is updated to item3
+                $A.test.addWaitForWithFailureMessage(item3.get('v.label'), function(){
+                    return menuLabel.get('v.label');
+                }, "Label should be updated to "+ item3.get('v.label'));
             }
         ]
+    },
+
+    getAnchor: function(trigger) {
+        return trigger.getElement().getElementsByTagName("a")[0];
+    },
+
+    clickAnchor: function (trigger) {
+        $A.test.clickOrTouch(this.getAnchor(trigger));
+    },
+
+    isMenuItemDisabled: function(menuItem) {
+        return menuItem.get("v.disabled") &&
+                this.getAnchor(menuItem).getAttribute("aria-disabled") === "true";
+    },
+
+    waitForMenuVisible: function(menu, isVisible) {
+        $A.test.addWaitForWithFailureMessage(true, function() {
+            return menu.get("v.visible") === isVisible &&
+                    $A.util.hasClass(menu.getElement(), "visible") === isVisible;
+        }, "Menu should" + (isVisible ? " " : " not ") + "be visible");
     }
-    
-})
+})//eslint-disable-line

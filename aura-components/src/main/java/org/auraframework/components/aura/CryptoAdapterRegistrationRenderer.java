02fea4c2a6d3b2404833615e485cb5cb78fbb38b
Kevin Venkiteswaran <kvenkiteswaran@salesforce.com>
2016-07-27 17:20:15 -0700

    Fix error handling when fetching key for auraStorage:crypto @bug W-3258797@ @rev tbliss@

--- aura-components/src/main/java/org/auraframework/components/aura/CryptoAdapterRegistrationRenderer.java
+++ aura-components/src/main/java/org/auraframework/components/aura/CryptoAdapterRegistrationRenderer.java
@@ -17,8 +17,8 @@ package org.auraframework.components.aura;
 
 import java.io.IOException;
 
-import org.auraframework.annotations.Annotations.ServiceComponentRenderer;
 import org.auraframework.Aura;
+import org.auraframework.annotations.Annotations.ServiceComponentRenderer;
 import org.auraframework.def.Renderer;
 import org.auraframework.instance.BaseComponent;
 import org.auraframework.system.RenderContext;
@@ -29,20 +29,80 @@ import org.auraframework.throwable.quickfix.QuickFixException;
  */
 @ServiceComponentRenderer
 public class CryptoAdapterRegistrationRenderer implements Renderer {
-	private final String INLINE_JS = "\tfunction buildStreamKey (key) {\n\t\tvar buffer = new ArrayBuffer(key.length);\n\t\tvar view = new Uint8Array(buffer);\n\t\tview.set(key);\n\t\treturn buffer;\n\t}\n\n\tfunction fetchKey (url, callback) {\n\t\tvar request = new XMLHttpRequest();\n\t\trequest.addEventListener('load', function(event) {\n\t\t\tvar key, validKey;\n\t\t\ttry {\n\t\t\t\tkey = JSON.parse(this.responseText);\n\t\t\t} catch (e) {};\n\n\t\t\tif (Array.isArray(key) && (key.length === 32 || key.length === 16)) {\n\t\t\t\tcallback(key);\n\t\t\t} else {\n\t\t\t\t$A.log('CryptoAdapter received invalid key; calling CryptoAdapter.setKey()');\n\t\t\t\tcallback();\n\t\t\t}\n\t\t});\n\n\t\trequest.addEventListener('error', function(event) {\n\t\t\t$A.log('CryptoAdapter key fetch errored; calling CryptoAdapter.setKey()');\n\t\t\tcallback();\n\t\t});\n\n\t\trequest.addEventListener('abort', function(event) {\n\t\t\t$A.log('CryptoAdapter key fetch aborted; calling CryptoAdapter.setKey()');\n\t\t\tcallback();\n\t\t});\n\n\t\trequest.open('GET', url, true);\n\t\trequest.send();\n\t}\n\n\tfunction setCryptoKey (key) {\n\t\tvar streamKey = buildStreamKey(key);\n\t\tCryptoAdapter.setKey(streamKey);\n\t}\n\n\tdebug && $A.log('CryptoAdapter registering');\n\tvar CryptoAdapter = $A.storageService.CryptoAdapter;\n\tCryptoAdapter.register();\n\t\n\tif (!$A.storageService.isRegisteredAdapter(CryptoAdapter.NAME)) {\n\t\t$A.log('CryptoAdapter was not registered');\n\t\treturn;\n\t}\n\n\tif (fetch) {\n\t\tfetchKey(url, setCryptoKey);\n\t} else {\n\t\twindow.Aura || (window.Aura = {});\n\n\t\tif (window.Aura.Crypto && window.Aura.Crypto.key) {\n\t\t\tsetCryptoKey(window.Aura.Crypto.key);\n\t\t} else {\n\t\t\tvar t = setTimeout(function () {\n\t\t\t\tif (!window.Aura.Crypto) {\n\t\t\t\t\tdelete window.Aura.afterEncryptionKeyReady;\n\t\t\t\t\tCryptoAdapter.setKey();\n\t\t\t\t}\n\t\t\t}, 3000);\n\n\t\t\tAura.afterEncryptionKeyReady = function () {\n\t\t\t\twindow.clearTimeout(t);\n\t\t\t\tsetCryptoKey(window.Aura.Crypto.key);\n\t\t\t};\n\n\t\t}\n\t}";
-	
+	private final String INLINE_JS =
+        "function setCryptoKey(key) {\n" +
+        "    var buffer, view;\n" +
+        "    if (Array.isArray(key) && (key.length === 32 || key.length === 16)) {\n" +
+        "        buffer = new ArrayBuffer(key.length);\n" +
+        "        view = new Uint8Array(buffer);\n" +
+        "        view.set(key);\n" +
+        "    }\n" +
+        "    CryptoAdapter.setKey(buffer);\n" +
+        "}\n" +
+        "function fetchKey(url, callback) {\n" +
+        "    var request = new XMLHttpRequest();\n" +
+        "    request.addEventListener('load', function(event) {\n" +
+        "        var key, validKey;\n" +
+        "        try {\n" +
+        "            key = JSON.parse(this.responseText);\n" +
+        "        } catch (e) {};\n" +
+        "        if (!Array.isArray(key) || (key.length !== 32 && key.length !== 16)) {\n" +
+        "            debug && $A.log('CryptoAdapter received invalid key; calling CryptoAdapter.setKey()');\n" +
+        "        }\n" +
+        "        callback(key);\n" +
+        "    });\n" +
+        "    request.addEventListener('error', function(event) {\n" +
+        "        debug && $A.log('CryptoAdapter key fetch errored; calling CryptoAdapter.setKey()');\n" +
+        "        callback();\n" +
+        "    });\n" +
+        "    request.addEventListener('abort', function(event) {\n" +
+        "        debug && $A.log('CryptoAdapter key fetch aborted; calling CryptoAdapter.setKey()');\n" +
+        "        callback();\n" +
+        "    });\n" +
+        "    request.open('GET', url, true);\n" +
+        "    request.send();\n" +
+        "}\n" +
+        "debug && $A.log('CryptoAdapter registering');\n" +
+        "var CryptoAdapter = $A.storageService.CryptoAdapter;\n" +
+        "CryptoAdapter.register();\n" +
+        "if (!$A.storageService.isRegisteredAdapter(CryptoAdapter.NAME)) {\n" +
+        "    $A.log('CryptoAdapter was not registered');\n" +
+        "    return;\n" +
+        "}\n" +
+        "if (fetchRemoteKey) {\n" +
+        "    fetchKey(url, setCryptoKey);\n" +
+        "} else {\n" +
+        "    window.Aura || (window.Aura = {});\n" +
+        "    if (window.Aura.Crypto && window.Aura.Crypto.key) {\n" +
+        "        setCryptoKey(window.Aura.Crypto.key);\n" +
+        "    } else {\n" +
+        "        var t = setTimeout(function() {\n" + // TODO W-3258797 replace setTimeout with proper error detection
+        "            if (!window.Aura.Crypto) {\n" +
+        "                delete window.Aura.afterEncryptionKeyReady;\n" +
+        "                CryptoAdapter.setKey();\n" +
+        "            }\n" +
+        "        }, 3000);\n" +
+        "        Aura.afterEncryptionKeyReady = function() {\n" +
+        "            window.clearTimeout(t);\n" +
+        "            setCryptoKey(window.Aura.Crypto.key);\n" +
+        "        };\n" +
+        "    }\n" +
+        "}"
+    ;
+
     @Override
     public void render(BaseComponent<?, ?> component, RenderContext renderContext) throws IOException, QuickFixException {
 
         Boolean debug = (Boolean) component.getAttributes().getValue("debugLoggingEnabled");
         Boolean fetchRemoteKey = (Boolean) component.getAttributes().getValue("fetchRemoteKey");
-        
+
         String encryptionKeyUrl = Aura.getConfigAdapter().getEncryptionKeyURL(false);
         renderContext.pushScript();
         renderContext.getCurrent()
-            .append("(function(debug, fetch, url){\n")
-            .append(INLINE_JS)
-            .append("\n}("+ debug + "," + fetchRemoteKey + ", \"" + encryptionKeyUrl + "\"));");
+                .append("(function(debug, fetchRemoteKey, url){\n")
+                .append(INLINE_JS)
+                .append("\n}(").append(String.valueOf(debug)).append(",").append(String.valueOf(fetchRemoteKey))
+                    .append(", \"").append(encryptionKeyUrl).append("\"));");
         renderContext.popScript();
     }
 }

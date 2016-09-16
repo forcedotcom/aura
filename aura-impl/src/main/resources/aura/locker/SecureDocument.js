8696d2f9324317ce2c03815b8ba89c38b436d935
JF Paradis <jparadis@salesforce.com>
2016-09-16 16:08:59 -0700

    @bug W-3359656@ Locker Service - Remove leftover test code

--- aura-impl/src/main/resources/aura/locker/SecureDocument.js
+++ aura-impl/src/main/resources/aura/locker/SecureDocument.js
@@ -103,7 +103,7 @@ function SecureDocument(doc, key) {
             value: doc[name]
         });
     });
-    
+
     SecureObject.addPrototypeMethodsAndProperties(SecureDocument.metadata, o, doc, key);
 
     setLockerSecret(o, "key", key);
@@ -285,14 +285,8 @@ SecureDocument.metadata = {
             "queryCommandState":                FUNCTION,
             "queryCommandSupported":            FUNCTION,
             "queryCommandValue":                FUNCTION,
-            "querySelector":                    {type: "function",
-                                                 empty: {args: "#none", value: "null"},
-                                                 opaque: {args: "#tic", value: "null"}
-                                                },
-            "querySelectorAll":                 {type: "function",
-                                                 empty: {args: "#none", value: "{}"},
-                                                 opaque: {args: "#tic", value: "{}"}
-                                                },
+            "querySelector":                    FUNCTION,
+            "querySelectorAll":                 FUNCTION,
             "readyState":                       DEFAULT,
             "referrer":                         DEFAULT,
             "registerElement":                  FUNCTION,

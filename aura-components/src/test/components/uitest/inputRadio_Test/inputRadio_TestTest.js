62b30fbdaa7dcb3afa2a3aebadd77dc10ca47aff
Charandeep <ctatlah@salesforce.com>
2016-10-12 09:49:04 -0700

    inputRadio update value property upon losing focus @bug W-3184381@ @rev stephen.woods@

--- aura-components/src/test/components/uitest/inputRadio_Test/inputRadio_TestTest.js
+++ aura-components/src/test/components/uitest/inputRadio_Test/inputRadio_TestTest.js
@@ -14,114 +14,114 @@
  * limitations under the License.
  */
 ({
-	/**
-	 * Test to verify radioItems works when interacting with attribute value directly
-	 * using AURA API  
-	 */
-	owner:"ronak.shah",
-	
-	testToggleRadioMenu:{
-		test: [function(cmp) {
-				var defaultStringValue = this.getValue(cmp, "String");
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-				this.toggleRadio(cmp, booleanValue);
-			},function(cmp){
-				booleanValue = this.getValue(cmp, "Boolean");
-				this.verifyInputRadioButton(cmp, booleanValue);
-			}
-		]
-	},
+    /**
+     * Test to verify radioItems works when interacting with attribute value directly
+     * using AURA API  
+     */
+    owner:"ronak.shah",
+    
+    testToggleRadioMenu:{
+        test: [function(cmp) {
+                var defaultStringValue = this.getValue(cmp, "String");
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+                this.toggleRadio(cmp, booleanValue);
+            },function(cmp){
+                booleanValue = this.getValue(cmp, "Boolean");
+                this.verifyInputRadioButton(cmp, booleanValue);
+            }
+        ]
+    },
 
-	/**
-	 * Get value for cmp attribute
-	 */
+    /**
+     * Get value for cmp attribute
+     */
     getValue: function(cmp, attribute){
-    	var exp = "v.myValue" + attribute;
-    	return cmp.get(exp);
+        var exp = "v.myValue" + attribute;
+        return cmp.get(exp);
     },
     
     /**
      * Toggle radio button by firing press event
      */
     toggleRadio: function(cmp, booleanValue){
-    	if(booleanValue){
-    		var nopeButton = cmp.find("nope");
-    		this.pressButtonAndVerify(cmp, nopeButton, booleanValue)
-    	}
-    	else{
-    		var yupButton = cmp.find("yup");
-    		this.pressButtonAndVerify(cmp, yupButton)
-    	}
+        if(booleanValue){
+            var nopeButton = cmp.find("nope");
+            this.pressButtonAndVerify(cmp, nopeButton, booleanValue)
+        }
+        else{
+            var yupButton = cmp.find("yup");
+            this.pressButtonAndVerify(cmp, yupButton)
+        }
     },
     
     /**
      * Firing the event and verify the attribute is set correctly
      */
     pressButtonAndVerify: function(cmp, buttonToPress, booleanValue){
-    	buttonToPress.get("e.press").fire();
-	    $A.test.addWaitForWithFailureMessage(!booleanValue, function(){return cmp.get("v.myValueBoolean");}, "Boolean value should be changed to false after button was pressed");
-	},
-	
-	/**
-	 * Verify inputRadio button are set correctly
-	 */
-	verifyInputRadioButton: function(cmp, booleanValue){
-		var inputRadioYupWithStringExp = this.getRadioButtonElement(cmp, "expYup");
-    	var inputRadioYupWithBooleanExp = this.getRadioButtonElement(cmp, "booleanYup");
-    	var inputRadioNopeWithStringExp = this.getRadioButtonElement(cmp, "expNope");
-    	var inputRadioNopeWithBooleanExp = this.getRadioButtonElement(cmp, "booleanNope");
-    	if(booleanValue){
-        	aura.test.assertTrue(inputRadioYupWithStringExp.checked, "Yup Radio Element with value Expression should be checked");
-	        aura.test.assertTrue(inputRadioYupWithBooleanExp.checked, "Yup Radio Element with boolean Expression should be checked");
-	        aura.test.assertFalse(inputRadioNopeWithStringExp.checked, "Nope Radio Element with value Expression should be un-checked");
-	        aura.test.assertFalse(inputRadioNopeWithBooleanExp.checked, "Nope Radio Element with boolean Expression should be un-checked");
-		}
-    	else{
-    		aura.test.assertFalse(inputRadioYupWithStringExp.checked, "Yup Radio Element with value Expression should be un-checked");
-	        aura.test.assertFalse(inputRadioYupWithBooleanExp.checked, "Yup Radio Element with boolean Expression should be un-checked");
-	        aura.test.assertTrue(inputRadioNopeWithStringExp.checked, "Nope Radio Element with value Expression should be checked");
-	        aura.test.assertTrue(inputRadioNopeWithBooleanExp.checked, "Nope Radio Element with boolean Expression should be checked");
-    	}
+        buttonToPress.get("e.press").fire();
+        $A.test.addWaitForWithFailureMessage(!booleanValue, function(){return cmp.get("v.myValueBoolean");}, "Boolean value should be changed to false after button was pressed");
+    },
+    
+    /**
+     * Verify inputRadio button are set correctly
+     */
+    verifyInputRadioButton: function(cmp, booleanValue){
+        var inputRadioYupWithStringExp = this.getRadioButtonElement(cmp, "expYup");
+        var inputRadioYupWithBooleanExp = this.getRadioButtonElement(cmp, "booleanYup");
+        var inputRadioNopeWithStringExp = this.getRadioButtonElement(cmp, "expNope");
+        var inputRadioNopeWithBooleanExp = this.getRadioButtonElement(cmp, "booleanNope");
+        if(booleanValue){
+            aura.test.assertTrue(inputRadioYupWithStringExp.checked, "Yup Radio Element with value Expression should be checked");
+            aura.test.assertTrue(inputRadioYupWithBooleanExp.checked, "Yup Radio Element with boolean Expression should be checked");
+            aura.test.assertFalse(inputRadioNopeWithStringExp.checked, "Nope Radio Element with value Expression should be un-checked");
+            aura.test.assertFalse(inputRadioNopeWithBooleanExp.checked, "Nope Radio Element with boolean Expression should be un-checked");
+        }
+        else{
+            aura.test.assertFalse(inputRadioYupWithStringExp.checked, "Yup Radio Element with value Expression should be un-checked");
+            aura.test.assertFalse(inputRadioYupWithBooleanExp.checked, "Yup Radio Element with boolean Expression should be un-checked");
+            aura.test.assertTrue(inputRadioNopeWithStringExp.checked, "Nope Radio Element with value Expression should be checked");
+            aura.test.assertTrue(inputRadioNopeWithBooleanExp.checked, "Nope Radio Element with boolean Expression should be checked");
+        }
     },
     
     /*
      * get inputRadio button given localId
      */
     getRadioButtonElement: function(cmp, cmpLocalId){
-    	return cmp.find(cmpLocalId).find("radio").getElement();
+        return cmp.find(cmpLocalId).find("radio").getElement();
     }
     
 })

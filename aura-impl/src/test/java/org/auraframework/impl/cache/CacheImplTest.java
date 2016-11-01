c13c8fdde93c1b4ffa0dbf43b44b45a75ba68753
Gordon <goliver@salesforce.com>
2016-11-01 11:32:36 -0400

    Throttle stats logging more intelligently.
    @bug W-3436539@
    
    @rev byao@

--- aura-impl/src/test/java/org/auraframework/impl/cache/CacheImplTest.java
+++ aura-impl/src/test/java/org/auraframework/impl/cache/CacheImplTest.java
@@ -27,159 +27,159 @@ import java.util.Set;
 
 public class CacheImplTest extends UnitTestCase {
 
-	private <K, T> com.google.common.cache.Cache<K, T> getMockCache() {
-		com.google.common.cache.Cache<K, T> cache = com.google.common.cache.CacheBuilder
-				.newBuilder().build();
-		return Mockito.spy(cache);
-	}
+    private <K, T> com.google.common.cache.Cache<K, T> getMockCache() {
+        com.google.common.cache.Cache<K, T> cache = com.google.common.cache.CacheBuilder
+                .newBuilder().build();
+        return Mockito.spy(cache);
+    }
 
-	private class CollectionContainsAllMatcher<T> extends ArgumentMatcher<T> {
-		private Collection<T> expected;
+    private class CollectionContainsAllMatcher<T> extends ArgumentMatcher<T> {
+        private Collection<T> expected;
 
-		CollectionContainsAllMatcher(@SuppressWarnings("unchecked") T... expected) {
-			this.expected = Arrays.asList(expected);
-		}
+        CollectionContainsAllMatcher(@SuppressWarnings("unchecked") T... expected) {
+            this.expected = Arrays.asList(expected);
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public boolean matches(Object argument) {
-			Collection<T> actual = (Collection<T>) argument;
-			return expected.containsAll(actual) && actual.containsAll(expected);
-		}
-	}
+        @SuppressWarnings("unchecked")
+        @Override
+        public boolean matches(Object argument) {
+            Collection<T> actual = (Collection<T>) argument;
+            return expected.containsAll(actual) && actual.containsAll(expected);
+        }
+    }
 
-	@Test
-	public void testGetIfPresent() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		Object key = new Object();
-		Object expected = new Object();
-		Mockito.doReturn(expected).when(backingCache).getIfPresent(key);
+    @Test
+    public void testGetIfPresent() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        Object key = new Object();
+        Object expected = new Object();
+        Mockito.doReturn(expected).when(backingCache).getIfPresent(key);
 
-		Object actual = cache.getIfPresent(key);
-		assertEquals(expected, actual);
-	}
+        Object actual = cache.getIfPresent(key);
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testPut() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		Object key = new Object();
-		Object data = new Object();
+    @Test
+    public void testPut() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        Object key = new Object();
+        Object data = new Object();
 
-		cache.put(key, data);
-		Mockito.verify(backingCache, Mockito.times(1)).put(key, data);
-	}
+        cache.put(key, data);
+        Mockito.verify(backingCache, Mockito.times(1)).put(key, data);
+    }
 
-	@Test
-	public void testInvalidateKey() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		Object key = new Object();
+    @Test
+    public void testInvalidateKey() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        Object key = new Object();
 
-		cache.invalidate(key);
-		Mockito.verify(backingCache, Mockito.times(1)).invalidate(key);
-	}
+        cache.invalidate(key);
+        Mockito.verify(backingCache, Mockito.times(1)).invalidate(key);
+    }
 
-	@Test
-	public void testInvalidateKeys() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		@SuppressWarnings("unchecked")
-		Iterable<Object> keys = Mockito.mock(Iterable.class);
+    @Test
+    public void testInvalidateKeys() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        @SuppressWarnings("unchecked")
+        Iterable<Object> keys = Mockito.mock(Iterable.class);
 
-		cache.invalidate(keys);
-		Mockito.verify(backingCache, Mockito.times(1)).invalidate(keys);
-	}
+        cache.invalidate(keys);
+        Mockito.verify(backingCache, Mockito.times(1)).invalidate(keys);
+    }
 
-	@Test
-	public void testInvalidateAll() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
+    @Test
+    public void testInvalidateAll() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
 
-		cache.invalidateAll();
-		Mockito.verify(backingCache, Mockito.times(1)).invalidateAll();
-	}
+        cache.invalidateAll();
+        Mockito.verify(backingCache, Mockito.times(1)).invalidateAll();
+    }
 
-	@Test
-	public void testGetKeySet() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
+    @Test
+    public void testGetKeySet() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
 
-		Set<Object> actualKeySet = cache.getKeySet();
-		assertEquals(backingCache.asMap().keySet(), actualKeySet);
-	}
+        Set<Object> actualKeySet = cache.getKeySet();
+        assertEquals(backingCache.asMap().keySet(), actualKeySet);
+    }
 
-	@Test
-	public void testInvalidatePartial_NullInput() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		cache.invalidatePartial(null);
-		Mockito.verify(backingCache, Mockito.times(1)).invalidateAll();
-	}
+    @Test
+    public void testInvalidatePartial_NullInput() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        cache.invalidatePartial(null);
+        Mockito.verify(backingCache, Mockito.times(1)).invalidateAll();
+    }
 
-	@Test
-	public void testInvalidatePartial_EmptyString() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		cache.invalidatePartial("");
-		Mockito.verify(backingCache, Mockito.times(1)).invalidateAll();
-	}
+    @Test
+    public void testInvalidatePartial_EmptyString() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        cache.invalidatePartial("");
+        Mockito.verify(backingCache, Mockito.times(1)).invalidateAll();
+    }
 
-	@Test
-	public void testInvalidatePartial_WhitespaceString() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		backingCache.put("someKey", "someValue");
-		cache.invalidatePartial(" ");
-		Mockito.verify(backingCache, Mockito.never()).invalidate(
-				Mockito.anyCollection());
-	}
+    @Test
+    public void testInvalidatePartial_WhitespaceString() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        backingCache.put("someKey", "someValue");
+        cache.invalidatePartial(" ");
+        Mockito.verify(backingCache, Mockito.never()).invalidate(
+                Mockito.anyCollection());
+    }
 
-	@Test
-	public void testInvalidatePartial_SingleMatch() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		backingCache.put("someKey", "someValue");
-		cache.invalidatePartial("someKey");
-		Mockito.verify(backingCache, Mockito.times(1)).invalidate(
-				Mockito.argThat(new CollectionContainsAllMatcher<Object>("someKey")));
-	}
+    @Test
+    public void testInvalidatePartial_SingleMatch() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        backingCache.put("someKey", "someValue");
+        cache.invalidatePartial("someKey");
+        Mockito.verify(backingCache, Mockito.times(1)).invalidate(
+                Mockito.argThat(new CollectionContainsAllMatcher<Object>("someKey")));
+    }
 
-	@Test
-	public void testInvalidatePartial_MultipleMatches() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		backingCache.put("someKey", "someValue");
-		backingCache.put("someOtherKey", "someValue");
-		backingCache.put("unmatchedKey", "someValue");
-		backingCache.put("someThing", "someValue");
+    @Test
+    public void testInvalidatePartial_MultipleMatches() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        backingCache.put("someKey", "someValue");
+        backingCache.put("someOtherKey", "someValue");
+        backingCache.put("unmatchedKey", "someValue");
+        backingCache.put("someThing", "someValue");
 
-		cache.invalidatePartial("some");
-		Mockito.verify(backingCache, Mockito.times(1)).invalidate(
-				Mockito.argThat(new CollectionContainsAllMatcher<Object>("someKey",
-						"someOtherKey", "someThing")));
-	}
+        cache.invalidatePartial("some");
+        Mockito.verify(backingCache, Mockito.times(1)).invalidate(
+                Mockito.argThat(new CollectionContainsAllMatcher<Object>("someKey",
+                        "someOtherKey", "someThing")));
+    }
 
-	@Test
-	public void testInvalidatePartial_NoMatch() {
-		com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
-		Cache<Object, Object> cache = new CacheImpl<>(
-				backingCache);
-		backingCache.put("someKey", "someValue");
-		cache.invalidatePartial("otherKey");
-		Mockito.verify(backingCache, Mockito.never()).invalidate(
-				Mockito.anyCollection());
-	}
+    @Test
+    public void testInvalidatePartial_NoMatch() {
+        com.google.common.cache.Cache<Object, Object> backingCache = getMockCache();
+        Cache<Object, Object> cache = new CacheImpl<>(
+                backingCache);
+        backingCache.put("someKey", "someValue");
+        cache.invalidatePartial("otherKey");
+        Mockito.verify(backingCache, Mockito.never()).invalidate(
+                Mockito.anyCollection());
+    }
 }

afe82ee6ed3ebfb55c92e566d7c330658f7d118f
Cheng <cheng.li@salesforce.com>
2016-11-07 11:17:26 -0800

    fixing clientApiTest:utilTests.testContainsForSVGElement
    @rev lhong@
    @bug W-3455358@

--- aura-components/src/test/components/clientApiTest/utilTests/utilTestsTest.js
+++ aura-components/src/test/components/clientApiTest/utilTests/utilTestsTest.js
@@ -125,7 +125,7 @@
 
     testContainsForSVGElement: {
         test: function() {
-            var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "SVG");
+            var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             var divElement = document.createElement("div");
 
             divElement.appendChild(svgElement);

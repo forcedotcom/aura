b613b83e7f3eab1500b9e3025335b8f5f34aee8f
Diego <dval@salesforce.com>
2016-11-18 16:14:06 -0800

    Adding new file size properties for resources in metricService (per timming spec)

--- aura-impl/src/main/resources/aura/metrics/AuraMetricsService.js
+++ aura-impl/src/main/resources/aura/metrics/AuraMetricsService.js
@@ -822,15 +822,18 @@ Aura.Services.MetricsService.prototype.registerBeacon = function (beacon) {
 */
 Aura.Services.MetricsService.prototype.summarizeResourcePerfInfo = function (r) {
     return {
-        "name"         : r.name,
-        "duration"     : parseInt(r.responseEnd - r.startTime, 10),
-        "startTime"    : parseInt(r.startTime, 10),
-        "fetchStart"   : parseInt(r.fetchStart, 10),
-        "requestStart" : parseInt(r.requestStart, 10),
-        "dns"          : parseInt(r.domainLookupEnd - r.domainLookupStart, 10),
-        "tcp"          : parseInt(r.connectEnd - r.connectStart, 10),
-        "ttfb"         : parseInt(r.responseStart - r.startTime, 10),
-        "transfer"     : parseInt(r.responseEnd - r.responseStart, 10)
+        "name"            : r.name,
+        "duration"        : parseInt(r.responseEnd - r.startTime, 10),
+        "startTime"       : parseInt(r.startTime, 10),
+        "fetchStart"      : parseInt(r.fetchStart, 10),
+        "requestStart"    : parseInt(r.requestStart, 10),
+        "dns"             : parseInt(r.domainLookupEnd - r.domainLookupStart, 10),
+        "tcp"             : parseInt(r.connectEnd - r.connectStart, 10),
+        "ttfb"            : parseInt(r.responseStart - r.startTime, 10),
+        "transfer"        : parseInt(r.responseEnd - r.responseStart, 10),
+        "transferSize"    : r.transferSize || 0,
+        "encodedBodySize" : r.encodedBodySize || 0,
+        "decodedBodySize" : r.decodedBodySize || 0
     };
 };
 

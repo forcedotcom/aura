d59ffacdad58c97ae9c512834c65fd85e8567ae9
Albert Li <albert.li@salesforce.com>
2016-12-08 10:29:35 -0800

    remove extra empty line.

--- aura-components/src/main/components/ui/inputSelect/inputSelectHelper.js
+++ aura-components/src/main/components/ui/inputSelect/inputSelectHelper.js
@@ -64,7 +64,6 @@
             element.getElementsByTagName('input')[0] || element.getElementsByTagName('select')[0] || element.getElementsByTagName('textarea')[0] || element;
     },
 
-
     /**
      * Returns a package with the array of options (as either an array of components or an array of JS objects)
      * and the strategy to work with that array

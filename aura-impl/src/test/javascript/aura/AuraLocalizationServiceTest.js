89cfaf7dccd2c1ed7b9b2a75911f7e52612b699b
Cheng <cheng.li@salesforce.com>
2017-03-16 10:52:46 -0700

    Fix and re-enable localization tests
    @rev lhong@
    @bug W-3782627@

--- aura-impl/src/test/javascript/aura/AuraLocalizationServiceTest.js
+++ aura-impl/src/test/javascript/aura/AuraLocalizationServiceTest.js
@@ -1774,7 +1774,7 @@ Test.Aura.AuraLocalizationServiceTest = function() {
         function CallsToISOStringOnDateIfExits() {
             // Arrange
             var targetService = new Aura.Services.AuraLocalizationService();
-            var date = new Date(2004,10,23,12,30,59,123);
+            var date = new Date();
             date.toISOString = Stubs.GetMethod();
 
             // Act
@@ -1786,40 +1786,41 @@ Test.Aura.AuraLocalizationServiceTest = function() {
             Assert.Equal(1, date.toISOString.Calls.length);
         }
 
-//        [Fact]
-//        function ReturnsISOStringForDateWithToISOString(){
-//            // Arrange
-//            var targetService = new Aura.Services.AuraLocalizationService();
-//            var date = new Date(2004,10,09,12,30,59,123);
-//            var expected = "2004-11-09T20:30:59.123Z";
-//            var actual;
-//
-//            // Act
-//            mockUtil(function() {
-//                actual = targetService.toISOString(date);
-//            });
-//
-//            // Assert
-//            Assert.Equal(expected, actual);
-//        }
+        [Fact]
+        function ReturnsISOStringForDateWithToISOString(){
+            // Arrange
+            var targetService = new Aura.Services.AuraLocalizationService();
+            var expected = "2004-11-09T12:30:59.123Z";
+            var date = new Date(Date.UTC(2004,10,09,12,30,59,123));
+            var actual;
 
-//        [Fact]
-//        function ReturnsISOStringForDateWithoutToISOString(){
-//            // Arrange
-//            var targetService = new Aura.Services.AuraLocalizationService();
-//            var expected = "2004-11-09T20:30:59.123Z";
-//            var date = new Date(2004,10,09,12,30,59,123);
-//            date.toISOString = undefined;
-//            var actual;
-//
-//            // Act
-//            mockUtil(function() {
-//                actual = targetService.toISOString(date);
-//            });
-//
-//            // Assert
-//            Assert.Equal(expected, actual);
-//        }
+            // Act
+            mockUtil(function() {
+                actual = targetService.toISOString(date);
+            });
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        function ReturnsISOStringForDateWithoutToISOString(){
+            // Arrange
+            var targetService = new Aura.Services.AuraLocalizationService();
+            var expected = "2004-11-09T12:30:59.123Z";
+            var date = new Date(Date.UTC(2004,10,09,12,30,59,123));
+            date.toISOString = undefined;
+            var actual;
+
+            // Act
+            mockUtil(function() {
+                actual = targetService.toISOString(date);
+            });
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
     }
 
     [Fixture]

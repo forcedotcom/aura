8339ec0860db1f9cf33507fbfd184a961ffd7d8e
Diego Ferreiro Val <elfogris@gmail.com>
2017-04-10 15:36:17 -0700

    Upgrade compiler to 0.7.1

--- aura-modules/src/test/testdata/modules/moduletest/expected.js
+++ aura-modules/src/test/testdata/modules/moduletest/expected.js
@@ -1,39 +1,35 @@
-define('modules-moduletest', ['x-test'], function (_xTest) { 'use strict';
+define('modules-moduletest', ['x-test'], function (_xTest) {
+    'use strict';
 
-function tmpl($api, $cmp, $slotset, $ctx) {
-    const m = $ctx.memoized || ($ctx.memoized = {});
-    return [$api.c(
-        "x-test",
-        _xTest,
-        {
+    function tmpl($api, $cmp, $slotset, $ctx) {
+        const m = $ctx.memoized || ($ctx.memoized = {});
+        return [$api.c("x-test", _xTest, {
             slotset: {
-                $default$: [$api.s($cmp.test)]
+                $default$: [$cmp.test]
             }
+        })];
+    }
+    tmpl.ids = ["test"];
+
+    class Test {
+        constructor() {
+            this.stringQuote = 'str"ing';
+            this.stringDoubleQuote = "str'ing";
+            this.stringBacktick = `key=${value}`;
+            this.VALID_NAME_RE = /^([a-zA-Z]\w*):([a-zA-Z]\w*)$/;
         }
-    )];
-}
-tmpl.ids = ["test"];
 
-class Test {
-    constructor() {
-        this.stringQuote = 'str"ing';
-        this.stringDoubleQuote = "str'ing";
-        this.stringBacktick = `key=${value}`;
-        this.VALID_NAME_RE = /^([a-zA-Z]\w*):([a-zA-Z]\w*)$/;
+        render() {
+            return tmpl;
+        }
+
     }
+    Test.publicProps = {
+        stringQuote: 1,
+        stringDoubleQuote: 1,
+        stringBacktick: 1,
+        VALID_NAME_RE: 1
+    };
 
-    render() {
-        return tmpl;
-    }
-
-}
-Test.publicProps = {
-    stringQuote: 'str"ing',
-    stringDoubleQuote: "str'ing",
-    stringBacktick: `key=${value}`,
-    VALID_NAME_RE: /^([a-zA-Z]\w*):([a-zA-Z]\w*)$/
-};
-
-return Test;
-
+    return Test;
 });

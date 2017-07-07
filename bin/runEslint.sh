d8334e91dc24ccce97e4bece76de0b6800115190
gtorres <gtorres@salesforce.com>
2017-07-06 18:02:45 -0700

    run eslint in builds (#1097)
    @bypasscoreitests@

--- bin/runEslint.sh
+++ bin/runEslint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm ./aura-impl/target/eslint-output || true
-rm ./aura-components/target/eslint-output || true
-mvn pre-clean -Peslint || cat ./aura-impl/target/eslint-output && cat ./aura-components/target/eslint-output
+rm */target/eslint-output || true
+mvn pre-clean -Peslint || { status=$?; cat */target/eslint-output; exit ${status}; }
 mvn tools:eslint-raptor

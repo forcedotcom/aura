65e067384283abbc36996b79fb3628f936560b81
Roman Salvador <rsalvador@salesforce.com>
2017-07-11 08:00:36 -0700

    Fix runEslint.sh (#1142)

--- bin/runEslint.sh
+++ bin/runEslint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm */target/eslint-output || true
 mvn pre-clean -Peslint || { status=$?; cat */target/eslint-output; exit ${status}; }
-mvn tools:eslint-raptor
+mvn -pl aura-modules tools:eslint-raptor

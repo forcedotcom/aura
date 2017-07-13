1dc78781794c0ee43bee1f7b64aac0727e68c981
Bob Yao <byao@salesforce.com>
2017-07-13 12:26:32 -0700

    Override both ServiceConfig and Query spring bean for polyfill-service (#1156)
    ServiceConfig override did not load required settings.
    
    @bug W-4106042@
    @runcoreitests@

--- aura-spring/src/main/java/org/auraframework/AuraConfiguration.java
+++ aura-spring/src/main/java/org/auraframework/AuraConfiguration.java
@@ -15,8 +15,12 @@
  */
 package org.auraframework;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.polyfill.api.components.Feature;
+import org.polyfill.api.components.Query;
 import org.polyfill.api.components.ServiceConfig;
 import org.polyfill.api.configurations.PolyfillApiConfig;
 import org.springframework.context.annotation.Bean;
@@ -25,8 +29,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 
-import com.google.common.collect.Lists;
-
 /**
  * Spring configuration to autoscan all aura packages
  */
@@ -34,33 +36,41 @@ import com.google.common.collect.Lists;
 @ComponentScan(basePackages = {"org.auraframework"}, lazyInit = true)
 @Import(PolyfillApiConfig.class)
 public class AuraConfiguration {
+
+    private List<String> polyfills = Arrays.asList(
+            "Array.prototype.find",
+            "Array.prototype.findIndex",
+            "Array.prototype.fill",
+            "Array.prototype.keys",
+            "Array.prototype.includes",
+            "Array.from",
+            "CustomEvent",
+            "Date.now",
+            "Event",
+            "Function.name",
+            "Function.prototype.bind",
+            "Object.keys",
+            "Object.assign",
+            "requestAnimationFrame",
+            "WeakMap",
+            "WeakSet",
+            "Symbol",
+            "compat.classList",
+            "compat.freeze"
+    );
+
     @Primary
     @Bean
     public ServiceConfig getAuraPolyfillServiceConfig() {
-        List<String> polyfills = Lists.newArrayList();
-        polyfills.add("Array.prototype.find");
-        polyfills.add("Array.prototype.findIndex");
-        polyfills.add("Array.prototype.fill");
-        polyfills.add("Array.prototype.keys");
-        polyfills.add("Array.prototype.includes");
-        polyfills.add("CustomEvent");
-        polyfills.add("Date.now");
-        polyfills.add("Event");
-        polyfills.add("Function.name");
-        polyfills.add("Function.prototype.bind");
-        polyfills.add("Object.keys");
-        polyfills.add("Object.assign");
-        polyfills.add("requestAnimationFrame");
-        polyfills.add("WeakMap");
-        polyfills.add("WeakSet");
-        polyfills.add("Symbol");
-        polyfills.add("compat.classList");
-        polyfills.add("compat.freeze");
+        return new ServiceConfig().setPolyfills(this.polyfills);
+    }
 
-        return new ServiceConfig()
-                .setGated(true)
-                .setMinify(true)
-                .setLoadOnUnknownUA(true)
-                .setPolyfills(polyfills);
+    @Primary
+    @Bean
+    public Query defaultQuery() {
+        List<Feature> polyfillRequestList = this.polyfills.stream()
+                .map(Feature::new)
+                .collect(Collectors.toList());
+        return new Query.Builder(polyfillRequestList).build();
     }
 }

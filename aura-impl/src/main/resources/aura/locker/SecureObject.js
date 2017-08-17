46c5dbeac9a647d4cd529db6dc14a36f70b4e5f8
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2017-08-17 10:31:45 -0700

    Guard MediaStream usage to protect instanceof checks in all browsers that might or might not support MediaStream (#1401)
    @rev jparadis@
    @bug W-4231415@

--- aura-impl/src/main/resources/aura/locker/SecureObject.js
+++ aura-impl/src/main/resources/aura/locker/SecureObject.js
@@ -1691,7 +1691,7 @@ SecureObject.isUnfilteredType = function(raw, key) {
     var namespace = key["namespace"];
     // Special previlege for RTC, TODO:RJ remove it once we have SecureMediaStream
     if ((namespace === "runtime_rtc_spark" || namespace === "runtime_rtc")
-        && $A.lockerService.instanceOf(raw, window["MediaStream"])) {
+        && window["MediaStream"] && $A.lockerService.instanceOf(raw, window["MediaStream"])) {
         return true;
     }
     // Do not filter ArrayBufferView types. https://developer.mozilla.org/en-US/docs/Web/API/ArrayBufferView

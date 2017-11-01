b3cf3af3ab437ca7cb9a326f0fff4c249e0c2f82
Bob Yao <byao@salesforce.com>
2017-10-31 17:09:44 -0700

    Remove polyfill service (#1832)
    @bug W-4438723@

--- aura-spring/src/main/java/org/auraframework/AuraConfiguration.java
+++ aura-spring/src/main/java/org/auraframework/AuraConfiguration.java
@@ -15,59 +15,13 @@
  */
 package org.auraframework;
 
-import org.polyfillservice.api.components.ServiceConfig;
-import org.polyfillservice.api.configurations.PolyfillApiConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Spring configuration to autoscan all aura packages
  */
 @Configuration
 @ComponentScan(basePackages = {"org.auraframework"}, lazyInit = true)
-@Import(PolyfillApiConfig.class)
 public class AuraConfiguration {
-
-    private List<String> polyfills = Arrays.asList(
-            "Array.prototype.find",
-            "Array.prototype.findIndex",
-            "Array.prototype.fill",
-            "Array.prototype.keys",
-            "Array.prototype.includes",
-            "Array.from",
-            "Array.prototype.@@iterator",
-            "CustomEvent",
-            "Date.now",
-            "Event",
-            "Function.name",
-            "Function.prototype.bind",
-            "Map",
-            "Object.keys",
-            "Object.assign",
-            "Object.entries",
-            "requestAnimationFrame",
-            "WeakMap",
-            "WeakSet",
-            "Symbol",
-            "compat.classList",
-            "compat.freeze",
-            "Set"
-    );
-
-    @Primary
-    @Bean
-    public ServiceConfig getAuraPolyfillServiceConfig() {
-        return new ServiceConfig.Builder()
-                .setPolyfills(this.polyfills)
-                .setGated(true)
-                .setMinify(true)
-                .setLoadOnUnknownUA(true)
-                .build();
-    }
 }

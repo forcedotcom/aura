2eefbd489a8b6a907bbe62052621983637c79cca
Alex Papko <apapko@salesforce.com>
2017-11-14 14:33:31 -0800

    Remove raptor equivalent aura test modules (#1924)

--- aura-modules/src/test/components/moduleTest/compositeCmp/compositeCmpController.js
+++
@@ -1,9 +0,0 @@
-({
-  onSimplePress: function (cmp, event) {
-      console.log('onSimplePress');
-  },
-  onSimpleCallbackAction: function (cmp, event) {
-    console.log('onSimpleCallbackAction');
-  }
-          
-})

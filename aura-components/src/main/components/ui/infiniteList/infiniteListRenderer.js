965818ef202c8ffb8c8152d7f7d3e26d7a66815d
Amruta Phanse <aphanse@salesforce.com>
2018-04-11 16:41:18 -0700

    W-4280131 Infinite list afterrender error (#2539)
    * prevent gack in afterRender of inifiniteList
    
    * reformatting code

--- aura-components/src/main/components/ui/infiniteList/infiniteListRenderer.js
+++ aura-components/src/main/components/ui/infiniteList/infiniteListRenderer.js
@@ -13,24 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-({	
-	afterRender: function (cmp, hlp) {
-		var list;
-		
-		if (cmp.get('v.enableRowSwipe')) {
-			hlp.initializeHandlers(cmp);
-			list = cmp.getElement();
-		
-			// Just attach the start handler, move and end is conditionally added.
-			// Attached to the capture phase in order to cancel click events if necessary. 
-			// This is necessary because Aura's FastClick implementation is attached directly
-			// to aura:html components instead of at the window level.
-			list.addEventListener(hlp.getEventNames().start, cmp._ontouchstart, true); 
-			
-			list.addEventListener('InfiniteListRowOpen', cmp._onInfiniteListRowOpen, false);
-			list.addEventListener('InfiniteListRowClose', cmp._onInfiniteListRowClose, false);
-		}
-		
+({
+    afterRender: function (cmp, hlp) {
+        var list;
+
+        if (cmp.get('v.enableRowSwipe')) {
+            hlp.initializeHandlers(cmp);
+            if (cmp && cmp.isValid()) {
+                list = cmp.getElement();
+
+                // Just attach the start handler, move and end is conditionally added.
+                // Attached to the capture phase in order to cancel click events if necessary.
+                // This is necessary because Aura's FastClick implementation is attached directly
+                // to aura:html components instead of at the window level.
+                list.addEventListener(hlp.getEventNames().start, cmp._ontouchstart, true);
+
+                list.addEventListener('InfiniteListRowOpen', cmp._onInfiniteListRowOpen, false);
+                list.addEventListener('InfiniteListRowClose', cmp._onInfiniteListRowClose, false);
+            }
+        }
         this.superAfterRender();
-	}
+    }
 })// eslint-disable-line semi

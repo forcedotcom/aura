ba4257aee2a5ab8bf05c6ae90eb4564311aaf28a
Theodore Lau <t.lau@salesforce.com>
2018-04-17 07:33:58 -0700

    @W-4896341@ HTML Imports Fix Tests (#2659)

--- aura-components/src/test/components/lockerTest/secureLinkElementTest/secureLinkElementTestController.js
+++ aura-components/src/test/components/lockerTest/secureLinkElementTest/secureLinkElementTestController.js
@@ -2,10 +2,11 @@
     testBlockLinkElementImportAttribute: function(cmp, event, helper) {
         var testUtils = cmp.get("v.testUtils");
         var linkElement = document.createElement("link");
-        
-        linkElement.rel = "import";
 
-        testUtils.assertEquals(null, linkElement.getAttribute("rel"), "The 'rel' attribute cannot be changed to the 'import' value!");
+        ["import", "IMPORT", " import ", "abc import xyz", ";/abc/import/xyz"].forEach(function(test) {
+            linkElement.rel = test;
+            testUtils.assertEquals(null, linkElement.getAttribute("rel"), "The 'rel' attribute cannot be changed to the 'import' value!");
+        });
 
         linkElement.rel = "stylesheet";
         linkElement.href = "test.css";

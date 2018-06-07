adab0f44f4c3e3b73841c97a0409ee5792a83712
JF Paradis <jparadis@salesforce.com>
2018-06-07 16:05:25 -0700

    @bug W-5068440@ Cleanup unused polyfills (#2916)

--- aura/src/main/resources/compact-polyfills/compat/proxy/min.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var freeze = Object.freeze;
-    Object.freeze = function(obj) {
-    if (!obj || typeof obj !== 'object') {
-        return obj;
-    }
-    return freeze(obj);
-    };
-})();

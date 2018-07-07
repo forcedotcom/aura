c2882e3b26a7cd6b1a9f68ff385e3ec749e21dd2
Dejan Mihai Gitin <dgitin@salesforce.com>
2018-07-06 21:15:24 -0700

    Improved tests for LWC-Locker component lifecycle (#3005)

--- aura-modules/src/test/components/secureModuleTest/componentLifecycle/componentLifecycleController.js
+++ aura-modules/src/test/components/secureModuleTest/componentLifecycle/componentLifecycleController.js
@@ -1,11 +1,46 @@
 ({
     componentLifecycleTester: function(cmp, event) {
         var testUtils = cmp.get("v.testUtils");
+        var capturingDiv = cmp.find('capturingDiv').getElement();
         var params = event.getParam('arguments');
+
+        var eventTriggered = false;
+
+        capturingDiv.addEventListener('foo', function(ev) {
+            testUtils.assertEquals(
+                'SecureWindow: [object Window]{ key: {"namespace":"secureModuleTest"} }',
+                ev.detail.win.toString(),
+                'Mismatch in custom event data for window'
+            );
+            testUtils.assertEquals(
+                'SecureDocument: [object HTMLDocument]{ key: {"namespace":"secureModuleTest"} }',
+                ev.detail.doc.toString(),
+                'Mismatch in custom event data for document'
+            );
+            if (Object.hasOwnProperty('domElement', ev.detail)) {
+                testUtils.assertEquals(
+                    'SecureElement: [object HTMLParagraphElement]{ key: {"namespace":"secureModuleTest"} }',
+                    ev.detail.domElement.toString(),
+                    'Mismatch in custom event data for document'
+                );
+            }
+            eventTriggered = true;
+        });
+
         // Making sure that the life cycle hook was indeed invoked and we don't get any false positives
         testUtils.addWaitForWithFailureMessage(true, function(){
             return window[params.testCaseConfirmationFlag];
             },
             "Life cycle hook was not invoked:" + params.testCase);
+
+        if (!['disconnectedCallbackHookCalled', 'constructorHookCalled'].includes(params.testCaseConfirmationFlag)) {
+            testUtils.addWaitForWithFailureMessage(
+                true,
+                function() {
+                    return eventTriggered;
+                },
+                "Event in lifecycle method was not fired"
+            );
+        }
     }
 })
--- aura-modules/src/test/modules/securemoduletest/component-lifecycle-tester/component-lifecycle-tester.js
+++ aura-modules/src/test/modules/securemoduletest/component-lifecycle-tester/component-lifecycle-tester.js
@@ -1,6 +1,23 @@
 import { Element, api } from 'engine';
 import * as testUtil from 'securemoduletest-test-util';
 
+const SECURE_ELEMENT_REGEX = /^SecureElement: \[.+\]{ key: {"namespace":"secureModuleTest"} }$/
+const isSecureWindow = function(win) {
+    testUtil.assertEquals(
+        'SecureWindow: [object Window]{ key: {"namespace":"secureModuleTest"} }',
+        win.toString(),
+        'Expected window to be a SecureWindow'
+    );
+}
+
+const isSecureDocument = function(doc) {
+    testUtil.assertEquals(
+        'SecureDocument: [object HTMLDocument]{ key: {"namespace":"secureModuleTest"} }',
+        doc.toString(),
+        'Expected window to be a SecureDocument'
+    );
+}
+
 export default class ComponentLifecycleTester extends Element {
     // Boolean flags to indicate which lifecycle hooks are being tested
     @api shouldTestConstructorHook = false;
@@ -9,48 +26,98 @@ export default class ComponentLifecycleTester extends Element {
     @api shouldTestAttributeChangedCallbackHook = false;
     @api shouldTestRenderedCallbackHook = false;
 
+    get customEvent() {
+        return new CustomEvent('foo', {
+            bubbles: true,
+            composed: true,
+            detail: this.complexDataStructure
+        });
+    }
+
+    get complexDataStructure() {
+        return {           
+            domElement: this.template.querySelector('section.component-lifecyle-tester > p'),
+            win: window,
+            doc: document
+        };
+    }
+
     connectedCallback() {
-        if (this.shouldTestConnectedCallbackHook) {
-            // Verify that render method has access to secure wrappers when invoked through interop
-            testUtil.assertStartsWith("SecureWindow", window.toString(), "Expected window to"
-                + " return SecureWindow in connectedCallback");
-            testUtil.assertEquals("undefined", typeof $A, // eslint-disable-line lwc/no-aura
-                "Expected $A to be not accessible in connectedCallback");
-            testUtil.assertStartsWith("SecureDocument", document.toString(), "Expected document to"
-                + " return SecureDocument in connectedCallback");
-            testUtil.assertTrue(this instanceof ComponentLifecycleTester,
-                "Expected context to be an instance of the component class in connectedCallback");
-            window.connectedCallbackHookCalled = true;
+        if (!this.shouldTestConnectedCallbackHook) {
+            return;
         }
+        // Verify that render method has access to secure wrappers when invoked through interop
+        isSecureWindow(window);
+        isSecureDocument(document);
+        
+        testUtil.assertEquals("undefined", typeof $A, // eslint-disable-line lwc/no-aura
+            "Expected $A to be not accessible in connectedCallback");        
+        testUtil.assertTrue(this instanceof ComponentLifecycleTester,
+            "Expected context to be an instance of the component class in connectedCallback");
+        const data = this.complexDataStructure;
+        testUtil.assertEquals(
+            null,
+            data.domElement,
+            'Mismatch in domElement value'
+        );
+        isSecureWindow(data.win);
+        isSecureDocument(data.doc);
+        setTimeout(() => {
+            this.dispatchEvent(this.customEvent);
+        }, 1000);        
+        window.connectedCallbackHookCalled = true;
     }
 
     disconnectedCallback() {
-        if (this.shouldTestDisconnectedCallbackHook) {
-            // Verify that render method has access to secure wrappers when invoked through interop
-            testUtil.assertStartsWith("SecureWindow", window.toString(), "Expected window to"
-                + " return SecureWindow in disconnectedCallback");
-            testUtil.assertEquals("undefined", typeof $A, // eslint-disable-line lwc/no-aura
-                "Expected $A to be not accessible in disconnectedCallback");
-            testUtil.assertStartsWith("SecureDocument", document.toString(), "Expected document to"
-                + " return SecureDocument in disconnectedCallback");
-            testUtil.assertTrue(this instanceof ComponentLifecycleTester,
-                "Expected context to be an instance of the component class in disconnectedCallback");
-            window.disconnectedCallbackHookCalled = true;
+        if (!this.shouldTestDisconnectedCallbackHook) {
+            return;
         }
+        // Verify that render method has access to secure wrappers when invoked through interop
+        isSecureWindow(window);
+        isSecureDocument(document);
+        testUtil.assertEquals("undefined", typeof $A, // eslint-disable-line lwc/no-aura
+            "Expected $A to be not accessible in disconnectedCallback");
+        testUtil.assertTrue(this instanceof ComponentLifecycleTester,
+            "Expected context to be an instance of the component class in disconnectedCallback");
+        const data = this.complexDataStructure;
+        isSecureDocument(data.doc);
+        isSecureWindow(data.win);
+        testUtil.assertEquals(
+            true,
+            SECURE_ELEMENT_REGEX.test(data.domElement),
+            'Mismatch in domElement value'
+        );
+        setTimeout(() => {
+            this.dispatchEvent(this.customEvent);
+        }, 1000);        
+
+        window.disconnectedCallbackHookCalled = true;        
     }
 
     renderedCallback() {
-        if (this.shouldTestRenderedCallbackHook) {
-            // Verify that render method has access to secure wrappers when invoked through interop
-            testUtil.assertStartsWith("SecureWindow", window.toString(), "Expected window to"
-                + " return SecureWindow in renderedCallback");
-            testUtil.assertEquals("undefined", typeof $A, // eslint-disable-line lwc/no-aura
-                "Expected $A to be not accessible in renderedCallback");
-            testUtil.assertStartsWith("SecureDocument", document.toString(), "Expected document to"
-                + " return SecureDocument in renderedCallback");
-            testUtil.assertTrue(this instanceof ComponentLifecycleTester,
-                "Expected context to be an instance of the component class in renderedCallback");
-            window.renderedCallbackHookCalled = true;
+        if (!this.shouldTestRenderedCallbackHook) {
+            return;
         }
+        // Verify that render method has access to secure wrappers when invoked through interop
+        isSecureWindow(window);
+        isSecureDocument(document);
+        testUtil.assertEquals("undefined", typeof $A, // eslint-disable-line lwc/no-aura
+            "Expected $A to be not accessible in renderedCallback");
+        testUtil.assertTrue(this instanceof ComponentLifecycleTester,
+            "Expected context to be an instance of the component class in renderedCallback");
+        
+        const data = this.complexDataStructure;
+        isSecureWindow(data.win);
+        isSecureDocument(data.doc);
+        testUtil.assertEquals(
+            true,
+            SECURE_ELEMENT_REGEX.test(data.domElement),
+            'Mismatch in domElement value'
+        );
+        setTimeout(() => {
+            this.dispatchEvent(this.customEvent);
+        }, 1000);        
+
+        window.renderedCallbackHookCalled = true;
     }
 }

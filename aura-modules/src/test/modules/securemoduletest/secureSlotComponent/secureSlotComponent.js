0511b9d190c08bc3c3909de9ccc8406a5cb8b69a
Theodore Lau <t.lau@salesforce.com>
2018-10-12 10:22:52 -0700

    @W-5483666@ Fix Slot Bug (#3610)
    *  @W-5483666@ Snapshot behavior of slots on this commit.
    
    * @W-5483666@ Commit Signing
    
    * @W-5483666@ Commit Signing
    
    * @W-5483666@ Only Expected Results
    
    * @W-5483666@ Only Expected Results
    
    * @W-5483666@ Comment Change

--- aura-modules/src/test/modules/securemoduletest/secureSlotComponent/secureSlotComponent.js
+++ aura-modules/src/test/modules/securemoduletest/secureSlotComponent/secureSlotComponent.js
@@ -12,15 +12,16 @@ export default class SecureSlotComponent extends LightningElement {
         let secureSlotElementAssignedElements = secureSlotElement.assignedElements();
 
         testUtils.assertEquals('my-slot', secureSlotElementName, 'Expected named <slot> to have name equal to "my-slot"');
-        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[0].toString(), 'Expected first assigned node of named <slot> to be <span>!');
-        testUtils.assertEquals('SecureElement: [object HTMLDivElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[1].toString(), 'Expected second assigned node of named <slot> to be <div>!');
-        testUtils.assertEquals(undefined, secureSlotElementAssignedNodes[2], 'Expected no more assigned nodes to named <slot>!');
-        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[0].toString(), 'Expected first assigned node of named <slot> to be <span>!');
-        testUtils.assertEquals('SecureElement: [object HTMLDivElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[1].toString(), 'Expected second assigned node of named <slot> to be <div>!');
-        testUtils.assertEquals(undefined, secureSlotElementAssignedElements[2], 'Expected no more assigned nodes to named <slot>!');
+        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[0] + '', 'Expected first assigned node of named <slot> to be <span>!');
+        testUtils.assertEquals('SecureElement: [object HTMLDivElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[1] + '', 'Expected second assigned node of named <slot> to be <div>!');
+        testUtils.assertEquals(undefined, secureSlotElementAssignedNodes[4], 'Expected no more assigned nodes to named <slot>!');
+        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[0] + '', 'Expected first assigned elements of named <slot> to be <span>!');
+        testUtils.assertEquals('SecureElement: [object HTMLDivElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[1] + '', 'Expected second assigned elements of named <slot> to be <div>!');
+        testUtils.assertEquals(undefined, secureSlotElementAssignedElements[4], 'Expected no more assigned elements to named <slot>!');
 
-        testUtils.assertEquals('Non-Default Named Slot Text', secureSlotElement.outerText, 'Expected named <slot> to have non-default text!');
-        testUtils.assertEquals('SecureElement: [object HTMLSlotElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElement.toString(), 'Expected named <slot> to be a SecureElement!');
+        testUtils.assertEquals(0, secureSlotElement.querySelectorAll('div').length, 'Expected 0 <div> tags to be inside the slot!');
+        testUtils.assertEquals('', secureSlotElement.innerText, 'Expected named <slot> to be an empty string!');
+        testUtils.assertEquals('SecureElement: [object HTMLSlotElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElement + '', 'Expected named <slot> to be a SecureElement!');
         testUtils.assertTrue(secureSlotElement instanceof HTMLSlotElement, 'Expected named <slot> to be an instance of HTMLSlotElement!');
     }
 
@@ -32,15 +33,16 @@ export default class SecureSlotComponent extends LightningElement {
         let secureSlotElementAssignedElements = secureSlotElement.assignedElements();
 
         testUtils.assertEquals('', secureSlotElementName, 'Expected default <slot> to have name equal to an empty string.');
-        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[0].toString(), 'Expected first assigned node of named <slot> to be <span>!');
-        testUtils.assertEquals('SecureElement: [object HTMLParagraphElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[1].toString(), 'Expected second assigned node of named <slot> to be <p>!');
-        testUtils.assertEquals('SecureElement: [object Text]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[2].toString(), 'Expected no more assigned nodes to named <slot>!');
-        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[0].toString(), 'Expected first assigned node of named <slot> to be <span>!');
-        testUtils.assertEquals('SecureElement: [object HTMLParagraphElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[1].toString(), 'Expected second assigned node of named <slot> to be <p>!');
-        testUtils.assertEquals(undefined, secureSlotElementAssignedElements[2], 'Expected no more assigned nodes to named <slot>!');
+        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[0] + '', 'Expected first assigned node of named <slot> to be <span>!');
+        testUtils.assertEquals('SecureElement: [object HTMLParagraphElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[1] + '', 'Expected second assigned node of named <slot> to be <p>!');
+        testUtils.assertEquals('SecureElement: [object Text]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedNodes[4] + '', 'Expected no more assigned nodes to named <slot>!');
+        testUtils.assertEquals('SecureElement: [object HTMLSpanElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[0] + '', 'Expected first assigned elements of named <slot> to be <span>!');
+        testUtils.assertEquals('SecureElement: [object HTMLParagraphElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElementAssignedElements[1] + '', 'Expected second assigned elements of named <slot> to be <p>!');
+        testUtils.assertEquals(undefined, secureSlotElementAssignedElements[4], 'Expected no more assigned elements to named <slot>!');
 
-        testUtils.assertEquals('Non-Default Default Slot Text:TextNode', secureSlotElement.outerText, 'Expected default <slot> to have non-default text!');
-        testUtils.assertEquals('SecureElement: [object HTMLSlotElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElement.toString(), 'Expected default <slot> to be a SecureElement!');
+        testUtils.assertEquals(0, secureSlotElement.querySelectorAll('p').length, 'Expected 0 <p> tags to be inside the slot!');
+        testUtils.assertEquals('', secureSlotElement.innerText, 'Expected default <slot> to be an empty string!');
+        testUtils.assertEquals('SecureElement: [object HTMLSlotElement]{ key: {"namespace":"secureModuleTest"} }', secureSlotElement + '', 'Expected default <slot> to be a SecureElement!');
         testUtils.assertTrue(secureSlotElement instanceof HTMLSlotElement, 'Expected default <slot> to be an instance of HTMLSlotElement!');
     }
 }
